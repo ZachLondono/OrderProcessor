89b4ec9c853eb22c5ed2dc8b831609cf3e331f70
ZachLondono <zachary.londono5@gmail.com>
2022-11-03 17:38:29 -0400

    Added table of orders scheduled for a specific date

---
+++ ApplicationCore/ApplicationCore/Features/Schedule/GetOrdersByDate.cs
@@ -0,0 +1,40 @@
+﻿using ApplicationCore.Infrastructure;
+using ApplicationCore.Infrastructure.Data;
+using Dapper;
+
+namespace ApplicationCore.Features.Schedule;
+
+public class GetOrdersByDate {
+
+    public record Query(DateTime Date) : IQuery<IEnumerable<ScheduledOrder>>;
+
+    public class Handler : QueryHandler<Query, IEnumerable<ScheduledOrder>> {
+
+        private readonly IDbConnectionFactory _factory;
+
+        public Handler(IDbConnectionFactory factory) {
+           _factory = factory;
+        }
+
+        public override async Task<Response<IEnumerable<ScheduledOrder>>> Handle(Query query) {
+
+            try { 
+                
+                using var connection = _factory.CreateConnection();
+
+                const string sql = "SELECT id, number, name, productiondate, status FROM orders WHERE date(productiondate) = date(@Date);";
+                var orders = await connection.QueryAsync<ScheduledOrder>(sql, query);
+
+                return new(orders);
+
+            } catch (Exception e) {
+                return new(new Error() {
+                    Message = $"Exception thrown while trying to find orders scheduled for date {query.Date.ToShortDateString()}\n\nException:\n{e.Message}"
+                });
+            }
+
+        }
+
+    }
+
+}
---
+++ ApplicationCore/ApplicationCore/Features/Schedule/ScheduledOrder.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationCore.Features.Orders.Domain;
+
+namespace ApplicationCore.Features.Schedule;
+
+public class ScheduledOrder {
+
+    public Guid Id { get; set; }
+
+    public string Number { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public DateTime ScheduledDate { get; set; }
+
+    public Status Status { get; set; }
+
+}
