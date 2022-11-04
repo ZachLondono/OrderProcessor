76d844a063c946732fdec2b2f735ac2c58d19e04
ZachLondono <zachary.londono5@gmail.com>
2022-11-04 18:08:33 -0400

    Added comment

--- ApplicationCore/ApplicationCore/Infrastructure/IDomainRequest.cs
+++ ApplicationCore/ApplicationCore/Infrastructure/IDomainRequest.cs
@@ -5,3 +5,6 @@ namespace ApplicationCore.Infrastructure;
 public interface IQuery<TResponse> : IBaseDomainRequest, IRequest<Response<TResponse>> { }
 
 public interface ICommand : IBaseDomainRequest, IRequest<Response> { }
+
+
+// TODO: create an ICommand with a response
