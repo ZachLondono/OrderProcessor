4f8d3b993528e1bc6d096e616d5efadbd309d5bd
ZachLondono <zachary.londono5@gmail.com>
2022-11-04 18:04:34 -0400

    Added message box interface and wpfimplementation

---
+++ ApplicationCore/ApplicationCore/Shared/IMessageBox.cs
@@ -0,0 +1,26 @@
+﻿namespace ApplicationCore.Shared;
+
+public interface IMessageBoxService {
+
+    // TODO: create seperate methods for different message boxes (ie. error, informaiton, Yes/No)
+
+    public void OpenDialog(string text, string caption);
+
+    public OKCancelResult OpenDialogOKCancel(string text, string caption);
+
+    public YesNoResult OpenDialogYesNo(string text, string caption);
+
+}
+
+public enum OKCancelResult {
+    Unknown,
+    OK,
+    Cancel
+}
+
+
+public enum YesNoResult {
+    Unknown,
+    Yes,
+    No
+}
---
+++ DesktopHost/Dialogs/WFPMessageBox.cs
@@ -0,0 +1,31 @@
+﻿using ApplicationCore.Shared;
+using System.Windows;
+
+namespace DesktopHost.Dialogs;
+
+internal class WPFMessageBox : IMessageBoxService {
+
+    public void OpenDialog(string text, string caption) {
+        MessageBox.Show(text, caption, MessageBoxButton.OK);
+    }
+
+    public OKCancelResult OpenDialogOKCancel(string text, string caption) {
+        var result = MessageBox.Show(text, caption, MessageBoxButton.OKCancel);
+
+        return result switch {
+            MessageBoxResult.OK => OKCancelResult.OK,
+            MessageBoxResult.Cancel => OKCancelResult.Cancel,
+            _ => OKCancelResult.Unknown,
+        };
+    }
+
+    public YesNoResult OpenDialogYesNo(string text, string caption) {
+        var result = MessageBox.Show(text, caption, MessageBoxButton.YesNo);
+
+        return result switch {
+            MessageBoxResult.Yes => YesNoResult.Yes,
+            MessageBoxResult.No => YesNoResult.No,
+            _ => YesNoResult.Unknown,
+        };
+    }
+}
