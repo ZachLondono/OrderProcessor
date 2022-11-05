d94873856ddffc2017c50750752ac1b3d6dba79e
ZachLondono <zachary.londono5@gmail.com>
2022-11-05 17:21:52 -0400

    Set properties when making instance

--- DesktopHost/Dialogs/WPFFilePickerDialogService.cs
+++ DesktopHost/Dialogs/WPFFilePickerDialogService.cs
@@ -8,12 +8,13 @@ public class WPFDialogFilePicker : IFilePicker {
     public bool TryPickFile(string title, string directory, FilePickerFilter filter, out string fileName) {
 
         fileName = string.Empty;
-        var dialog = new OpenFileDialog();
-        dialog.InitialDirectory = directory;
-        dialog.Multiselect = false;
-        dialog.Title = title;
-        dialog.Filter = filter.ToFilterString();
-        
+        var dialog = new OpenFileDialog {
+            InitialDirectory = directory,
+            Multiselect = false,
+            Title = title,
+            Filter = filter.ToFilterString()
+        };
+
         bool? result = dialog.ShowDialog();
         if (result is null) return false;
         bool wasPicked = (bool) result;
