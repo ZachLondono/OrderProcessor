220e8c6fb91301fde1bf2876b270f70b5b842086
ZachLondono <zachary.londono5@gmail.com>
2022-11-05 17:23:16 -0400

    Added constructors

--- ApplicationCore/ApplicationCore/Features/Orders/Domain/ValueObjects/UBoxDimensions.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Domain/ValueObjects/UBoxDimensions.cs
@@ -6,4 +6,12 @@ public class UBoxDimensions {
     public Dimension B { get; set; } = Dimension.FromMillimeters(0);
     public Dimension C { get; set; } = Dimension.FromMillimeters(0);
 
+    public UBoxDimensions() { }
+
+    public UBoxDimensions(Dimension a, Dimension b, Dimension c) {
+        A = a;
+        B = b;
+        C = c;
+    }
+
 }
--- ApplicationCore/ApplicationCore/Infrastructure/Error.cs
+++ ApplicationCore/ApplicationCore/Infrastructure/Error.cs
@@ -4,4 +4,10 @@ public class Error {
 
     public string Message { get; set; } = string.Empty;
 
+    public Error() { }
+
+    public Error(string message) {
+        Message = message;
+    }
+
 }
