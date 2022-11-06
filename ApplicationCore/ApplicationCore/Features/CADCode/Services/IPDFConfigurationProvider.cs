9659334fa942e60060bc08dd1c8a054087f837d7
ZachLondono <zachary.londono5@gmail.com>
2022-11-06 12:29:36 -0500

    Removed unneeded files

--- ApplicationCore/ApplicationCore/Features/CADCode/Services/IPDFConfigurationProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using ApplicationCore.Features.CADCode.Services.Domain.PDF;
-
-namespace ApplicationCore.Features.CADCode.Services;
-
-internal interface IPDFConfigurationProvider {
-
-    public PDFConfiguration GetConfiguration();
-
-}
