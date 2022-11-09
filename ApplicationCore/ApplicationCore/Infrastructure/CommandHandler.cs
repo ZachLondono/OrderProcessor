c20d6fe2cde4fce5392224b9560babc70819cdc9
ZachLondono <zachary.londono5@gmail.com>
2022-11-08 21:15:28 -0500

    Added an interface for commands with a response

--- ApplicationCore/ApplicationCore/Infrastructure/CommandHandler.cs
+++ ApplicationCore/ApplicationCore/Infrastructure/CommandHandler.cs
@@ -5,8 +5,18 @@ namespace ApplicationCore.Infrastructure;
 public abstract class CommandHandler<TCommand> : IRequestHandler<TCommand, Response> where TCommand : ICommand {
 
     public async Task<Response> Handle(TCommand request, CancellationToken cancellationToken) => await Handle(request);
-
+    
     // TODO: add cancellation token
     public abstract Task<Response> Handle(TCommand command);
+
+}
+
+
+public abstract class CommandHandler<TCommand, TResponse> : IRequestHandler<TCommand, Response<TResponse>> where TCommand : ICommand<TResponse> {
+
+    public async Task<Response<TResponse>> Handle(TCommand request, CancellationToken cancellationToken) => await Handle(request);
+
+    // TODO: add cancellation token
+    public abstract Task<Response<TResponse>> Handle(TCommand command);
 
 }
---
+++ ApplicationCore/ApplicationCore/Infrastructure/IQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace ApplicationCore.Infrastructure;
+
+// A query represents a request for data from a source, it's result can be cached
+public interface IQuery<TResponse> : IBaseDomainRequest, IRequest<Response<TResponse>> { }
