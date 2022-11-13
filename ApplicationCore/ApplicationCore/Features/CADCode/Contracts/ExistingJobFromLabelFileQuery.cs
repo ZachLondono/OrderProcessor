d5b806dfa03ebd39abc7a07e2d69c5f83fdf374a
ZachLondono <zachary.londono5@gmail.com>
2022-11-13 17:58:43 -0500

    Added query and handler for getting released job from cadcode label file

---
+++ ApplicationCore/ApplicationCore/Features/CADCode/Contracts/ExistingJobFromLabelFileQuery.cs
@@ -0,0 +1,6 @@
+﻿using ApplicationCore.Features.CADCode.Contracts.ProgramRelease;
+using ApplicationCore.Infrastructure;
+
+namespace ApplicationCore.Features.CADCode.Contracts;
+
+public record ExistingJobFromLabelFileQuery(string LabelFilePath, string JobName) : IQuery<ReleasedJob>;
---
+++ ApplicationCore/ApplicationCore/Features/CADCode/Handlers/ExistingJobFromLabelFileHandler.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationCore.Features.CADCode.Contracts;
+using ApplicationCore.Features.CADCode.Contracts.ProgramRelease;
+using ApplicationCore.Features.CADCode.Services;
+using ApplicationCore.Infrastructure;
+
+namespace ApplicationCore.Features.CADCode.Handlers;
+
+public class ExistingJobFromLabelFileHandler : QueryHandler<ExistingJobFromLabelFileQuery, ReleasedJob> {
+
+    private readonly IExistingJobProvider _existingJobProvider;
+
+    public ExistingJobFromLabelFileHandler(IExistingJobProvider existingJobProvider) {
+		_existingJobProvider = existingJobProvider;
+	}
+
+	public override async Task<Response<ReleasedJob>> Handle(ExistingJobFromLabelFileQuery query) {
+
+        var job = await _existingJobProvider.LoadExistingJobAsync(query.LabelFilePath, query.JobName);
+
+        return new(job);
+
+	}
+
+}
