8e0dc2c53a5abbacad4ee53bb74cd4989d418b80
ZachLondono <zachary.londono5@gmail.com>
2022-11-13 15:10:15 -0500

    Use javascript to toggle dropdown visibility so that it can be hidden when clicking outside of dropdown

---
+++ DesktopHost/wwwroot/js/dropdowns.js
@@ -0,0 +1,29 @@
+﻿function toggleDropdown(dropdown) {
+    if (dropdown.getAttribute("hidden") == null) {
+        dropdown.setAttribute("hidden", "");
+        return true;
+    } else {
+        dropdown.removeAttribute("hidden", "");
+        return false;
+    }
+}
+
+window.onclick = function (event) {
+
+    var dropdownbtns = document.getElementsByClassName("dropdown-btn")
+    for (i = 0; i < dropdownbtns.length; i++) {
+        if (dropdownbtns[i].contains(event.target)) return;
+    }
+
+    var dropdowns = document.getElementsByClassName("dropdown");
+    for (i = 0; i < dropdowns.length; i++) {
+        var dropdown = dropdowns[i];
+        
+        // If click is within a dropdown, don't close it
+        if (dropdown.contains(event.target)) continue;
+
+        if (dropdown.getAttribute("hidden") != null) return;
+        else dropdown.setAttribute("hidden", "");
+    }
+
+}
