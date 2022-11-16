d05f8e40f0df3074bc30d7e12a45a2563c5ae4ae
ZachLondono <zachary.londono5@gmail.com>
2022-11-15 23:30:31 -0500

    Added command and query for richelieu company id

---
+++ ApplicationCore/ApplicationCore/Features/Companies/Commands/CreateRichelieuIdCompanyIdMapping.cs
@@ -0,0 +1,33 @@
+﻿using ApplicationCore.Infrastructure;
+using ApplicationCore.Infrastructure.Data;
+using Dapper;
+
+namespace ApplicationCore.Features.Companies.Commands;
+
+public class CreateRichelieuIdCompanyIdMapping {
+
+    public record Command(string RichelieuId, Guid CompanyId) : ICommand;
+
+    public class Handler : CommandHandler<Command> {
+
+        private readonly IDbConnectionFactory _factory;
+
+        public Handler(IDbConnectionFactory factory) {
+            _factory = factory;
+        }
+
+        public override async Task<Response> Handle(Command command) {
+
+            using var connection = _factory.CreateConnection();
+
+            const string sql = "INSERT INTO richelieucompanies_companies (richelieuid, companyid) VALUES (@RichelieuId, @CompanyId);";
+
+            await connection.ExecuteAsync(sql, command);
+
+            return new Response();
+
+        }
+
+    }
+
+}
---
+++ ApplicationCore/ApplicationCore/Features/Companies/Queries/GetCompanyByRichelieuId.cs
@@ -0,0 +1,54 @@
+﻿using ApplicationCore.Features.Companies.Domain;
+using ApplicationCore.Features.Companies.Queries.DataModels;
+using ApplicationCore.Infrastructure;
+using ApplicationCore.Infrastructure.Data;
+using Dapper;
+
+namespace ApplicationCore.Features.Companies.Queries;
+
+public class GetCompanyByRichelieuId {
+
+    public record Query(string RichelieuId) : IQuery<Company?>;
+
+    public class Handler : QueryHandler<Query, Company?> {
+
+        private readonly IDbConnectionFactory _factory;
+
+        public Handler(IDbConnectionFactory factory) {
+           _factory = factory;
+        }
+
+        public override async Task<Response<Company?>> Handle(Query query) {
+            
+            using var connection = _factory.CreateConnection();
+
+            const string sql = @"SELECT
+                                        id, name, phonenumber, invoiceemail, confirmationemail, line1, line2, line3, city, state, zip, country
+                                    FROM
+                                        companies
+                                    LEFT JOIN addresses ON companies.id = addresses.companyid
+                                    WHERE id = (
+                                        SELECT companyid FROM richelieucompanies_companies WHERE richelieuid = @RichelieuCompanyId
+                                    );";
+
+            var data = await connection.QuerySingleOrDefaultAsync<CompanyDataModel>(sql, query);
+
+            if (data is null) return new((Company?)null);
+
+            var company = new Company(data.Id, data.Name, new() {
+                Line1 = data.Line1,
+                Line2 = data.Line2,
+                Line3 = data.Line3,
+                City = data.City,
+                State = data.State,
+                Zip = data.Zip,
+                Country = data.Country
+            }, data.PhoneNumber, data.InvoiceEmail, data.ConfirmationEmail);
+
+            return new(company);
+
+
+        }
+    }
+
+}
