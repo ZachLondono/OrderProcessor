511351a1ae6b213feffda50057db09aad68bbc2f
ZachLondono <zachary.londono5@gmail.com>
2022-11-17 21:18:51 -0500

    Added comment

--- ApplicationCore/ApplicationCore/Features/CADCode/Contracts/CNCReleaseRequest.cs
+++ ApplicationCore/ApplicationCore/Features/CADCode/Contracts/CNCReleaseRequest.cs
@@ -3,4 +3,5 @@ using ApplicationCore.Infrastructure;
 
 namespace ApplicationCore.Features.CADCode.Contracts;
 
+// TODO: rename to generate gcode request
 public record CNCReleaseRequest(CNCBatch Batch) : ICommand<ReleasedJob>;
