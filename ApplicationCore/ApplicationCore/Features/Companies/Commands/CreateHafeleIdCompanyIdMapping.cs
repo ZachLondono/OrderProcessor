9014dd4a264176dcacbb0b016034a8a70df2c914
ZachLondono <zachary.londono5@gmail.com>
2022-11-20 00:10:48 -0500

    Added command & query for mapping hafele account number to company ID

---
+++ ApplicationCore/ApplicationCore/Features/Companies/Commands/CreateHafeleIdCompanyIdMapping.cs
@@ -0,0 +1,33 @@
+﻿using ApplicationCore.Infrastructure;
+using ApplicationCore.Infrastructure.Data;
+using Dapper;
+
+namespace ApplicationCore.Features.Companies.Commands;
+
+public class CreateHafeleIdCompanyIdMapping {
+
+	public record Command(string HafeleAccountNumber, Guid CompanyId) : ICommand;
+
+	public class Handler : CommandHandler<Command> {
+
+		private readonly IDbConnectionFactory _factory;
+
+		public Handler(IDbConnectionFactory factory) {
+			_factory = factory;
+		}
+
+		public override async Task<Response> Handle(Command command) {
+
+			using var connection = _factory.CreateConnection();
+
+			const string sql = "INSERT INTO hafelecompanies_companies (hafeleaccountnum, companyid) VALUES (@HafeleAccountNumber, @CompanyId);";
+
+			await connection.ExecuteAsync(sql, command);
+
+			return new Response();
+
+		}
+
+	}
+
+}
