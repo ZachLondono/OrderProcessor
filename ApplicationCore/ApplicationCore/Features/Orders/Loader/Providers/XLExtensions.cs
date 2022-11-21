e4d9e017d16ca97da93eb36d7f84d9110068c514
ZachLondono <zachary.londono5@gmail.com>
2022-11-21 11:20:10 -0500

    Added extension methods to read values from cell better

--- ApplicationCore/ApplicationCore/Features/Orders/Loader/Providers/XLExtensions.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Loader/Providers/XLExtensions.cs
@@ -7,4 +7,86 @@ internal static class XLExtensions {
 		return relative.Address.Worksheet.Cell(relative.Address.RowNumber + rowOffset, relative.Address.ColumnNumber + colOffset);
 	}
 
+	public static string ReadString(this IXLCell cell) {
+
+		if (cell.HasFormula) return (string) cell.CachedValue;
+
+		bool wasRead = cell.TryGetValue<string>(out string val);
+
+		if (wasRead) return val;
+
+		return (string) cell.CachedValue;
+
+	}
+
+	public static double ReadDouble(this IXLCell cell) {
+
+		if (cell.HasFormula) {
+
+			if (cell.DataType == XLDataType.Number) return (double) cell.CachedValue;
+
+			if (double.TryParse(cell.CachedValue.ToString() ?? "0", out double parsed)) return parsed;
+
+			return default;
+
+		}
+
+		bool wasRead = cell.TryGetValue<double>(out double val);
+
+		if (wasRead) return val;
+
+		return default;
+
+	}
+
+	public static decimal ReadDecimal(this IXLCell cell) {
+
+		if (cell.HasFormula) {
+
+			if (cell.DataType == XLDataType.Number) return (decimal)cell.CachedValue;
+
+			if (decimal.TryParse(cell.CachedValue.ToString() ?? "0", out decimal parsed)) return parsed;
+
+			return default;
+
+		}
+
+		bool wasRead = cell.TryGetValue<decimal>(out decimal val);
+
+		if (wasRead) return val;
+
+		return default;
+
+	}
+
+	public static int ReadInt(this IXLCell cell) {
+
+		if (cell.HasFormula) {
+
+			if (cell.DataType == XLDataType.Number) return (int)cell.CachedValue;
+
+			if (int.TryParse(cell.CachedValue.ToString() ?? "0", out int parsed)) return parsed;
+
+			return default;
+
+		}
+
+		bool wasRead = cell.TryGetValue<int>(out int val);
+
+		if (wasRead) return val;
+
+		return default;
+
+	}
+
+	public static bool ValueIsNullOrWhitespace(this IXLCell cell) {
+
+		if (cell.HasFormula) return string.IsNullOrWhiteSpace(cell.CachedValue.ToString());
+
+		if (cell.TryGetValue(out string val)) return string.IsNullOrWhiteSpace(val);
+
+		return true;
+
+	}
+
 }
