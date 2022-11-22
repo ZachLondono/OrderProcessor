c9b983aeb25b3d9ae0701b9c8fae1525ab316913
ZachLondono <zachary.londono5@gmail.com>
2022-11-21 22:29:50 -0500

    Added default constructor

--- ApplicationCore/ApplicationCore/Features/Orders/Domain/DrawerBoxMatieral.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Domain/DrawerBoxMatieral.cs
@@ -10,6 +10,12 @@ public class DrawerBoxMaterial {
 
     public Dimension Thickness { get; set; }
 
+    public DrawerBoxMaterial() {
+        Id = Guid.Empty;
+        Name = string.Empty;
+        Thickness = Dimension.FromMillimeters(0);
+    }
+
     public DrawerBoxMaterial(Guid id, string name, Dimension thickness) {
         Id = id;
         Name = name;
