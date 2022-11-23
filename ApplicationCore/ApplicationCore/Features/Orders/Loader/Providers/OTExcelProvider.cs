f3835573df24397d2f519d261a8ca11ec0afa05f
ZachLondono <zachary.londono5@gmail.com>
2022-11-23 00:02:13 -0500

    Implemented OT order provider

--- ApplicationCore/ApplicationCore/Features/Orders/Loader/Providers/OTExcelProvider.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Loader/Providers/OTExcelProvider.cs
@@ -8,6 +8,7 @@ using ApplicationCore.Features.Companies.Commands;
 using ApplicationCore.Features.Companies.Domain.ValueObjects;
 using ApplicationCore.Features.Companies.Domain;
 using ApplicationCore.Features.Orders.Loader.Providers.Results;
+using DocumentFormat.OpenXml.Drawing.Charts;
 
 namespace ApplicationCore.Features.Orders.Loader.Providers;
 
@@ -15,10 +16,12 @@ internal class OTExcelProvider : OrderProvider {
 
     private readonly IFileReader _fileReader;
     private readonly IBus _bus;
+    private readonly OTConfiguration _configuration;
 
-    public OTExcelProvider(IFileReader fileReader, IBus bus) {
+    public OTExcelProvider(IFileReader fileReader, IBus bus, OTConfiguration configuration) {
         _fileReader = fileReader;
         _bus = bus;
+        _configuration = configuration;
     }
 
     public override async Task<LoadOrderResult> LoadOrderData(string source) {
@@ -38,127 +41,149 @@ internal class OTExcelProvider : OrderProvider {
             }
         };
 
-        string number = sheet.Cell("OrderNumber").GetValue<string>();
-        string name = sheet.Cell("OrderName").GetValue<string>();
-        string dateStr = sheet.Cell("OrderDate").GetValue<string>();
-        if (!DateTime.TryParse(dateStr, out DateTime orderdate)) orderdate = DateTime.Now;
+        var messages = new List<LoadMessage>();
 
-        string customerName = sheet.Cell("CustomerName").GetValue<string>();
-        string customerLine1 = sheet.Cell("CustomerLine1").GetValue<string>();
-        string customerLine2 = sheet.Cell("CustomerLine2").GetValue<string>();
-        string customerLine3 = sheet.Cell("CustomerLine3").GetValue<string>();
-        string customerCity = sheet.Cell("CustomerCity").GetValue<string>();
-        string customerState = sheet.Cell("CustomerState").GetValue<string>();
-        string customerZip = sheet.Cell("CustomerZip").GetValue<string>();
-        string customerCountry = sheet.Cell("CustomerCountry").GetValue<string>();
+        var (customer, customerMessages) = await GetCustomerFromWorksheet(sheet);
+        messages.AddRange(customerMessages);
 
-        var qtyStart = sheet.Cell("QtyStart");
-        var widthStart = sheet.Cell("WidthStart");
-        var heightStart = sheet.Cell("HeightStart");
-        var depthStart = sheet.Cell("DepthStart");
+        if (customer is null) return new() {
+            Messages = messages,
+            Data = null
+        };
 
-        List<DrawerBoxData> boxes = new();
+        if (!DateTime.TryParse(sheet.Cell("Date").ReadString(), out DateTime orderDate)) {
+            orderDate = DateTime.Today;
+        }
 
-        int offset = 1;
-        while (true) {
+        string trackingNumber = sheet.Cell("OrderName").ReadString();
+        string jobName = sheet.Cell("JobName").ReadString();
+        string vendorName = sheet.Cell("J7").ReadString();
 
-            var qtyStr = qtyStart.GetOffsetCell(offset).GetValue<string>();
+        string material = sheet.Cell("Material").ReadString();
+        string notch = sheet.Cell("Notch").ReadString();
+        string bottom = sheet.Cell("BotThickness").ReadString();
+        string sides = sheet.Cell("SideOption").ReadString();
+        string clips = sheet.Cell("C7").ReadString();
 
-            if (string.IsNullOrWhiteSpace(qtyStr) || !int.TryParse(qtyStr, out int qty)) break;
+        string postFinishStr = sheet.Cell("C8").ReadString();
+        bool postFinish = false;
+        switch (postFinishStr) {
+            case "Yes":
+                postFinish = true;
+                break;
+            case "No":
+                postFinish = false;
+                break;
+            default:
+                messages.Add(new() {
+                    Severity = MessageSeverity.Warning,
+                    Message = $"Unkown post finish value '{postFinishStr}'"
+                });
+                postFinish = false;
+                break;
+        }
 
-            double width = widthStart.GetOffsetCell(offset).GetValue<double>();
-            double height = heightStart.GetOffsetCell(offset).GetValue<double>();
-            double depth = depthStart.GetOffsetCell(offset).GetValue<double>();
+        var options = new GlobalOptions() {
+            BoxMaterialId = GetMaterialId(material, _configuration.MaterialMap),
+            BottomMaterialId = GetMaterialId(bottom, _configuration.MaterialMap),
+            Clips = clips,
+            Sides = sides,
+            Notch = notch,
+            PostFinish = postFinish
+        };
 
-            var heightDim = Dimension.FromInches(height);
-            var widthDim = Dimension.FromInches(width);
-            var depthDim = Dimension.FromInches(depth);
-            boxes.Add(new() {
-                Line = offset,
-                UnitPrice = 0M,
-                Qty = qty,
-                Height = heightDim,
-                Width = widthDim,
-                Depth = depthDim,
-                Logo = false,
-                PostFinish = false,
-                ScoopFront = false,
-                FaceMountingHoles = false,
-                UBox = false,
-                FixedDividers = false,
-                BoxMaterialOptionId = Guid.NewGuid(),
-                BottomMaterialOptionId = Guid.NewGuid(),
-                Clips = "No Clips",
-                Notch = "No Notch",
-                Accessory = "No Accessories"
-            });
+        string orderNotes = sheet.Cell("G11").ReadString();
+
+        string production = sheet.Cell("Rush").ReadString();
+        bool rush = false;
+        switch (production) {
+            case "Standard - 1 Week":
+            case "2 Week Production":
+                rush = false;
+                break;
+            case "4 Day Rush":
+            case "3 Day Rush":
+            case "2 Day Rush":
+            case "1 Day Rush":
+                rush = true;
+                break;
+            default:
+                messages.Add(new() {
+                    Severity = MessageSeverity.Warning,
+                    Message = $"Unkown production time value '{production}'"
+                });
+                rush = false;
+                break;
+        }
+
+        var columns = new DataColumns() {
+            LineNumber = sheet.Cell("A16"),
+            Qty = sheet.Cell("B16"),
+            Height = sheet.Cell("C16"),
+            Width = sheet.Cell("D16"),
+            Depth = sheet.Cell("E16"),
+            PullOut = sheet.Cell("F16"),
+            Logo = sheet.Cell("J16"),
+            Accessory = sheet.Cell("K16"),
+            UnitPrice = sheet.Cell("L16"),
+            Note = sheet.Cell("Q16"),
+            UBoxA = sheet.Cell("T16"),
+            UBoxB = sheet.Cell("U16"),
+            UBoxC = sheet.Cell("V16"),
+        };
+
+        var shipping = sheet.Cell("Freight").ReadDecimal();
+
+        // TODO: discount percent is not read correctly because the cached value is in scientific notation
+        var discountPercent = sheet.Cell("R12").ReadDecimal();
+        var subtotal = sheet.Cell("D13").ReadDecimal();
+        var priceAdj = discountPercent * subtotal;
+
+        var tax = wb.Worksheet("Invoice").Cell("I7").ReadDecimal();
+
+        var boxes = new List<DrawerBoxData>();
+
+        int offset = 0;
+        while (offset < 198) {
+
+            var qtyCell = columns.Qty.GetOffsetCell(rowOffset: offset);
+            if (qtyCell.ValueIsNullOrWhitespace()) {
+                offset++;
+                continue;
+            }
+
+            var wasRead = TryReadBox(columns, offset, out DrawerBoxData box, options);
+
+            boxes.Add(box);
 
             offset++;
 
         }
 
-        // TODO: get id from configuration file
-        Guid vendorId = Guid.Parse("a81d759d-5b6c-4053-8cec-55a6c94d609e");
-        bool didError = false;
-        Company? customer = null;
-        var response = await _bus.Send(new GetCompanyByName.Query(customerName));
+        var vendorId = new Guid(_configuration.VendorIds[vendorName]);
 
-        response.Match(
-            c => {
-                customer = c;
-            },
-            error => {
-                // TODO: log error
-                didError = true;
-            }
-        );
+        var order = new OrderData() {
+            Number = trackingNumber,
+            Name = jobName,
+            Comment = orderNotes,
+            OrderDate = orderDate,
+            CustomerId = customer.Id,
+            VendorId = vendorId,
+            Rush = rush,
 
-        if (customer is null) {
-            customer = await CreateCustomer(customerName, customerLine1, customerLine2, customerLine3, customerCity, customerState, customerZip, customerCountry);
-            if (customer is null) return new() {
-				Data = null,
-				Messages = new List<LoadMessage>() {
-				    new() {
-					    Message = "Could not find or create customer",
-					    Severity = MessageSeverity.Error
-				    }
-			    }
-			};
-		}
+            PriceAdjustment = priceAdj,
+            Shipping = shipping,
+            Tax = tax,
 
-        if (customer is null || didError) return new() {
-			Data = null,
-			Messages = new List<LoadMessage>() {
-					new() {
-						Message = "Could not find or create customer",
-						Severity = MessageSeverity.Error
-					}
-				}
-		};
-
-		decimal tax = 0M;
-        decimal shipping = 0M;
-        decimal priceAdj = 0M;
-        string orderComment = "";
-        Dictionary<string, string> info = new();
-        var items = new List<AdditionalItemData>();
+            Boxes = boxes,
+            AdditionalItems = new(),
+            Info = new(),
+        };
 
         return new() {
-            Data = new OrderData() {
-				Number = number,
-				Name = name,
-				Comment = orderComment,
-				Tax = tax,
-				Shipping = shipping,
-				PriceAdjustment = priceAdj,
-				OrderDate = orderdate,
-				CustomerId = customer.Id,
-				VendorId = vendorId,
-				Info = info,
-				AdditionalItems = items,
-				Boxes = boxes
-			}
-		};
+            Messages = messages,
+            Data = order
+        };
 
     }
 
@@ -171,7 +196,7 @@ internal class OTExcelProvider : OrderProvider {
                 });
 
         var extension = Path.GetExtension(source);
-        if (extension != ".xlsx" && extension != ".xlsm")
+        if (extension != ".xlsm")
             return Task.FromResult(new ValidationResult() {
                 IsValid = false,
                 ErrorMessage = $"Invalid file type '{extension}'"
@@ -181,32 +206,186 @@ internal class OTExcelProvider : OrderProvider {
 
     }
 
-    private async Task<Company?> CreateCustomer(string name, string line1, string line2, string line3, string city, string state, string zip, string country) {
-        var adderes = new Address() {
-            Line1 = line1,
-            Line2 = line2,
-            Line3 = line3,
-            City = city,
-            State = state,
-            Zip = zip,
-            Country = country
+    private bool TryReadBox(DataColumns data, int offset, out DrawerBoxData box, GlobalOptions options) {
+        
+        box = new() {
+            BoxMaterialOptionId = options.BoxMaterialId,
+            BottomMaterialOptionId= options.BottomMaterialId,
+            Notch = options.Notch,
+            PostFinish = options.PostFinish,
+            Clips = options.Clips,
+            Assembled = true,
+            FixedDividers = false,
+            FaceMountingHoles = false
         };
 
-        // TODO: get customer contact info from allmoxy order data
-        Response<Company> createResponse = await _bus.Send(new CreateCompany.Command(name, adderes, "", "", "", ""));
+        try {
 
+            box.Line = data.LineNumber.GetOffsetCell(offset).ReadInt();
+            box.Qty = data.Qty.GetOffsetCell(offset).ReadInt();
+
+            double height = data.Height.GetOffsetCell(offset).ReadDouble();
+            double width = data.Width.GetOffsetCell(offset).ReadDouble();
+            double depth = data.Depth.GetOffsetCell(offset).ReadDouble();
+            box.Height = Dimension.FromInches(height);
+            box.Width = Dimension.FromInches(width);
+            box.Depth = Dimension.FromInches(depth);
+
+            string pullout = data.PullOut.GetOffsetCell(offset).ReadString();
+
+            if (pullout.Equals("Scoop Front")) {
+                box.ScoopFront = true;
+            } else if (pullout.Equals(string.Empty)) {
+                box.ScoopFront = false;
+            } else {
+                box.ScoopFront = false;
+                // TODO: warn about unkown value
+            }
+
+            string logo = data.Logo.GetOffsetCell(offset).ReadString();
+            if (logo.Equals("Yes")) {
+                box.Logo = true;
+            } else if (logo.Equals("No") || logo.Equals(string.Empty)) {
+                box.Logo = false;
+            } else { 
+                box.Logo = false;
+                // TODO: warn about unkown value
+            }
+
+            string accessory = data.Accessory.GetOffsetCell(offset).ReadString();
+            if (accessory.Equals("U-Box")) {
+                box.UBox = true;
+                double uboxA = data.UBoxA.GetOffsetCell(offset).ReadDouble();
+                double uboxB = data.UBoxB.GetOffsetCell(offset).ReadDouble();
+                double uboxC = data.UBoxC.GetOffsetCell(offset).ReadDouble();
+                box.UBoxA = Dimension.FromInches(uboxA);
+                box.UBoxB = Dimension.FromInches(uboxB);
+                box.UBoxC = Dimension.FromInches(uboxC);
+            } else {
+                box.Accessory = accessory;
+            }
+
+            box.Note = data.Note.GetOffsetCell(offset).ReadString();
+            box.UnitPrice = data.UnitPrice.GetOffsetCell(offset).ReadDecimal();
+
+            return true;
+
+        } catch {
+
+            return false;
+
+        }
+
+    }
+
+    private async Task<(Company?, List<LoadMessage>)> GetCustomerFromWorksheet(IXLWorksheet sheet) {
+
+        // TODO: move reading from sheet outside of function
+
+        string customerName = sheet.Cell("CustomerName").ReadString();
+        var customerResponse = await _bus.Send(new GetCompanyByName.Query(customerName));
+
+        List<LoadMessage> messages = new();
         Company? customer = null;
-        createResponse.Match(
+        bool hasError = false;
+        customerResponse.Match(
             c => {
                 customer = c;
             },
             error => {
+                hasError = true;
                 // TODO: log error
-                customer = null;
+                messages.Add(new() {
+                    Severity = MessageSeverity.Error,
+                    Message = error.Title
+                });
             }
         );
 
-        return customer;
+        if (hasError) return (null, messages);
+
+        if (customer is null) {
+
+            string addrLine1 = sheet.Cell("Address1").ReadString();
+            string addrLine2 = sheet.Cell("Address2").ReadString();
+            string city = sheet.Cell("City").ReadString();
+            string state = sheet.Cell("State").ReadString();
+            string zip = sheet.Cell("Zip").ReadString();
+
+            string contact = "";
+            string phone = "";
+            string invoiceEmail = "";
+            string confirmationEmail = "";
+
+            var address = new Address() {
+                Line1 = addrLine1,
+                Line2 = addrLine2,
+                Line3 = string.Empty,
+                City = city,
+                State = state,
+                Zip = zip,
+                Country = "USA"
+            };
+
+            var createResponse = await _bus.Send(new CreateCompany.Command(customerName, address, phone, invoiceEmail, confirmationEmail, contact));
+
+            createResponse.Match(
+                c => {
+                    customer = c;
+                },
+                error => {
+                    hasError = true;
+                    // TODO: log error
+                    messages.Add(new() {
+                        Severity = MessageSeverity.Error,
+                        Message = error.Title
+                    });
+                }
+            );
+
+        }
+
+        if (hasError || customer is null) return (null, messages);
+
+        return (customer, messages);
+
+    }
+
+    private static Guid GetMaterialId(string name, Dictionary<string, string> materialMap) {
+        if (!materialMap.ContainsKey(name)) return Guid.Empty;
+        try {
+            return Guid.Parse(materialMap[name]);
+        } catch {
+            return Guid.Empty;
+        }
+    }
+
+    struct GlobalOptions {
+
+        public Guid BoxMaterialId { get; set; }
+        public Guid BottomMaterialId { get; set; }
+        public string Notch { get; set; }
+        public string Sides { get; set; } // TODO: add this option to drawerboxes
+        public string Clips { get; set; }
+        public bool PostFinish { get; set; }
+
+    }
+
+    struct DataColumns {
+
+        public IXLCell LineNumber { get; set; }
+        public IXLCell Qty { get; set; }
+        public IXLCell Height { get; set; }
+        public IXLCell Width { get; set; }
+        public IXLCell Depth { get; set; }
+        public IXLCell PullOut { get; set; }
+        public IXLCell Logo { get; set; }
+        public IXLCell Accessory { get; set; }
+        public IXLCell UnitPrice { get; set; }
+        public IXLCell Note { get; set; }
+        public IXLCell UBoxA { get; set; }
+        public IXLCell UBoxB { get; set; }
+        public IXLCell UBoxC { get; set; }
 
     }
 
