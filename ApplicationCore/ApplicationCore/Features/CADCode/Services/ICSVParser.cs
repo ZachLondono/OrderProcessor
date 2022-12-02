72b68a59a0834a0b73a697315d2919620667cda9
ZachLondono <zachary.londono5@gmail.com>
2022-12-02 17:03:25 -0500

    Added CSV parser service

---
+++ ApplicationCore/ApplicationCore/Features/CADCode/Services/ICSVParser.cs
@@ -0,0 +1,9 @@
+﻿using ApplicationCore.Features.CADCode.Domain.CSV;
+
+namespace ApplicationCore.Features.CADCode.Services;
+
+internal interface ICSVParser {
+
+    public Task<CSVParseResult> ParsePartsAsync(string filepath);
+
+}
