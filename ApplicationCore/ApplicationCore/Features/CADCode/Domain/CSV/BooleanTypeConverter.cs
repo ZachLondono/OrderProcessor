f4e9b5ab38123de134e74a2d1b376b83c4f0a055
ZachLondono <zachary.londono5@gmail.com>
2022-12-02 17:02:19 -0500

    Added classes used in reading CSV tokens

---
+++ ApplicationCore/ApplicationCore/Features/CADCode/Domain/CSV/BooleanTypeConverter.cs
@@ -0,0 +1,15 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using CsvHelper.TypeConversion;
+
+namespace ApplicationCore.Features.CADCode.Domain.CSV;
+
+internal class BooleanTypeConverter : DefaultTypeConverter {
+
+	public override object? ConvertFromString(string? text, IReaderRow row, MemberMapData memberMapData) => text.ToLower() switch {
+		"yes" or "y" => true,
+		"no" or "n" or "" => false,
+		_ => false
+	};
+
+}
---
+++ ApplicationCore/ApplicationCore/Features/CADCode/Domain/CSV/NumberConverter.cs
@@ -0,0 +1,17 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using CsvHelper.TypeConversion;
+using System.Numerics;
+
+namespace ApplicationCore.Features.CADCode.Domain.CSV;
+
+internal class NumberConverter<TNumber> : DefaultTypeConverter where TNumber : INumber<TNumber> {
+
+	public override object ConvertFromString(string? text, IReaderRow row, MemberMapData memberMapData) {
+
+		if (string.IsNullOrWhiteSpace(text)) return TNumber.Zero;
+		return TNumber.Parse(text, null);
+
+	}
+
+}
