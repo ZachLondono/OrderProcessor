36aaa6cef9878072501ee33ec5ed82fb310286d1
ZachLondono <zachary.londono5@gmail.com>
2022-12-04 14:59:46 -0500

    Added class for converting csv tokens to cnc batch

---
+++ ApplicationCore/ApplicationCore/Features/CNC/CSVTokensParser.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Features.CNC.CSV.Contracts;
+using ApplicationCore.Features.CNC.GCode.Contracts;
+
+namespace ApplicationCore.Features.CNC;
+
+public class CSVTokensParser {
+
+	public CNCBatch ParseTokens(CSVReadResult csv) {
+
+		throw new NotImplementedException();
+
+	}
+
+}
