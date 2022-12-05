a2f33f926cd4939695cdc886e50bc96a85567181
ZachLondono <zachary.londono5@gmail.com>
2022-12-04 20:05:04 -0500

    Added window that will display progress info about csv reader

---
+++ DesktopHost/ReleasingCSVTokensWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using ApplicationCore.Features.CLI;
+using ApplicationCore.Infrastructure;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace DesktopHost;
+/// <summary>
+/// Interaction logic for ReleasingCSVTokensWindow.xaml
+/// </summary>
+public partial class ReleasingCSVTokensWindow : Window, IUIListener, IUIListener<CSVTokenProgressNotification> {
+	
+	public ReleasingCSVTokensWindow() {
+		InitializeComponent();
+	}
+
+	public void Handle(CSVTokenProgressNotification notification) {
+		Dispatcher.Invoke(() => ProgressMessageBox.AppendText(notification.Message + '\n'));
+	}
+
+}
