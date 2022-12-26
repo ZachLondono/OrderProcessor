204d999ac18766aa5da7cf512e68ba685a3ab584
ZachLondono <zachary.londono5@gmail.com>
2022-12-26 11:39:54 -0500

    Added some comments

--- ApplicationCore/ApplicationCore/Features/CNC/GCode/Configuration/GCodeGenerationConfiguration.cs
+++ ApplicationCore/ApplicationCore/Features/CNC/GCode/Configuration/GCodeGenerationConfiguration.cs
@@ -1,5 +1,8 @@
 ﻿namespace ApplicationCore.Features.CNC.GCode.Configuration;
 
+/// <summary>
+/// Settings required to correctly generate the gcode for specific machines
+/// </summary>
 public class GCodeGenerationConfiguration {
 	public string InventoryFilePath { get; set; } = string.Empty;
 	public GCodeMachineConfiguration DefaultMachineConfiguration { get; set; } = new();
--- ApplicationCore/ApplicationCore/Features/CNC/GCode/Domain/TableOrientation.cs
+++ ApplicationCore/ApplicationCore/Features/CNC/GCode/Domain/TableOrientation.cs
@@ -1,6 +1,7 @@
 ﻿namespace ApplicationCore.Features.CNC.GCode.Domain;
 
 // TODO: remove references to this in other features
+// TODO: Instead of "standard" or "rotated" specify which axis (x or y) is the long axis of the table
 public enum TableOrientation {
     Standard,
     Rotated
