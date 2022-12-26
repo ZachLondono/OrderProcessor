0fb3430e0011e0cb695bdd9a83227e5bf0ddf433
ZachLondono <zachary.londono5@gmail.com>
2022-12-26 11:40:17 -0500

    Refactored notification handler

--- ApplicationCore/ApplicationCore/Features/Orders/Release/Handlers/CADCodeProgramHandler.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Release/Handlers/CADCodeProgramHandler.cs
@@ -3,6 +3,7 @@ using ApplicationCore.Features.CNC.GCode;
 using ApplicationCore.Features.CNC.GCode.Contracts;
 using ApplicationCore.Features.CNC.GCode.Contracts.Machining;
 using ApplicationCore.Features.CNC.GCode.Contracts.Options;
+using ApplicationCore.Features.CNC.GCode.Contracts.Results;
 using ApplicationCore.Features.CNC.ReleasePDF;
 using ApplicationCore.Features.Orders.Domain;
 using ApplicationCore.Features.Orders.Domain.ValueObjects;
@@ -29,92 +30,124 @@ internal class CADCodeProgramHandler : DomainListener<TriggerOrderReleaseNotific
 
     public override async Task Handle(TriggerOrderReleaseNotification notification) {
 
-        if (!notification.ReleaseProfile.GenerateCNCPrograms) {
-            _uibus.Publish(new OrderReleaseInfoNotification("Not generating CADCode CNC release because option was disabled"));
-            return;
-        }
+		if (!notification.ReleaseProfile.GenerateCNCPrograms) {
+			_uibus.Publish(new OrderReleaseInfoNotification("Not generating CADCode CNC release because option was disabled"));
+			return;
+		}
 
-        _uibus.Publish(new OrderReleaseInfoNotification("Starting CADCode CNC release"));
+		_uibus.Publish(new OrderReleaseInfoNotification("Starting CADCode CNC release"));
+		List<Batch> batches = await CreateCNCBatches(notification);
 
-        var bottoms = notification.Order.Boxes.SelectMany(b => b.GetParts(_construction).Where(p => p.Type == DrawerBoxPartType.Bottom));
+		var options = GetGCodeOptions();
 
-        var parts = new List<Part>();
+		foreach (var batch in batches) { 
+			
+			var response = await _bus.Send(new GenerateGCode.Command(batch, options));
 
-        int index = 1;
-        foreach (var bottom in bottoms) {
+			await response.OnSuccessAsync(result => {
+				return GeneratePDFFromGCodeResult(result, batch, notification.ReleaseProfile.CNCReportOutputDirectory);
+			});
 
-            var matResponse = await _bus.Send(new GetDrawerBoxMaterialById.Query(bottom.MaterialId));
-            DrawerBoxMaterial material = new DrawerBoxMaterial(Guid.Empty, "UNKNOWN", Dimension.FromMillimeters(0));
-            matResponse.Match(
-                m => {
-                    if (m is not null) material = m;
-                },
-                error => { }
-            );
+			response.OnError(error => {
+				_uibus.Publish(new OrderReleaseErrorNotification($"{error.Title} - {error.Details}"));
+			});
 
-            var part = new Part() {
-                Description = "Drawer Box Bottom",
-                Length = bottom.Width,
-                Width = bottom.Length,
-                ContainsShape = false,
-                Qty = bottom.Qty,
-                LabelFields = new List<LabelField>(),
-                PrimaryFace = new() {
-                    FileName = $"Bottom{index++}",  // TODO: encode more part informaiton in file name
-                    Operations = new List<MachiningOperation>()
-                },
-                SecondaryFace = null
-            };
+		}
 
-            parts.Add(part);
+	}
 
-        }
-
-        var batch = new Batch() {
-            Name = $"{notification.Order.Number} - {notification.Order.Name}",
-            Parts = parts,
-            LabelFields = new List<LabelField>()
-        };
-
-
-		GCodeGenerationOptions options = new() {
-			Machines = new List<MachineGCodeOptions>() {
-				new() {
-					GenerateLabels = true,
-					GenerateNestPrograms = true,
-					GenerateSinglePartPrograms = true,
-					Name = "Andi"
-				}
+	private static GCodeGenerationOptions GetGCodeOptions() => new() {
+		Machines = new List<MachineGCodeOptions>() {
+			new() {
+				GenerateLabels = true,
+				GenerateNestPrograms = true,
+				GenerateSinglePartPrograms = true,
+				Name = "Andi"
 			}
+		}
+	};
+
+	private async Task GeneratePDFFromGCodeResult(GCodeGenerationResult result, Batch batch, string outputDirectory) {
+		
+		var job = GCodeToReleasedJobConverter.ConvertResult(result.MachineResults, result.GeneratedPicturesDirectory, batch.Name, batch.Parts);
+
+		var pdfResponse = await _bus.Send(new GenerateCNCReleasePDF.Command(job, outputDirectory));
+		pdfResponse.OnSuccess(pdfResult => {
+			foreach (var file in pdfResult.FilePaths)
+				_uibus.Publish(new OrderReleaseSuccessNotification($"CNC job report created {file}"));
+		});
+
+		pdfResponse.OnError(error => {
+			_uibus.Publish(new OrderReleaseErrorNotification($"{error.Title} - {error.Details}"));
+		});
+
+	}
+
+	private async Task<List<Batch>> CreateCNCBatches(TriggerOrderReleaseNotification notification) {
+		
+		var bottomGroups = notification.Order
+								.Boxes
+								.SelectMany(b => b.GetParts(_construction).Where(p => p.Type == DrawerBoxPartType.Bottom))
+								.GroupBy(b => b.MaterialId);
+
+		var batches = new List<Batch>();
+
+		foreach (var group in bottomGroups) {
+
+			var material = await GetMaterialById(group.Key);
+			var batchMaterial = new BatchMaterial() {
+				SheetStock = material.Name,
+				Thickness = material.Thickness
+			};
+
+			var parts = new List<Part>();
+			int index = 1;
+			foreach (var bottom in group) { 
+				var part = CreateCNCPart(bottom, index);
+				parts.Add(part);
+				index++;
+			}
+
+			var batch = new Batch() {
+				Name = $"{notification.Order.Number} - {notification.Order.Name}",
+				Material = batchMaterial,
+				Parts = parts,
+				LabelFields = new List<LabelField>()
+			};
+
+			batches.Add(batch);
+
+		}
+
+		return batches;
+	}
+
+	private static Part CreateCNCPart(DrawerBoxPart bottom, int index)
+		=> new() {
+			Description = "Drawer Box Bottom",
+			Length = bottom.Width,
+			Width = bottom.Length,
+			ContainsShape = false,
+			Qty = bottom.Qty,
+			LabelFields = new List<LabelField>(),
+			PrimaryFace = new() {
+				FileName = $"Bottom{index}",
+				Operations = Enumerable.Empty<MachiningOperation>()
+			},
+			SecondaryFace = null
 		};
 
-		// TODO send emails to shop manager
-		var response = await _bus.Send(new GenerateGCode.Command(batch, options));
+	private async Task<DrawerBoxMaterial> GetMaterialById(Guid materialId) {
+		var matResponse = await _bus.Send(new GetDrawerBoxMaterialById.Query(materialId));
+		
+		DrawerBoxMaterial material = new(Guid.Empty, "UNKNOWN", Dimension.FromMillimeters(0));
 
-        await response.MatchAsync(
+		matResponse.OnSuccess(m => {
+			if (m is not null) material = m;
+		});
 
-            async result => {
+		matResponse.OnError(error => _logger.LogWarning("Unrecognized drawer box material id {materialId}", materialId));
 
-                var job = new GCodeToReleasedJobConverter().ConvertResult(result, batch.Name, batch.Parts);
-
-                var pdfResponse = await _bus.Send(new GenerateCNCReleasePDF.Command(job, notification.ReleaseProfile.CNCReportOutputDirectory));
-                pdfResponse.Match(
-                    pdfResult => {
-						foreach (var file in pdfResult.FilePaths)
-							_uibus.Publish(new OrderReleaseSuccessNotification($"CNC job report created {file}"));
-					},
-                    error => {
-						_uibus.Publish(new OrderReleaseErrorNotification($"{error.Title} - {error.Details}"));
-					}
-                );
-
-            },
-            error => {
-                _uibus.Publish(new OrderReleaseErrorNotification($"{error.Title} - {error.Details}"));
-                return Task.CompletedTask;
-            }
-        );
-
-    }
-
+		return material;
+	}
 }
