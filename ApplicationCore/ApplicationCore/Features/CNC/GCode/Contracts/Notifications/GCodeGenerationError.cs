bfe9dfc3c3e5b416f6bbf63dae099362ed3bddb8
ZachLondono <zachary.londono5@gmail.com>
2022-12-27 16:55:34 -0500

    Added progress and error notifications

---
+++ ApplicationCore/ApplicationCore/Features/CNC/GCode/Contracts/Notifications/GCodeGenerationError.cs
@@ -0,0 +1,5 @@
+﻿using ApplicationCore.Infrastructure;
+
+namespace ApplicationCore.Features.CNC.GCode.Contracts.Notifications;
+
+public record GCodeGenerationError(string Message) : IUINotification;
---
+++ ApplicationCore/ApplicationCore/Features/CNC/GCode/Contracts/Notifications/GCodeGenerationProgress.cs
@@ -0,0 +1,5 @@
+﻿using ApplicationCore.Infrastructure;
+
+namespace ApplicationCore.Features.CNC.GCode.Contracts.Notifications;
+
+public record GCodeGenerationProgress(double PercentComplete, string Message) : IUINotification;
