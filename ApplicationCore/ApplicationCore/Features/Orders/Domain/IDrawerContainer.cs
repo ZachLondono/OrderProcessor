620fe24ead941ccc605ce237cd1947e47f63e614
ZachLondono <zachary.londono5@gmail.com>
2022-12-28 13:22:04 -0500

    Created an interface which products can extend if they contain drawer boxes

---
+++ ApplicationCore/ApplicationCore/Features/Orders/Domain/IDrawerContainer.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplicationCore.Features.Orders.Domain;
+
+/// <summary>
+/// An object which contains drawer boxes
+/// </summary>
+internal interface IDrawerContainer {
+
+    IEnumerable<DrawerBox> GetDrawerBoxes();
+
+}
