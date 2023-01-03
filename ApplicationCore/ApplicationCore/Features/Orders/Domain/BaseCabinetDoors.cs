070165413098755dd0896538e8dc7e485c188e7f
ZachLondono <zachary.londono5@gmail.com>
2023-01-03 16:18:44 -0500

    Added optional MDF options to base cabinet doors

--- ApplicationCore/ApplicationCore/Features/Orders/Domain/BaseCabinetDoors.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Domain/BaseCabinetDoors.cs
@@ -1,18 +1,23 @@
-﻿namespace ApplicationCore.Features.Orders.Domain;
+﻿using ApplicationCore.Features.Orders.Domain.ValueObjects;
+
+namespace ApplicationCore.Features.Orders.Domain;
 
 public record BaseCabinetDoors {
 
     public int Quantity { get; init; }
     public HingeSide HingeSide { get; init; } = HingeSide.NotApplicable;
-    
-    public BaseCabinetDoors() {
+    public MDFDoorOptions? MDFOptions { get; init; }
+
+    public BaseCabinetDoors(MDFDoorOptions? mdfOptions = null) {
         Quantity = 2;
         HingeSide = HingeSide.NotApplicable;
+        MDFOptions = mdfOptions;
     }
 
-    public BaseCabinetDoors(HingeSide hingeSide) {
+    public BaseCabinetDoors(HingeSide hingeSide, MDFDoorOptions? mdfOptions = null) {
         Quantity = 1;
         HingeSide = hingeSide;
+        MDFOptions = mdfOptions;
     }
 
 }
