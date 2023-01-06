3f2e7016cdab87847f9f05b064ce2bf7458418b7
ZachLondono <zachary.londono5@gmail.com>
2023-01-06 13:02:16 -0500

    Added tall cabinet product

---
+++ ApplicationCore/ApplicationCore/Features/Orders/Domain/TallCabinetDoors.cs
@@ -0,0 +1,27 @@
+﻿using ApplicationCore.Features.Orders.Domain.ValueObjects;
+
+namespace ApplicationCore.Features.Orders.Domain;
+
+public record TallCabinetDoors {
+
+    public int UpperQuantity { get; init; }
+    public int LowerQuantity { get; init; }
+    public HingeSide HingeSide { get; init; } = HingeSide.NotApplicable;
+    public MDFDoorOptions? MDFOptions { get; init; }
+
+    public TallCabinetDoors(bool twoSections, HingeSide hingeSide = HingeSide.NotApplicable, MDFDoorOptions? mdfOptions = null) {
+        
+        if (hingeSide == HingeSide.NotApplicable) { 
+            UpperQuantity = twoSections ? 2 : 0;
+            LowerQuantity = 2;
+        } else {
+            UpperQuantity = twoSections ? 1 : 0;
+            LowerQuantity = 1;
+        }
+
+        HingeSide = hingeSide;
+        MDFOptions = mdfOptions;
+
+    }
+
+}
