79be52a78cd17497dc573e10eccfcb90d3ca5bbd
ZachLondono <zachary.londono5@gmail.com>
2023-01-06 13:09:47 -0500

    Updated to specify lower door height

--- ApplicationCore/ApplicationCore/Features/Orders/Domain/TallCabinetDoors.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Domain/TallCabinetDoors.cs
@@ -6,19 +6,36 @@ public record TallCabinetDoors {
 
     public int UpperQuantity { get; init; }
     public int LowerQuantity { get; init; }
+    public double LowerDoorHeight { get; init; }
     public HingeSide HingeSide { get; init; } = HingeSide.NotApplicable;
     public MDFDoorOptions? MDFOptions { get; init; }
 
-    public TallCabinetDoors(bool twoSections, HingeSide hingeSide = HingeSide.NotApplicable, MDFDoorOptions? mdfOptions = null) {
+    public TallCabinetDoors(HingeSide hingeSide = HingeSide.NotApplicable, MDFDoorOptions? mdfOptions = null) {
         
         if (hingeSide == HingeSide.NotApplicable) { 
-            UpperQuantity = twoSections ? 2 : 0;
+            UpperQuantity = 0;
             LowerQuantity = 2;
         } else {
-            UpperQuantity = twoSections ? 1 : 0;
+            UpperQuantity = 0;
             LowerQuantity = 1;
         }
 
+        HingeSide = hingeSide;
+        MDFOptions = mdfOptions;
+
+    }
+
+    public TallCabinetDoors(double lowerDoorHeight, HingeSide hingeSide = HingeSide.NotApplicable, MDFDoorOptions? mdfOptions = null) {
+
+        if (hingeSide == HingeSide.NotApplicable) {
+            UpperQuantity = 2;
+            LowerQuantity = 2;
+        } else {
+            UpperQuantity = 1;
+            LowerQuantity = 1;
+        }
+
+        LowerDoorHeight = lowerDoorHeight;
         HingeSide = hingeSide;
         MDFOptions = mdfOptions;
 
