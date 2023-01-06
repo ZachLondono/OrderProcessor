7b217b994e815ee4595adb88615011475c8cd5b6
ZachLondono <zachary.londono5@gmail.com>
2023-01-06 14:23:46 -0500

    Added properties for position 4 & 5 from XML

--- ApplicationCore/ApplicationCore/Features/Orders/Loader/Providers/AllmoxyXMLModels/RollOuts.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Loader/Providers/AllmoxyXMLModels/RollOuts.cs
@@ -13,6 +13,12 @@ public class RollOuts {
     [XmlElement("rollOutPos3")]
     public string Pos3 { get; set; } = string.Empty;
 
+    [XmlElement("rollOutPos4")]
+    public string Pos4 { get; set; } = string.Empty;
+
+    [XmlElement("rollOutPos5")]
+    public string Pos5 { get; set; } = string.Empty;
+
     [XmlElement("rollOutBlocks")]
     public string Blocks { get; set; } = string.Empty;
 
