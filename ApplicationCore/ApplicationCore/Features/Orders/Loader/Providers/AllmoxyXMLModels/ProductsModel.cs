d2ab40dfb006fe2945a05007f5afc6d2620f4f24
ZachLondono <zachary.londono5@gmail.com>
2023-01-06 13:23:05 -0500

    Added tall cabinets to order data models

--- ApplicationCore/ApplicationCore/Features/Orders/Loader/Providers/AllmoxyXMLModels/ProductsModel.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Loader/Providers/AllmoxyXMLModels/ProductsModel.cs
@@ -13,6 +13,9 @@ public class ProductsModel {
     [XmlElement("wallCabinet")]
     public WallCabinetModel[] WallCabinets { get; set; } = Array.Empty<WallCabinetModel>();
 
+    [XmlElement("tallCabinet")]
+    public TallCabinetModel[] TallCabinets { get; set; } = Array.Empty<TallCabinetModel>();
+
     /*[XmlArrayItem(typeof(DrawerBoxModel), ElementName="drawerBox")]
 	public List<DrawerBoxModel> DrawerBoxes { get; set; } = new();*/
 
--- ApplicationCore/ApplicationCore/Features/Orders/Loader/Providers/DTO/OrderData.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Loader/Providers/DTO/OrderData.cs
@@ -32,6 +32,8 @@ public record OrderData {
     
     public List<DrawerBaseCabinetData> DrawerBaseCabinets { get; set; } = new();
 
+    public List<TallCabinetData> TallCabinets { get; set; } = new();
+
     public List<AdditionalItemData> AdditionalItems { get; set; } = new();
 
 }
