8b099e0ed08e4b9dfb419baf15331f9c5cd26872
ZachLondono <zachary.londono5@gmail.com>
2023-01-11 21:05:46 -0500

    Added pie cut corner cabinet

---
+++ ApplicationCore/ApplicationCore/Features/Orders/Domain/Products/PieCutCornerCabinet.cs
@@ -0,0 +1,78 @@
+﻿using ApplicationCore.Features.Orders.Domain.ValueObjects;
+using ApplicationCore.Features.ProductPlanner.Contracts;
+using ApplicationCore.Shared.Domain;
+
+namespace ApplicationCore.Features.Orders.Domain.Products;
+
+internal class PieCutCornerCabinet : Cabinet, IPPProductContainer, IDoorContainer {
+
+    public Dimension RightWidth { get; }
+    public Dimension RightDepth { get; }
+    public IToeType ToeType { get; }
+    public int AdjustableShelves { get; }
+    public HingeSide HingeSide { get; }
+    public MDFDoorOptions? MDFOptions { get; }
+
+    public PieCutCornerCabinet(Guid id, int qty, decimal unitPrice, string room, bool assembled,
+                        Dimension height, Dimension width, Dimension depth,
+                        CabinetMaterial boxMaterial, CabinetMaterial finishMaterial, string edgeBandingColor,
+                        CabinetSide rightSide, CabinetSide leftSide,
+                        Dimension rightWidth, Dimension rightDepth, IToeType toeType, int adjustableShelves, HingeSide hingeSide, MDFDoorOptions? mDFDoorOptions)
+                        : base(id, qty, unitPrice, room, assembled, height, width, depth, boxMaterial, finishMaterial, edgeBandingColor, rightSide, leftSide) {
+        RightWidth = rightWidth;
+        RightDepth = rightDepth;
+        ToeType = toeType;
+        AdjustableShelves = adjustableShelves;
+        HingeSide = hingeSide;
+        MDFOptions = mDFDoorOptions;
+    }
+
+    public IEnumerable<MDFDoor> GetDoors() {
+        if (MDFOptions is null) return Enumerable.Empty<MDFDoor>();
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<PPProduct> GetPPProducts() {
+        string doorType = (MDFOptions is null) ? "Slab" : "Buyout";
+        yield return new PPProduct(Room, "BCPC", "Royal2", GetMaterialType(), doorType, "Standard", GetFinishMaterials(), GetEBMaterials(), GetParameters(), GetOverrideParameters());
+    }
+
+    private Dictionary<string, string> GetParameters() {
+        var parameters = new Dictionary<string, string>() {
+            { "ProductW", Width.AsMillimeters().ToString() },
+            { "ProductWRight", RightWidth.AsMillimeters().ToString() },
+            { "ProductH", Height.AsMillimeters().ToString() },
+            { "ProductD", Depth.AsMillimeters().ToString() },
+            { "ProductDRight", RightDepth.AsMillimeters().ToString() },
+            { "FinishedLeft", GetSideOption(LeftSide.Type) },
+            { "FinishedRight", GetSideOption(RightSide.Type) },
+            { "ShelfQ", AdjustableShelves.ToString() },
+            { "AppliedPanel", GetAppliedPanelOption() },
+        };
+
+        if (HingeSide != HingeSide.NotApplicable) {
+            parameters.Add("HingeLeft", GetHingeSideOption());
+        }
+
+        return parameters;
+    }
+
+    private string GetHingeSideOption() => HingeSide switch {
+        HingeSide.NotApplicable => "0",
+        HingeSide.Left => "1",
+        HingeSide.Right => "0",
+        _ => "0"
+    };
+
+    private Dictionary<string, string> GetOverrideParameters() {
+
+        var parameters = new Dictionary<string, string>();
+        if (ToeType.PSIParameter != "2") {
+            parameters.Add("__ToeBaseType", ToeType.PSIParameter);
+        }
+
+        return parameters;
+
+    }
+
+}
