6ef05e79511010cda8d8ae7330e42c2a7b7ae138
ZachLondono <zachary.londono5@gmail.com>
2023-01-16 14:03:28 -0500

    Added service for validating XML against a schema

---
+++ ApplicationCore/ApplicationCore/Features/Orders/Loader/XMLValidation/IXMLValidator.cs
@@ -0,0 +1,7 @@
+﻿namespace ApplicationCore.Features.Orders.Loader.XMLValidation;
+
+public interface IXMLValidator {
+
+    public IEnumerable<XMLValidationError> ValidateXML(Stream xmlDataStream, string schemaFilePath);
+
+}
