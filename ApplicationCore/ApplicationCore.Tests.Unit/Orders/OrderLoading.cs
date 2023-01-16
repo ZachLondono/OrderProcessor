f176fc11d56a16e905f8ba305beaad25451bbdcc
ZachLondono <zachary.londono5@gmail.com>
2023-01-16 12:31:41 -0500

    Added test to make sure a provider exists for each provider type

---
+++ ApplicationCore/ApplicationCore.Tests.Unit/Orders/OrderLoading.cs
@@ -0,0 +1,37 @@
+﻿using ApplicationCore.Features.Orders.Loader;
+using ApplicationCore.Features.Orders.Loader.Providers;
+using ApplicationCore.Features.Orders.Providers;
+using FluentAssertions;
+using NSubstitute;
+
+namespace ApplicationCore.Tests.Unit.Orders;
+
+public class OrderProviderFactoryTest {
+
+    private readonly OrderProviderFactory _sut;
+    private readonly IServiceProvider _serviceProvider = Substitute.For<IServiceProvider>();
+
+    public OrderProviderFactoryTest() {
+
+        _sut = new(_serviceProvider);
+
+    }
+
+    [Fact]
+    public void Should_NotThrowException_ForAnySourceType() {
+
+        var provider = Substitute.For<OrderProvider>();
+
+        foreach (var sourceType in Enum.GetValues(typeof(OrderSourceType))) {
+
+            // Act
+            var action = () => _sut.GetOrderProvider((OrderSourceType) sourceType);
+
+            // Assert
+            action.Should().NotThrow<KeyNotFoundException>();
+
+        }
+
+    }
+
+}
