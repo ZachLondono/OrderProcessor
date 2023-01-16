7d4c09a58c01779e5e6eead447736c467f015a3c
ZachLondono <zachary.londono5@gmail.com>
2023-01-15 23:08:34 -0500

    Created MDF door builder

---
+++ ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/Builders/MDFDoorConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationCore.Features.Shared.Domain;
+
+namespace ApplicationCore.Features.Orders.Shared.Domain.Builders;
+
+internal class MDFDoorConfiguration {
+
+    public required string Material { get; init; }
+    public required string FramingBead { get; init; }
+    public required string EdgeDetail { get; init; }
+    public required Dimension TopRail { get; init; }
+    public required Dimension BottomRail { get; init; }
+    public required Dimension LeftStile { get; init; }
+    public required Dimension RightStile { get; init; }
+
+}
