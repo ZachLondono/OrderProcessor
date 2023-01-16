76f797a673f1cbf4d7f320df11606ece00b5be76
ZachLondono <zachary.londono5@gmail.com>
2023-01-15 23:08:46 -0500

    created product builder factory

---
+++ ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/Builders/ProductBuilderFactory.cs
@@ -0,0 +1,23 @@
+﻿using ApplicationCore.Features.Shared.Domain;
+
+namespace ApplicationCore.Features.Orders.Shared.Domain.Builders;
+
+internal class ProductBuilderFactory {
+
+    public MDFDoorBuilder CreateMDFDoorBuilder() {
+
+        MDFDoorConfiguration configuration = new() {
+            Material = "MDF-3/4\"",
+            FramingBead = "Shaker",
+            EdgeDetail = "Eased",
+            TopRail = Dimension.Zero,
+            BottomRail = Dimension.Zero,
+            LeftStile = Dimension.Zero,
+            RightStile = Dimension.Zero,
+        };
+
+        return new MDFDoorBuilder(configuration);
+
+    }
+
+}
