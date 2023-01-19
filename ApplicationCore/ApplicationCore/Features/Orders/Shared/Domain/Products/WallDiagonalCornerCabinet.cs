7dbacbe1c3dc525edbd0c62efc8f6770bbab4ffc
ZachLondono <zachary.londono5@gmail.com>
2023-01-19 14:47:58 -0500

    Added wall version of corner cabinets

---
+++ ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/Products/WallDiagonalCornerCabinet.cs
@@ -0,0 +1,90 @@
+﻿using ApplicationCore.Features.Orders.Shared.Domain.Builders;
+using ApplicationCore.Features.Orders.Shared.Domain.Enums;
+using ApplicationCore.Features.Orders.Shared.Domain.ValueObjects;
+using ApplicationCore.Features.ProductPlanner.Contracts;
+using ApplicationCore.Features.Shared.Domain;
+
+namespace ApplicationCore.Features.Orders.Shared.Domain.Products;
+
+internal class WallDiagonalCornerCabinet : Cabinet, IPPProductContainer, IDoorContainer {
+
+    public Dimension RightWidth { get; }
+    public Dimension RightDepth { get; }
+    public HingeSide HingeSide { get; }
+    public int DoorQty { get; }
+    public MDFDoorOptions? MDFOptions { get; }
+    public int AdjustableShelves { get; }
+
+    public WallDiagonalCornerCabinet(Guid id, int qty, decimal unitPrice, string room, bool assembled,
+                        Dimension height, Dimension width, Dimension depth,
+                        CabinetMaterial boxMaterial, CabinetMaterial finishMaterial, string edgeBandingColor,
+                        CabinetSide rightSide, CabinetSide leftSide,
+                        Dimension rightWidth, Dimension rightDepth, int adjShelfQty, HingeSide hingeSide, int doorQty, MDFDoorOptions? mdfOptions)
+                        : base(id, qty, unitPrice, room, assembled, height, width, depth, boxMaterial, finishMaterial, edgeBandingColor, rightSide, leftSide) {
+
+        if (leftSide.Type == CabinetSideType.AppliedPanel || rightSide.Type == CabinetSideType.AppliedPanel)
+            throw new InvalidOperationException("Wall cabinet cannot have applied panel sides");
+
+        RightWidth = rightWidth;
+        RightDepth = rightDepth;
+        AdjustableShelves = adjShelfQty;
+        HingeSide = hingeSide;
+        DoorQty = doorQty;
+        MDFOptions = mdfOptions;
+
+        if (DoorQty > 2 || DoorQty < 1) throw new InvalidOperationException("Invalid number of doors");
+
+    }
+
+    public static WallDiagonalCornerCabinet Create(int qty, decimal unitPrice, string room, bool assembled,
+                        Dimension height, Dimension width, Dimension depth,
+                        CabinetMaterial boxMaterial, CabinetMaterial finishMaterial, string edgeBandingColor,
+                        CabinetSide rightSide, CabinetSide leftSide,
+                        Dimension rightWidth, Dimension rightDepth, int adjShelfQty, HingeSide hingeSide, int doorQty, MDFDoorOptions? mdfOptions)
+                        => new(Guid.NewGuid(), qty, unitPrice, room, assembled, height, width, depth, boxMaterial, finishMaterial, edgeBandingColor, rightSide, leftSide, rightWidth, rightDepth, adjShelfQty, hingeSide, doorQty, mdfOptions);
+
+    public IEnumerable<MDFDoor> GetDoors(Func<MDFDoorBuilder> getBuilder) {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<PPProduct> GetPPProducts() {
+        string doorType = (MDFOptions is null) ? "Slab" : "Buyout";
+
+        string sku = DoorQty switch {
+            1 => "WC1D-M",
+            2 => "WC2D-M",
+            _ => "WC1D-M"
+        };
+
+        yield return new PPProduct(Room, sku, "Royal2", GetMaterialType(), doorType, "Standard", GetFinishMaterials(), GetEBMaterials(), GetParameters(), new(), new());
+    }
+
+    private Dictionary<string, string> GetParameters() {
+        var parameters = new Dictionary<string, string>() {
+            { "ProductW", Width.AsMillimeters().ToString() },
+            { "ProductWRight", RightWidth.AsMillimeters().ToString() },
+            { "ProductH", Height.AsMillimeters().ToString() },
+            { "ProductD", Depth.AsMillimeters().ToString() },
+            { "ProductDRight", RightDepth.AsMillimeters().ToString() },
+            { "FinishedLeft", GetSideOption(LeftSide.Type) },
+            { "FinishedRight", GetSideOption(RightSide.Type) },
+            { "ShelfQ", AdjustableShelves.ToString() },
+            { "AppliedPanel", GetAppliedPanelOption() },
+            { "LazySusan", "N" },
+        };
+
+        if (HingeSide != HingeSide.NotApplicable) {
+            parameters.Add("HingeLeft", GetHingeSideOption());
+        }
+
+        return parameters;
+    }
+
+    private string GetHingeSideOption() => HingeSide switch {
+        HingeSide.NotApplicable => "0",
+        HingeSide.Left => "1",
+        HingeSide.Right => "0",
+        _ => "0"
+    };
+
+}
---
+++ ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/Products/WallPieCutCornerCabinet.cs
@@ -0,0 +1,77 @@
+﻿using ApplicationCore.Features.Orders.Shared.Domain.Builders;
+using ApplicationCore.Features.Orders.Shared.Domain.Enums;
+using ApplicationCore.Features.Orders.Shared.Domain.ValueObjects;
+using ApplicationCore.Features.ProductPlanner.Contracts;
+using ApplicationCore.Features.Shared.Domain;
+
+namespace ApplicationCore.Features.Orders.Shared.Domain.Products;
+
+internal class WallPieCutCornerCabinet : Cabinet, IPPProductContainer, IDoorContainer {
+
+    public Dimension RightWidth { get; }
+    public Dimension RightDepth { get; }
+    public int AdjustableShelves { get; }
+    public HingeSide HingeSide { get; }
+    public MDFDoorOptions? MDFOptions { get; }
+
+    public WallPieCutCornerCabinet(Guid id, int qty, decimal unitPrice, string room, bool assembled,
+                        Dimension height, Dimension width, Dimension depth,
+                        CabinetMaterial boxMaterial, CabinetMaterial finishMaterial, string edgeBandingColor,
+                        CabinetSide rightSide, CabinetSide leftSide,
+                        Dimension rightWidth, Dimension rightDepth, int adjustableShelves, HingeSide hingeSide, MDFDoorOptions? mdfDoorOptions)
+                        : base(id, qty, unitPrice, room, assembled, height, width, depth, boxMaterial, finishMaterial, edgeBandingColor, rightSide, leftSide) {
+
+        if (leftSide.Type == CabinetSideType.AppliedPanel || rightSide.Type == CabinetSideType.AppliedPanel)
+            throw new InvalidOperationException("Wall cabinet cannot have applied panel sides");
+
+        RightWidth = rightWidth;
+        RightDepth = rightDepth;
+        AdjustableShelves = adjustableShelves;
+        HingeSide = hingeSide;
+        MDFOptions = mdfDoorOptions;
+    }
+
+    public static WallPieCutCornerCabinet Create(int qty, decimal unitPrice, string room, bool assembled,
+                        Dimension height, Dimension width, Dimension depth,
+                        CabinetMaterial boxMaterial, CabinetMaterial finishMaterial, string edgeBandingColor,
+                        CabinetSide rightSide, CabinetSide leftSide,
+                        Dimension rightWidth, Dimension rightDepth, int adjustableShelves, HingeSide hingeSide, MDFDoorOptions? mdfDoorOptions)
+    => new(Guid.NewGuid(), qty, unitPrice, room, assembled, height, width, depth, boxMaterial, finishMaterial, edgeBandingColor, rightSide, leftSide, rightWidth, rightDepth, adjustableShelves, hingeSide, mdfDoorOptions);
+
+    public IEnumerable<MDFDoor> GetDoors(Func<MDFDoorBuilder> getBuilder) {
+        if (MDFOptions is null) return Enumerable.Empty<MDFDoor>();
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<PPProduct> GetPPProducts() {
+        string doorType = (MDFOptions is null) ? "Slab" : "Buyout";
+        yield return new PPProduct(Room, "WCPC", "Royal2", GetMaterialType(), doorType, "Standard", GetFinishMaterials(), GetEBMaterials(), GetParameters(), new(), new());
+    }
+
+    private Dictionary<string, string> GetParameters() {
+        var parameters = new Dictionary<string, string>() {
+            { "ProductW", Width.AsMillimeters().ToString() },
+            { "ProductWRight", RightWidth.AsMillimeters().ToString() },
+            { "ProductH", Height.AsMillimeters().ToString() },
+            { "ProductD", Depth.AsMillimeters().ToString() },
+            { "ProductDRight", RightDepth.AsMillimeters().ToString() },
+            { "FinishedLeft", GetSideOption(LeftSide.Type) },
+            { "FinishedRight", GetSideOption(RightSide.Type) },
+            { "ShelfQ", AdjustableShelves.ToString() }
+        };
+
+        if (HingeSide != HingeSide.NotApplicable) {
+            parameters.Add("HingeLeft", GetHingeSideOption());
+        }
+
+        return parameters;
+    }
+
+    private string GetHingeSideOption() => HingeSide switch {
+        HingeSide.NotApplicable => "0",
+        HingeSide.Left => "1",
+        HingeSide.Right => "0",
+        _ => "0"
+    };
+
+}
