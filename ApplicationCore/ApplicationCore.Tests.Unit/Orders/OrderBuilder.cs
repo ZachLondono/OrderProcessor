8cf86e69aac3302e36e7edb92fe84d2b13d2e9dd
ZachLondono <zachary.londono5@gmail.com>
2023-01-18 22:59:29 -0500

    Renamed method

--- ApplicationCore/ApplicationCore.Tests.Unit/Orders/OrderBuilder.cs
+++ ApplicationCore/ApplicationCore.Tests.Unit/Orders/OrderBuilder.cs
@@ -74,7 +74,7 @@ internal class OrderBuilder {
         return this;
     }
 
-    public OrderBuilder WithCustomerId(Customer customer) {
+    public OrderBuilder WithCustomer(Customer customer) {
         _customer = customer;
         return this;
     }
