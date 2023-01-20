85695dd5acefdae430920c2b32385478ec256a41
ZachLondono <zachary.londono5@gmail.com>
2023-01-19 23:44:29 -0500

    Moving cross cutting methods into Page

--- ApplicationCore/ApplicationCore/Features/Orders/Details/CompanyListItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApplicationCore.Features.Orders.Details;
-
-public class CompanyListItem {
-
-    public Guid Id { get; set; }
-
-    public string Name { get; set; } = string.Empty;
-
-}
---
+++ ApplicationCore/ApplicationCore/Features/Orders/Details/IOrderDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Features.Companies.Domain.ValueObjects;
+using ApplicationCore.Features.Orders.Shared.Domain.ValueObjects;
+
+namespace ApplicationCore.Features.Orders.Details;
+
+public interface IOrderDetailsViewModel {
+
+    public Task<string> GetCompanyName(Guid companyId);
+
+    public Task OpenCompanyPage(Guid companyId);
+
+    public Task<ReleaseProfile?> GetVendorReleaseProfile(Guid vendorId);
+
+    public Task<string> ExportOrderForPSI(Order order);
+
+}
