56da0fb0a101c4f22b97990238b622b022314841
ZachLondono <zachary.londono5@gmail.com>
2023-01-21 15:38:54 -0500

    Added blind wall cabinet builder

---
+++ ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/Builders/BlindWallCabinetBuilder.cs
@@ -0,0 +1,46 @@
+﻿using ApplicationCore.Features.Orders.Shared.Domain.Enums;
+using ApplicationCore.Features.Orders.Shared.Domain.Products;
+using ApplicationCore.Features.Orders.Shared.Domain.ValueObjects;
+using ApplicationCore.Features.Shared.Domain;
+
+namespace ApplicationCore.Features.Orders.Shared.Domain.Builders;
+
+internal class BlindWallCabinetBuilder : CabinetBuilder<BlindWallCabinet> {
+
+    public BlindCabinetDoors Doors { get; private set; }
+    public int AdjustableShelves { get; private set; }
+    public BlindSide BlindSide { get; private set; }
+    public Dimension BlindWidth { get; private set; }
+
+    public BlindWallCabinetBuilder() {
+        Doors = new();
+        AdjustableShelves = 0;
+        BlindSide = BlindSide.Left;
+        BlindWidth = Dimension.Zero;
+    }
+
+    public BlindWallCabinetBuilder WithDoors(BlindCabinetDoors doors) {
+        Doors = doors;
+        return this;
+    }
+
+    public BlindWallCabinetBuilder WithAdjustableShelves(int adjShelves) {
+        AdjustableShelves = adjShelves;
+        return this;
+    }
+
+    public BlindWallCabinetBuilder WithBlindSide(BlindSide blindSide) {
+        BlindSide = blindSide;
+        return this;
+    }
+
+    public BlindWallCabinetBuilder WithBlindWidth(Dimension blindWidth) {
+        BlindWidth = blindWidth;
+        return this;
+    }
+
+    public override BlindWallCabinet Build() {
+        return BlindWallCabinet.Create(Qty, UnitPrice, Room, Assembled, Height, Width, Depth, BoxMaterial, FinishMaterial, EdgeBandingColor, RightSide, LeftSide, Doors, BlindSide, BlindWidth, AdjustableShelves);
+    }
+
+}
