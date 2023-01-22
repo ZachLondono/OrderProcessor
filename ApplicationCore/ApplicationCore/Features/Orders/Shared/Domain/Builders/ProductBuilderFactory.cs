c7f9db879e53140f5f847c6b02805cb1d696afbb
ZachLondono <zachary.londono5@gmail.com>
2023-01-22 14:06:28 -0500

    Added methods to create builders in factory

--- ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/Builders/ProductBuilderFactory.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/Builders/ProductBuilderFactory.cs
@@ -20,4 +20,27 @@ public class ProductBuilderFactory {
 
     }
 
+    internal BaseCabinetBuilder CreateBaseCabinetBuilder() => new();
+
+    internal WallCabinetBuilder CreateWallCabinetBuilder() => new();
+
+    internal BaseDiagonalCornerCabinetBuilder CreateBaseDiagonalCornerCabinetBuilder() => new();
+
+    internal BasePieCutCornerCabinetBuilder CreateBasePieCutCornerCabinetBuilder() => new();
+
+    internal BlindBaseCabinetBuilder CreateBlindBaseCabinetBuilder() => new();
+
+    internal BlindWallCabinetBuilder CreateBlindWallCabinetBuilder() => new();
+
+    internal DrawerBaseCabinetBuilder CreateDrawerBaseCabinetBuilder() => new();
+
+    internal SinkCabinetBuilder CreateSinkCabinetBuilder() => new();
+
+    internal TallCabinetBuilder CreateTallCabinetBuilder() => new();
+
+    internal WallDiagonalCornerCabinetBuilder CreateWallDiagonalCornerCabinetBuilder() => new();
+
+    internal WallPieCutCornerCabinetBuilder CreateWallPieCutCornerCabinetBuilder() => new();
+
+
 }
