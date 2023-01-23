070a45865d6938ef5b4871a3f1100168015c7f86
ZachLondono <zachary.londono5@gmail.com>
2023-01-23 15:48:27 -0500

    Added service for navigating to different pages

---
+++ ApplicationCore/ApplicationCore/Features/Shared/NavigationService.cs
@@ -0,0 +1,29 @@
+﻿using ApplicationCore.Features.Companies;
+using ApplicationCore.Features.Orders.Shared.State;
+using Microsoft.AspNetCore.Components;
+
+namespace ApplicationCore.Features.Shared;
+
+public class NavigationService {
+
+    private readonly OrderState _orderState;
+    private readonly CompanyState _companyState;
+    private readonly NavigationManager _navigationManager;
+
+    public NavigationService(OrderState orderState, CompanyState companyState, NavigationManager navigationManager) {
+        _orderState = orderState;
+        _companyState = companyState;
+        _navigationManager = navigationManager;
+    }
+
+    public async Task NavigateToOrderPage(Guid orderId) {
+        await _orderState.LoadOrder(orderId);
+        _navigationManager.NavigateTo("/orders/details", true);
+    }
+
+    public async Task NavigateToCompanyPage(Guid companyId) {
+        await _companyState.LoadCompany(companyId);
+        _navigationManager.NavigateTo("/companies/details", true);
+    }
+
+}
