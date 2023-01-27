325c146f4cc636904a5c3292ce059790f45ba26d
ZachLondono <zachary.londono5@gmail.com>
2023-01-26 23:17:27 -0500

    Made CabinetDoorGaps a readonly struct

--- ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/ValueObjects/CabinetDoorGaps.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/ValueObjects/CabinetDoorGaps.cs
@@ -2,31 +2,33 @@
 
 namespace ApplicationCore.Features.Orders.Shared.Domain.ValueObjects;
 
-internal class CabinetDoorGaps {
+internal readonly struct CabinetDoorGaps {
+    
+    public CabinetDoorGaps() { }
 
     /// <summary>
     /// Gap between the top of a cabinet door/drawer and the top of it's cabinet
     /// </summary>
-    public Dimension TopGap { get; init; } = Dimension.Zero;
+    public required Dimension TopGap { get; init; } = Dimension.Zero;
 
     /// <summary>
     /// Gap between the bottom of a cabinet door and the bottom of it's cabinet
     /// </summary>
-    public Dimension BottomGap { get; init; } = Dimension.Zero;
+    public required Dimension BottomGap { get; init; } = Dimension.Zero;
 
     /// <summary>
     /// Gap between two horizontally adjacent doors/drawers
     /// </summary>
-    public Dimension HorizontalGap { get; init; } = Dimension.Zero;
+    public required Dimension HorizontalGap { get; init; } = Dimension.Zero;
 
     /// <summary>
     /// Gap between two vertically adjacent doors/drawers
     /// </summary>
-    public Dimension VerticalGap { get; init; } = Dimension.Zero;
+    public required Dimension VerticalGap { get; init; } = Dimension.Zero;
 
     /// <summary>
     /// Gap between the left/right edge of a cabinet and the edge of the left/right most door
     /// </summary>
-    public Dimension EdgeReveal { get; init; } = Dimension.Zero;
+    public required Dimension EdgeReveal { get; init; } = Dimension.Zero;
 
 }
