836b781e2bbaf8c3932ff3a5efb3470ab5ac0bee
ZachLondono <zachary.londono5@gmail.com>
2023-01-31 22:51:29 -0500

    Implement product model base class

--- ApplicationCore/ApplicationCore/Features/Orders/Loader/Providers/AllmoxyXMLModels/DrawerBoxModel.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Loader/Providers/AllmoxyXMLModels/DrawerBoxModel.cs
@@ -1,13 +1,19 @@
-﻿using System.Xml.Serialization;
+﻿using ApplicationCore.Features.Orders.Shared.Domain.Builders;
+using ApplicationCore.Features.Orders.Shared.Domain.Products;
+using System.Xml.Serialization;
 
 namespace ApplicationCore.Features.Orders.Loader.Providers.AllmoxyXMLModels;
 
-public class DrawerBoxModel {
+public class DrawerBoxModel : ProductModel {
 
     [XmlElement("qty")]
     public int Qty { get; set; }
 
     [XmlElement("unitPrice")]
     public decimal UnitPrice { get; set; }
+
+    public override IProduct CreateProduct(ProductBuilderFactory builderFactory) {
+        throw new NotImplementedException();
+    }
 
 }
