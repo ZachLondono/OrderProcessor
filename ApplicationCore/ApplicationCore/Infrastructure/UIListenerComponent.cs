090e322895b824382e65b927086b62dbe0bec3f3
ZachLondono <zachary.londono5@gmail.com>
2023-02-02 23:12:26 -0500

    Added abstract class that implments interface so components dont need to implement interface and extend class

---
+++ ApplicationCore/ApplicationCore/Infrastructure/UIListenerComponent.cs
@@ -0,0 +1,7 @@
+﻿namespace ApplicationCore.Infrastructure;
+
+public abstract class UIListenerComponent<TNotification> : BaseListenerComponent, IUIListener<TNotification> where TNotification : IUINotification {
+    
+    public abstract void Handle(TNotification notification);
+
+}
