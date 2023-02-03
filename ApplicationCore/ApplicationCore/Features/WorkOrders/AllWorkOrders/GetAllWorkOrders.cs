8524a474f7e055ac41a05e77004b2cdaefd253a3
ZachLondono <zachary.londono5@gmail.com>
2023-02-03 17:11:03 -0500

    Added component which lists all work orders

---
+++ ApplicationCore/ApplicationCore/Features/WorkOrders/AllWorkOrders/GetAllWorkOrders.cs
@@ -0,0 +1,50 @@
+﻿using ApplicationCore.Features.Shared;
+using ApplicationCore.Infrastructure;
+using ApplicationCore.Infrastructure.Data;
+using Dapper;
+
+namespace ApplicationCore.Features.WorkOrders.AllWorkOrders;
+
+public class GetAllWorkOrders {
+
+    public record Query() : ICommand<IEnumerable<Model>>;
+
+    public class Handler : CommandHandler<Query, IEnumerable<Model>> {
+
+        private readonly IDbConnectionFactory _factory;
+        private readonly Ordering.GetOrderNumberById _getOrderNumberById;
+
+        public Handler(IDbConnectionFactory factory, Ordering.GetOrderNumberById getOrderNumberById) {
+            _factory = factory;
+            _getOrderNumberById = getOrderNumberById;
+        }
+
+        public override async Task<Response<IEnumerable<Model>>> Handle(Query command) {
+
+            using var connection = _factory.CreateConnection();
+
+            var workorders = await connection.QueryAsync<Model>(@"SELECT id, order_id As OrderId, name, status FROM work_orders");
+
+            foreach (var wo in workorders) {
+
+                wo.OrderNumber = await _getOrderNumberById(wo.OrderId);
+
+            }
+
+            return Response<IEnumerable<Model>>.Success(workorders);
+
+        }
+
+    }
+
+    public class Model {
+
+        public Guid Id { get; set; }
+        public Guid OrderId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public Status Status { get; set; }
+        public string OrderNumber { get; set; } = string.Empty;
+
+    }
+
+}
