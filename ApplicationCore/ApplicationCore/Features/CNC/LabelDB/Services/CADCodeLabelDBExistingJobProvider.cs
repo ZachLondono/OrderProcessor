9338610db04d3330560c897634ec3452b7c3974f
ZachLondono <zachary.londono5@gmail.com>
2023-02-04 13:09:38 -0500

    Added part class to manufactured part record

--- ApplicationCore/ApplicationCore/Features/CNC/LabelDB/Services/CADCodeLabelDBExistingJobProvider.cs
+++ ApplicationCore/ApplicationCore/Features/CNC/LabelDB/Services/CADCodeLabelDBExistingJobProvider.cs
@@ -1,6 +1,8 @@
 ﻿using ApplicationCore.Features.CNC.LabelDB.Contracts;
 using ApplicationCore.Features.Shared;
 using Dapper;
+using System.Data;
+using System.Data.OleDb;
 
 namespace ApplicationCore.Features.CNC.LabelDB.Services;
 
@@ -23,11 +25,11 @@ internal class CADCodeLabelDBExistingJobProvider : IExistingJobProvider {
         var inventory = await connection.QueryAsync<UsedInventory>($"SELECT [Sheets Used] As Qty, Description AS Name, Width, Length, Thickness, Graining As Grained FROM [Inventory:{jobName}];");
 
         connection.Open();
-        var schema = await connection.GetSchemaAsync("COLUMNS");
+        DataTable? schema = await connection.GetSchemaAsync("COLUMNS");
         connection.Close();
-        bool containsId = schema?.Select($"TABLE_NAME='{jobName}' AND COLUMN_NAME='ProductId'").Length > 0;
 
-        string subQuery = containsId ? $"(SELECT TOP 1 ProductId FROM [{jobName}] WHERE cStr([{jobName}].[GlobalId]) = cStr([Label Sequence:{jobName}].[GlobalId])) As ProductId" : "\"0\" As ProductId";
+        string productIdSubQuery = GetProductIdSubQuery(jobName, schema);
+        string productClassSubQuery = GetPartClassSubQuery(jobName, schema);
 
         var parts = await connection.QueryAsync<ManufacturedPart>(
             @$"SELECT
@@ -40,7 +42,8 @@ internal class CADCodeLabelDBExistingJobProvider : IExistingJobProvider {
 			    YDimension As InsertY,
                 (SELECT TOP 1 [Cabinet Number] FROM [{jobName}] WHERE cStr([{jobName}].[GlobalId]) = cStr([Label Sequence:{jobName}].[GlobalId])) As ProductNumber,
                 (Face6Part = 0) AS HasFace6,
-                {subQuery}
+                {productIdSubQuery},
+                {productClassSubQuery}
 		    From [Label Sequence:{jobName}]");
 
         var patterns = await connection.QueryAsync<Pattern>($"SELECT [PatternFilename] AS Name, [PatternFilename] AS ImagePath, Material AS MaterialName, [Panel Width] AS MaterialWidth, [Panel Length] AS MaterialLength, [Thickness] As MaterialThickness  FROM [Label Sequence:{jobName}]");
@@ -53,6 +56,34 @@ internal class CADCodeLabelDBExistingJobProvider : IExistingJobProvider {
             Patterns: patterns,
             Parts: parts
         );
+
+    }
+
+    private static string GetProductIdSubQuery(string jobName, DataTable? schema) {
+        
+        bool containsId = schema?.Select($"TABLE_NAME='{jobName}' AND COLUMN_NAME='ProductId'").Length > 0;
+
+        if (containsId) {
+
+            return $"(SELECT TOP 1 ProductId FROM [{jobName}] WHERE cStr([{jobName}].[GlobalId]) = cStr([Label Sequence:{jobName}].[GlobalId])) As ProductId";
+
+        } 
+       
+        return "\"0\" As ProductId";
+
+    }
+
+    private static string GetPartClassSubQuery(string jobName, DataTable? schema) {
+
+        bool containsId = schema?.Select($"TABLE_NAME='{jobName}' AND COLUMN_NAME='PartClass'").Length > 0;
+
+        if (containsId) {
+
+            return $"(SELECT TOP 1 PartClass FROM [{jobName}] WHERE cStr([{jobName}].[GlobalId]) = cStr([Label Sequence:{jobName}].[GlobalId])) As PartClass";
+
+        }
+
+        return "\"Unknown\" As PartClass";
 
     }
 
