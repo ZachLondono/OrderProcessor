a20dbf87990a9cf2248fa80f2bbd907c6068d858
ZachLondono <zachary.londono5@gmail.com>
2023-02-04 14:08:28 -0500

    Code formatting

--- ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/Extensions.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/Extensions.cs
@@ -7,12 +7,20 @@ public static class Extensions {
 
         var fraction = dimension.AsInchFraction();
 
-        if (fraction.N == 0) return "0";
+        if (fraction.N == 0) {
+            return "0";
+        }
+        
         int whole = fraction.N / fraction.D;
         int n = fraction.N - (whole * fraction.D);
-        if (whole == 0) return $"<span class=\"diagonal-fractions\">{n}/{fraction.D}</span>";
-        else if (n != 0) return $"{whole} <span class=\"diagonal-fractions\">{n}/{fraction.D}</span>";
-        else return whole.ToString();
+
+        if (whole == 0) {
+            return $"<span class=\"diagonal-fractions\">{n}/{fraction.D}</span>";
+        } else if (n != 0) {
+            return $"{whole} <span class=\"diagonal-fractions\">{n}/{fraction.D}</span>";
+        } else {
+            return whole.ToString();
+        }
 
     }
 
