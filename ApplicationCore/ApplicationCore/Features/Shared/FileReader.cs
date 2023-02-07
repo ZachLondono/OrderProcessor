9b4a50e93d6ee5509399a5520f98238397599245
ZachLondono <zachary.londono5@gmail.com>
2023-02-07 12:07:22 -0500

    Added method to file reader interface

--- ApplicationCore/ApplicationCore/Features/Shared/FileReader.cs
+++ ApplicationCore/ApplicationCore/Features/Shared/FileReader.cs
@@ -4,6 +4,26 @@ public class FileReader : IFileReader {
 
     public bool DoesFileExist(string filePath) => File.Exists(filePath);
 
+    public string GetAvailableFileName(string direcotry, string filename, string fileExtension = "") {
+        
+        int index = 1;
+
+        foreach (char c in Path.GetInvalidFileNameChars()) {
+            filename = filename.Replace(c, '_');
+        }
+
+        string filepath = Path.Combine(direcotry, $"{filename}{(fileExtension == string.Empty ? "" : $".{fileExtension}")}");
+
+        while (DoesFileExist(filepath)) {
+
+            filepath = Path.Combine(direcotry, $"{filename} ({index++}).{fileExtension}");
+
+        }
+
+        return filepath;
+
+    }
+
     public Stream OpenReadFileStream(string filepath, FileAccess access = FileAccess.Read) => new FileStream(filepath, FileMode.Open, access, FileShare.ReadWrite);
 
     public async Task<string> ReadFileContentsAsync(string filePath) => await File.ReadAllTextAsync(filePath);
