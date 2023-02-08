7ed4ea66e276a9bf57057521c7b6c1751f49ae37
ZachLondono <zachary.londono5@gmail.com>
2023-02-08 16:37:26 -0500

    Implemented invoice for all product types

--- ApplicationCore/ApplicationCore/Features/Orders/Release/Handlers/Invoice/InvoiceHandler.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Release/Handlers/Invoice/InvoiceHandler.cs
@@ -1,14 +1,10 @@
 ﻿using ApplicationCore.Features.Companies.Queries;
-using ApplicationCore.Features.ExcelTemplates.Contracts;
-using ApplicationCore.Features.ExcelTemplates.Domain;
-using ApplicationCore.Features.Orders.Shared.Domain.Products;
-using ApplicationCore.Features.Orders.Release.Handlers.Invoice.Models;
 using ApplicationCore.Infrastructure;
 using ApplicationCore.Features.Shared;
-using ClosedXML.Excel;
 using Microsoft.Extensions.Logging;
 using Company = ApplicationCore.Features.Companies.Domain.Company;
-using ApplicationCore.Features.Orders.Shared.Domain.Enums;
+using ApplicationCore.Features.Orders.Shared.Domain.Entities;
+using ApplicationCore.Features.Orders.Shared.Domain.Products;
 
 namespace ApplicationCore.Features.Orders.Release.Handlers.Invoice;
 
@@ -17,25 +13,49 @@ internal class InvoiceHandler : DomainListener<TriggerOrderReleaseNotification> 
     private readonly ILogger<InvoiceHandler> _logger;
     private readonly IBus _bus;
     private readonly IUIBus _uibus;
-    private readonly IFileReader _reader;
+    private readonly IFileReader _fileReader;
 
-    public InvoiceHandler(ILogger<InvoiceHandler> logger, IBus bus, IUIBus uibus, IFileReader reader) {
+    public InvoiceHandler(ILogger<InvoiceHandler> logger, IBus bus, IUIBus uibus, IFileReader fileReader) {
         _logger = logger;
         _bus = bus;
         _uibus = uibus;
-        _reader = reader;
+        _fileReader = fileReader;
     }
 
     public override async Task Handle(TriggerOrderReleaseNotification notification) {
 
-        /*if (!notification.ReleaseProfile.GenerateInvoice) {
+        if (!notification.ReleaseProfile.GenerateInvoice) {
             _uibus.Publish(new OrderReleaseInfoNotification("Not creating invoice, because option was disabled"));
             return;
         }
 
         var order = notification.Order;
 
-        bool didError = false;
+        if (!order.Products.Any()) {
+            _uibus.Publish(new OrderReleaseInfoNotification("Not creating invoice, because there where no products found"));
+            return;
+        }
+
+        Models.Invoice invoice = await CreateInvoice(order);
+
+        var outputDir = notification.ReleaseProfile.InvoiceOutputDirectory;
+        var doPrint = notification.ReleaseProfile.PrintInvoice;
+
+        var service = new InvoiceService();
+        try {
+            var wb = service.GenerateInvoice(invoice);
+            string outputFile = _fileReader.GetAvailableFileName(outputDir, $"{order.Number} - {order.Name} Invoice", "xlsx");
+            wb.SaveAs(outputFile);
+            _uibus.Publish(new OrderReleaseFileCreatedNotification("Packing List created", outputFile));
+        } catch (Exception ex) {
+            _uibus.Publish(new OrderReleaseErrorNotification($"Error creating invoice {ex.Message}"));
+        }
+
+    }
+
+    private async Task<Models.Invoice> CreateInvoice(Order order) {
+        
+        bool hasError = false;
 
         Company? vendor = null;
         var vendorQuery = await GetCompany(order.VendorId);
@@ -44,31 +64,24 @@ internal class InvoiceHandler : DomainListener<TriggerOrderReleaseNotification> 
                 vendor = company;
             },
             (error) => {
-                didError = true;
+                hasError = true;
                 _logger.LogError("Error loading vendor {Error}", error);
             }
         );
 
-        if (didError || vendor is null) {
+        if (hasError || vendor is null) {
             _uibus.Publish(new OrderReleaseErrorNotification("Could not load customer or vendor information for order"));
-            return;
         }
 
-        var config = new ClosedXMLTemplateConfiguration() { TemplateFilePath = notification.ReleaseProfile.InvoiceTemplatePath };
-        var outputDir = notification.ReleaseProfile.InvoiceOutputDirectory;
-        var doPrint = notification.ReleaseProfile.PrintInvoice;
-
         var custLine2Str = string.IsNullOrWhiteSpace(order.Shipping.Address.Country + order.Shipping.Address.State + order.Shipping.Address.Zip) ? "" : $"{order.Shipping.Address.City}, {order.Shipping.Address.State} {order.Shipping.Address.Zip}";
-        var vendLine2Str = string.IsNullOrWhiteSpace(vendor.Address.Country + vendor.Address.State + vendor.Address.Zip) ? "" : $"{vendor.Address.City}, {vendor.Address.State} {vendor.Address.Zip}";
+        var vendLine2Str = string.IsNullOrWhiteSpace(vendor?.Address.Country + vendor?.Address.State + vendor?.Address.Zip) ? "" : $"{vendor?.Address.City}, {vendor?.Address.State} {vendor?.Address.Zip}";
 
-        var items = order.Products
-                        .Where(p => p is DovetailDrawerBoxProduct)
-                        .Cast<DovetailDrawerBoxProduct>()
-                        .Select(b => new Item() {
+        var drawerBoxes = order.Products
+                        .OfType<DovetailDrawerBoxProduct>()
+                        .Select(b => new Models.DrawerBoxItem() {
                             Line = b.ProductNumber,
                             Qty = b.Qty,
-                            Description = "Drawer Box",
-                            Logo = b.Options.Logo == LogoPosition.None ? "N" : "Y",
+                            Description = b.Description,
                             Height = b.Height.AsInchFraction().ToString(),
                             Width = b.Width.AsInchFraction().ToString(),
                             Depth = b.Depth.AsInchFraction().ToString(),
@@ -76,7 +89,23 @@ internal class InvoiceHandler : DomainListener<TriggerOrderReleaseNotification> 
                             ExtPrice = (b.UnitPrice * b.Qty).ToString("$0.00")
                         }).ToList();
 
-        var packinglist = new Models.PackingList() {
+        var cabinets = order.Products
+                        .OfType<Cabinet>()
+                        .Select(b => new Models.CabinetItem() {
+                            Line = b.ProductNumber,
+                            Qty = b.Qty,
+                            Description = b.Description,
+                            Height = b.Height.AsInchFraction().ToString(),
+                            Width = b.Width.AsInchFraction().ToString(),
+                            Depth = b.Depth.AsInchFraction().ToString(),
+                            Price = b.UnitPrice.ToString("$0.00"),
+                            ExtPrice = (b.UnitPrice * b.Qty).ToString("$0.00")
+                        }).ToList();
+
+        var doors = new List<Models.DoorItem>();
+        var closetParts = new List<Models.ClosetPartItem>();
+
+        var invoice = new Models.Invoice() {
             Customer = new() {
                 Name = order.Customer.Name,
                 Line1 = order.Shipping.Address.Line1,
@@ -84,56 +113,27 @@ internal class InvoiceHandler : DomainListener<TriggerOrderReleaseNotification> 
                 Line3 = order.Shipping.PhoneNumber
             },
             Vendor = new() {
-                Name = vendor.Name,
-                Line1 = vendor.Address.Line1,
+                Name = vendor?.Name ?? "",
+                Line1 = vendor?.Address.Line1 ?? "",
                 Line2 = vendLine2Str,
-                Line3 = vendor.PhoneNumber
+                Line3 = vendor?.PhoneNumber ?? ""
             },
             Date = order.OrderDate,
-            ItemCount = order.Products.Sum(b => b.Qty),
             OrderName = order.Name,
             OrderNumber = order.Number,
-            Total = order.Total.ToString("$0.00"), // TODO: use excel formatting instead of string formatting
-            Discount = order.PriceAdjustment.ToString("0.00"),
-            NetAmount = order.AdjustedSubTotal.ToString("0.00"),
-            SalesTax = order.Tax.ToString("0.00"),
-            SubTotal = order.SubTotal.ToString("0.00"),
-            Items = items
+            Total = order.Total,
+            Discount = order.PriceAdjustment,
+            NetAmount = order.AdjustedSubTotal,
+            SalesTax = order.Tax,
+            Shipping = order.Shipping.Price,
+            SubTotal = order.SubTotal,
+            DrawerBoxes = drawerBoxes,
+            Cabinets = cabinets,
+            ClosetParts = closetParts,
+            Doors = doors
         };
 
-        var response = await _bus.Send(new FillTemplateRequest(packinglist, outputDir, $"{order.Number} - {order.Name} INVOICE", doPrint, config));
-
-        FillTemplateResponse? invResponse = null;
-        didError = false;
-        response.Match(
-            inv => invResponse = inv,
-            error => {
-                _logger.LogInformation("Error creating invoice {Error}", error);
-                _uibus.Publish(new OrderReleaseErrorNotification($"Error creating nvoice {error.Details}"));
-                didError = true;
-            }
-        );
-
-        if (didError || invResponse is null) return;
-
-        // TODO: check if order has a discount
-        if (order.PriceAdjustment != 0M) {
-
-            using var stream = _reader.OpenReadFileStream(invResponse.FilePath, FileAccess.ReadWrite);
-            using var workbook = new XLWorkbook(stream);
-            var sheet = workbook.Worksheet("Invoice");
-
-            var rng = workbook.NamedRange("DiscountRows").Ranges.First();
-            var first = rng.Rows().First().RowNumber();
-            int last = rng.Rows().Last().RowNumber();
-            sheet.Rows(first, last).Hide();
-
-            workbook.Save();
-
-        }
-
-        _logger.LogInformation("Invoice created: {FilePath}", invResponse.FilePath);
-        _uibus.Publish(new OrderReleaseSuccessNotification($"Invoice created {invResponse.FilePath}"));*/
+        return invoice;
 
     }
 
---
+++ ApplicationCore/ApplicationCore/Features/Orders/Release/Handlers/Invoice/InvoiceService.cs
@@ -0,0 +1,283 @@
+﻿using ApplicationCore.Features.Orders.Release.Handlers.Invoice.Models;
+using ClosedXML.Excel;
+
+namespace ApplicationCore.Features.Orders.Release.Handlers.Invoice;
+
+internal class InvoiceService {
+
+    private static readonly XLColor HighlightColor = XLColor.FromArgb(206, 213, 234);
+
+    public IXLWorkbook GenerateInvoice(Models.Invoice invoice) {
+
+        var workbook = new XLWorkbook();
+
+        var ws = workbook.Worksheets.Add("Invoice");
+
+        var titleRng = ws.Range("A1:J1");
+        titleRng.Cell(1, 1).Value = "Invoice";
+        titleRng.Style.Font.FontSize = 48;
+        titleRng.Merge();
+        titleRng.Style.Alignment.SetVertical(XLAlignmentVerticalValues.Bottom);
+        titleRng.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        ws.Row(1).Height = 62;
+
+        var fromCell = ws.Cell("B2");
+        fromCell.Value = "From: ";
+        fromCell.Style.Font.SetItalic(true);
+        fromCell.Style.Alignment.SetVertical(XLAlignmentVerticalValues.Bottom);
+        fromCell.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Right);
+
+        AddCompanyInfo(invoice.Vendor, fromCell.CellRight());
+
+        var toCell = ws.Cell("B7");
+        toCell.Value = "To: ";
+        toCell.Style.Font.SetItalic(true);
+        toCell.Style.Alignment.SetVertical(XLAlignmentVerticalValues.Bottom);
+        toCell.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Right);
+        AddCompanyInfo(invoice.Customer, toCell.CellRight());
+
+        AddInfoField(ws.Cell("H2"), "Date", invoice.Date.ToShortDateString());
+        ws.Range("I2:J2").Merge();
+        AddInfoField(ws.Cell("H4"), "Tracking #:", invoice.OrderNumber);
+        AddInfoField(ws.Cell("H5"), "Name:", invoice.OrderName);
+
+        AddInfoField(ws.Cell("I8"), "Subtotal", invoice.SubTotal.ToString("$0.00"));
+        int row = 9;
+        if (invoice.Discount != 0) {
+            AddInfoField(ws.Cell($"I{row++}"), "Discount", invoice.Discount.ToString("0.00"));
+            AddInfoField(ws.Cell($"I{row++}"), "Net Amt.", invoice.NetAmount.ToString("0.00"));
+        }
+        AddInfoField(ws.Cell($"I{row++}"), "Sales Tax", invoice.SalesTax.ToString("0.00"));
+        AddInfoField(ws.Cell($"I{row++}"), "Shipping", invoice.Shipping.ToString("0.00"));
+        var totalCell = ws.Cell($"J{row}");
+        totalCell.Value = invoice.Total.ToString("0.00");
+        totalCell.Style.Font.SetBold(true);
+        totalCell.Style.Border.SetTopBorder(XLBorderStyleValues.Thin);
+        ws.Range($"I8:J{row}").Style.Border.SetOutsideBorder(XLBorderStyleValues.Thin);
+
+        ws.Row(6).Height = 7;
+        ws.Row(row+1).Height = 7;
+        row += 2;
+
+        if (invoice.DrawerBoxes.Any()) {
+            row = AddDrawerBoxTable(ws, row, invoice.DrawerBoxes);
+        }
+
+        if (invoice.Doors.Any()) {
+            row = AddDoorTable(ws, row, invoice.Doors);
+        }
+
+        if (invoice.ClosetParts.Any()) {
+            // Do something...
+        }
+
+        if (invoice.Cabinets.Any()) {
+            row = AddCabinetTable(ws, row, invoice.Cabinets);
+        }
+
+        ws.Column(4).Width = 20;
+
+        ws.PageSetup.PrintAreas.Add(1, 1, row, 10);
+        ws.PageSetup.PagesWide = 1;
+
+        return workbook;
+
+    }
+
+    public static void AddInfoField(IXLCell cell, string header, object value) {
+
+        cell.Value = header;
+        cell.Style.Alignment.SetVertical(XLAlignmentVerticalValues.Bottom);
+        cell.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Right);
+
+        cell = cell.CellRight();
+        cell.Value = value;
+        cell.Style.Alignment.SetVertical(XLAlignmentVerticalValues.Bottom);
+        cell.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Left);
+
+    }
+
+    public static void AddCompanyInfo(Company company, IXLCell cell) {
+
+        cell.Value = company.Name;
+        cell.Style.Font.SetFontSize(16);
+        cell.Style.Font.SetBold(true);
+
+        cell = cell.CellBelow();
+        cell.Value = company.Line1;
+
+        cell = cell.CellBelow();
+        cell.Value = company.Line2;
+
+        cell = cell.CellBelow();
+        cell.Value = company.Line3;
+
+    }
+
+    private static void AddProductTitle(IXLWorksheet worksheet, int row, int qty, string message) {
+
+        var countCell = worksheet.Cell(row, 2);
+        countCell.Value = qty;
+        countCell.Style.Fill.BackgroundColor = HighlightColor;
+        countCell.Style.Border.SetOutsideBorder(XLBorderStyleValues.Thin);
+        countCell.Style.Font.SetFontSize(14);
+
+        var titleCell = worksheet.Cell(row, 3);
+        titleCell.Value = message;
+        titleCell.Style.Border.SetLeftBorder(XLBorderStyleValues.Thin);
+        titleCell.Style.Border.SetTopBorder(XLBorderStyleValues.Thin);
+        titleCell.Style.Border.SetBottomBorder(XLBorderStyleValues.Thin);
+        titleCell.Style.Font.SetFontSize(14);
+        var titleCell2 = worksheet.Cell(row, 4);
+        titleCell2.Style.Border.SetRightBorder(XLBorderStyleValues.Thin);
+        titleCell2.Style.Border.SetTopBorder(XLBorderStyleValues.Thin);
+        titleCell2.Style.Border.SetBottomBorder(XLBorderStyleValues.Thin);
+
+    }
+
+    private static void AddHeader(IXLCell cell, string value) {
+
+        cell.Value = value;
+        cell.Style.Font.SetBold(true);
+        cell.Style.Fill.BackgroundColor = HighlightColor;
+        cell.Style.Border.SetOutsideBorder(XLBorderStyleValues.Thin);
+        cell.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+
+    }
+
+    private static void AddRowCell(IXLCell cell, object value) {
+
+        cell.Value = value;
+        cell.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        cell.Style.Border.SetOutsideBorder(XLBorderStyleValues.Thin);
+
+    }
+
+    private static void AddDescriptionCell(IXLWorksheet worksheet, IXLCell cell, object value) {
+
+        cell.Value = value;
+        worksheet.Range(cell, cell.CellRight()).Style.Border.SetOutsideBorder(XLBorderStyleValues.Thin);
+    }
+
+    public static int AddDrawerBoxTable(IXLWorksheet worksheet, int row, IEnumerable<DrawerBoxItem> drawerBoxes) {
+
+        AddProductTitle(worksheet, row, drawerBoxes.Sum(d => d.Qty), "Drawer Box(es) in order");
+
+        worksheet.Row(++row).Height = 7;
+
+        ++row;
+        AddHeader(worksheet.Cell(row, 2), "Cab");
+        AddHeader(worksheet.Cell(row, 3), "Qty");
+        AddHeader(worksheet.Cell(row, 6), "Width");
+        AddHeader(worksheet.Cell(row, 7), "Height");
+        AddHeader(worksheet.Cell(row, 8), "Depth");
+        AddHeader(worksheet.Cell(row, 9), "Price");
+        AddHeader(worksheet.Cell(row, 10), "Ext. Price");
+
+        var desc = worksheet.Range(row, 4, row, 5);
+        desc.Cell(1, 1).Value = "Description";
+        desc.Merge();
+        desc.Style.Font.SetBold(true);
+        desc.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        desc.Style.Fill.BackgroundColor = HighlightColor;
+        desc.Style.Border.SetOutsideBorder(XLBorderStyleValues.Thin);
+
+        foreach (var drawerBoxItem in drawerBoxes) {
+
+            ++row;
+            AddRowCell(worksheet.Cell(row, 2), drawerBoxItem.Line);
+            AddRowCell(worksheet.Cell(row, 3), drawerBoxItem.Qty);
+            AddDescriptionCell(worksheet, worksheet.Cell(row, 4), drawerBoxItem.Description);
+            AddRowCell(worksheet.Cell(row, 6), drawerBoxItem.Width);
+            AddRowCell(worksheet.Cell(row, 7), drawerBoxItem.Height);
+            AddRowCell(worksheet.Cell(row, 8), drawerBoxItem.Depth);
+            AddRowCell(worksheet.Cell(row, 9), drawerBoxItem.Price);
+            AddRowCell(worksheet.Cell(row, 10), drawerBoxItem.ExtPrice);
+
+        }
+
+        return row;
+
+    }
+
+    public static int AddDoorTable(IXLWorksheet worksheet, int row, IEnumerable<DoorItem> doors) {
+
+        AddProductTitle(worksheet, row, doors.Sum(d => d.Qty), "Door(s) in order");
+
+        worksheet.Row(++row).Height = 7;
+
+        ++row;
+        AddHeader(worksheet.Cell(row, 2), "Cab");
+        AddHeader(worksheet.Cell(row, 3), "Qty");
+        AddHeader(worksheet.Cell(row, 6), "Width");
+        AddHeader(worksheet.Cell(row, 7), "Height");
+        AddHeader(worksheet.Cell(row, 8), "Price");
+        AddHeader(worksheet.Cell(row, 9), "Ext. Price");
+
+        var desc = worksheet.Range(row, 4, row, 5);
+        desc.Cell(1, 1).Value = "Description";
+        desc.Merge();
+        desc.Style.Font.SetBold(true);
+        desc.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        desc.Style.Fill.BackgroundColor = HighlightColor;
+        desc.Style.Border.SetOutsideBorder(XLBorderStyleValues.Thin);
+
+        foreach (var doorItem in doors) {
+
+            ++row;
+            AddRowCell(worksheet.Cell(row, 2), doorItem.Line);
+            AddRowCell(worksheet.Cell(row, 3), doorItem.Qty);
+            AddDescriptionCell(worksheet, worksheet.Cell(row, 4), doorItem.Description);
+            AddRowCell(worksheet.Cell(row, 6), doorItem.Width);
+            AddRowCell(worksheet.Cell(row, 7), doorItem.Height);
+            AddRowCell(worksheet.Cell(row, 9), doorItem.Price);
+            AddRowCell(worksheet.Cell(row, 10), doorItem.ExtPrice);
+
+        }
+
+        return row;
+
+    }
+
+    public static int AddCabinetTable(IXLWorksheet worksheet, int row, IEnumerable<CabinetItem> cabinets) {
+
+        AddProductTitle(worksheet, row, cabinets.Sum(c => c.Qty), "Cabinet(s) in order");
+
+        worksheet.Row(++row).Height = 7;
+
+        ++row;
+        AddHeader(worksheet.Cell(row, 2), "Cab");
+        AddHeader(worksheet.Cell(row, 3), "Qty");
+        AddHeader(worksheet.Cell(row, 6), "Width");
+        AddHeader(worksheet.Cell(row, 7), "Height");
+        AddHeader(worksheet.Cell(row, 8), "Depth");
+        AddHeader(worksheet.Cell(row, 9), "Price");
+        AddHeader(worksheet.Cell(row, 10), "Ext. Price");
+
+        var desc = worksheet.Range(row, 4, row, 5);
+        desc.Cell(1, 1).Value = "Description";
+        desc.Merge();
+        desc.Style.Font.SetBold(true);
+        desc.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        desc.Style.Fill.BackgroundColor = HighlightColor;
+        desc.Style.Border.SetOutsideBorder(XLBorderStyleValues.Thin);
+
+        foreach (var cabinetItem in cabinets) {
+
+            ++row;
+            AddRowCell(worksheet.Cell(row, 2), cabinetItem.Line);
+            AddRowCell(worksheet.Cell(row, 3), cabinetItem.Qty);
+            AddDescriptionCell(worksheet, worksheet.Cell(row, 4), cabinetItem.Description);
+            AddRowCell(worksheet.Cell(row, 6), cabinetItem.Width);
+            AddRowCell(worksheet.Cell(row, 7), cabinetItem.Height);
+            AddRowCell(worksheet.Cell(row, 8), cabinetItem.Depth);
+            AddRowCell(worksheet.Cell(row, 9), cabinetItem.Price);
+            AddRowCell(worksheet.Cell(row, 10), cabinetItem.ExtPrice);
+
+        }
+
+        return row;
+
+    }
+
+}
