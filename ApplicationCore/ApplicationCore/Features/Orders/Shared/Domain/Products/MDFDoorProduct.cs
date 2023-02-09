644ecdd9c2c2124873122799b38681ff4bc5015f
ZachLondono <zachary.londono5@gmail.com>
2023-02-08 20:47:24 -0500

    Added room to mdf door product

--- ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/Products/MDFDoorProduct.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/Products/MDFDoorProduct.cs
@@ -8,17 +8,19 @@ namespace ApplicationCore.Features.Orders.Shared.Domain.Products;
 public class MDFDoorProduct : MDFDoor, IProduct, IDoorContainer {
 
     public Guid Id { get; }
-
     public decimal UnitPrice { get; }
+    public string Room { get; }
 
-    public MDFDoorProduct(Guid id, decimal unitPrice, int qty, int productNumber, DoorType type, Dimension height, Dimension width, string note, string material, string framingBead, string edgeDetail, DoorFrame frameSize, Dimension panelDrop, string? paintColor)
+
+    public MDFDoorProduct(Guid id, decimal unitPrice, string room, int qty, int productNumber, DoorType type, Dimension height, Dimension width, string note, string material, string framingBead, string edgeDetail, DoorFrame frameSize, Dimension panelDrop, string? paintColor)
         : base(qty, productNumber, type, height, width, note, material, framingBead, edgeDetail, frameSize, panelDrop, paintColor) {
         Id = id;
         UnitPrice = unitPrice;
+        Room = room;
     }
 
-    public static MDFDoorProduct Create(decimal unitPrice, int qty, int productNumber, DoorType type, Dimension height, Dimension width, string note, string material, string framingBead, string edgeDetail, DoorFrame frameSize, Dimension panelDrop, string? paintColor)
-        => new(Guid.NewGuid(), unitPrice, qty, productNumber, type, height, width, note, material, framingBead, edgeDetail, frameSize, panelDrop, paintColor);
+    public static MDFDoorProduct Create(decimal unitPrice, string room, int qty, int productNumber, DoorType type, Dimension height, Dimension width, string note, string material, string framingBead, string edgeDetail, DoorFrame frameSize, Dimension panelDrop, string? paintColor)
+        => new(Guid.NewGuid(), unitPrice, room, qty, productNumber, type, height, width, note, material, framingBead, edgeDetail, frameSize, panelDrop, paintColor);
 
     public IEnumerable<MDFDoor> GetDoors(Func<MDFDoorBuilder> getBuilder) { yield return this; }
 
