5beca450ba181cbbea8c61349ae8fac19b84616d
ZachLondono <zachary.londono5@gmail.com>
2023-02-11 13:54:10 -0500

    Added qty property

--- ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/ValueObjects/RollOutOptions.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/ValueObjects/RollOutOptions.cs
@@ -3,4 +3,8 @@ using ApplicationCore.Features.Shared.Domain;
 
 namespace ApplicationCore.Features.Orders.Shared.Domain.ValueObjects;
 
-public record RollOutOptions(Dimension[] Positions, bool ScoopFront, RollOutBlockPosition Blocks, DrawerSlideType SlideType, CabinetDrawerBoxMaterial Material) { };
+public record RollOutOptions(Dimension[] Positions, bool ScoopFront, RollOutBlockPosition Blocks, DrawerSlideType SlideType, CabinetDrawerBoxMaterial Material) {
+
+    public int Qty => Positions.Length;
+
+};
