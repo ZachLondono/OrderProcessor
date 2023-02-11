82e4bc6a1a1ad165e7e0a0cb9c5940bbd8db4628
ZachLondono <zachary.londono5@gmail.com>
2023-02-11 13:43:55 -0500

    Added trash cabinet builder

---
+++ ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/Builders/TrashCabinetBuilder.cs
@@ -0,0 +1,54 @@
+﻿using ApplicationCore.Features.Orders.Shared.Domain.Enums;
+using ApplicationCore.Features.Orders.Shared.Domain.Products;
+using ApplicationCore.Features.Orders.Shared.Domain.ValueObjects;
+using ApplicationCore.Features.Shared.Domain;
+
+namespace ApplicationCore.Features.Orders.Shared.Domain.Builders;
+
+internal class TrashCabinetBuilder : CabinetBuilder<TrashCabinet> {
+
+    public Dimension DrawerFaceHeight { get; private set; }
+    public DrawerSlideType SlideType { get; private set; }
+    public CabinetDrawerBoxMaterial DrawerBoxMaterial { get; private set; }
+    public TrashPulloutConfiguration TrashPulloutConfiguration { get; private set; }
+    public IToeType ToeType { get; private set; }
+
+    public TrashCabinetBuilder() {
+        DrawerFaceHeight = Dimension.FromMillimeters(157);
+        SlideType = DrawerSlideType.UnderMount;
+        DrawerBoxMaterial = CabinetDrawerBoxMaterial.SolidBirch;
+        TrashPulloutConfiguration = TrashPulloutConfiguration.OneCan;
+        ToeType = new LegLevelers();
+    }
+
+    public TrashCabinetBuilder WithDrawerFaceHeight(Dimension drawerFaceHeight) {
+        DrawerFaceHeight = drawerFaceHeight;
+        return this;
+    }
+
+    public TrashCabinetBuilder WithToeType(IToeType toeType) {
+        ToeType = toeType;
+        return this;
+    }
+
+    public TrashCabinetBuilder WithSlideType(DrawerSlideType slideType) {
+        SlideType = slideType;
+        return this;
+    }
+
+    public TrashCabinetBuilder WithTrashPulloutConfiguration(TrashPulloutConfiguration trashPulloutConfiguration) {
+        TrashPulloutConfiguration = TrashPulloutConfiguration;
+        return this;
+    } 
+
+    public TrashCabinetBuilder WithDrawerBoxMaterial(CabinetDrawerBoxMaterial cabinetDrawerBoxMaterial) {
+        DrawerBoxMaterial = cabinetDrawerBoxMaterial;
+        return this;
+    }
+
+    public override TrashCabinet Build() {
+        var cabinet = TrashCabinet.Create(Qty, UnitPrice, ProductNumber, Room, Assembled, Height, Width, Depth, BoxMaterial, FinishMaterial, MDFDoorOptions, EdgeBandingColor, RightSide, LeftSide, Comment, DrawerFaceHeight, TrashPulloutConfiguration, SlideType, DrawerBoxMaterial, ToeType);
+        return cabinet;
+    }
+
+}
