9f57289a813de957db874dcd4e62d7e16f1cf95e
ZachLondono <zachary.londono5@gmail.com>
2023-02-23 18:28:06 -0500

    Updated test toe type

--- ApplicationCore/ApplicationCore.Tests.Unit/Orders/Products/TestToeType.cs
+++ ApplicationCore/ApplicationCore.Tests.Unit/Orders/Products/TestToeType.cs
@@ -3,12 +3,8 @@ using ApplicationCore.Features.Shared.Domain;
 
 namespace ApplicationCore.Tests.Unit.Orders.Products;
 
-public class TestToeType : ToeType {
-    public Dimension ToeHeight { get; }
-    public Dimension HeightAdjustment { get; }
-    public string PSIParameter => string.Empty;
-    public TestToeType(Dimension height) {
-        ToeHeight = height;
-        HeightAdjustment = height;
+public record TestToeType : ToeType {
+    public TestToeType(Dimension height) : base(height, height, "") {
+        
     }
 }
