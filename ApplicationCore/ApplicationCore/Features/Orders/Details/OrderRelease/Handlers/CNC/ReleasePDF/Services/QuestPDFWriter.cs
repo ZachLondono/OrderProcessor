1a2f665659ebe5fa53dc6a6190b3f81c526765cb
ZachLondono <zachary.londono5@gmail.com>
2023-03-20 11:19:58 -0400

    Group together equivalent programs on different machines

--- ApplicationCore/ApplicationCore/Features/Orders/Details/OrderRelease/Handlers/CNC/ReleasePDF/Services/QuestPDFWriter.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Details/OrderRelease/Handlers/CNC/ReleasePDF/Services/QuestPDFWriter.cs
@@ -2,6 +2,7 @@
 using ApplicationCore.Features.Orders.Details.OrderRelease.Handlers.CNC.ReleasePDF.Contracts;
 using ApplicationCore.Features.Orders.Details.OrderRelease.Handlers.CNC.ReleasePDF.PDFModels;
 using ApplicationCore.Features.Orders.Details.OrderRelease.Handlers.CNC.ReleasePDF.Styling;
+using System.Diagnostics.CodeAnalysis;
 
 namespace ApplicationCore.Features.Orders.Details.OrderRelease.Handlers.CNC.ReleasePDF.Services;
 
@@ -20,10 +21,12 @@ internal class QuestPDFWriter : IReleasePDFWriter {
 
         List<IDocumentDecorator> decorators = new();
 
-        foreach (var release in job.Releases) {
+        var groups = job.Releases.GroupBy(r => r, new ReleaseGroupComparer());
 
-            CoverModel cover = CreateCover(job, release);
-            List<PageModel> pages = CreatePages(job, release);
+        foreach (var group in groups) {
+
+            CoverModel cover = CreateCover(job, group);
+            List<PageModel> pages = CreatePages(job, group);
 
             decorators.Add(new ReleasePDFDecorator(config, cover, pages));
 
@@ -33,10 +36,16 @@ internal class QuestPDFWriter : IReleasePDFWriter {
 
     }
 
-    private static List<PageModel> CreatePages(ReleasedJob job, MachineRelease release) {
+    private static List<PageModel> CreatePages(ReleasedJob job, IEnumerable<MachineRelease> releases) {
 
         var pages = new List<PageModel>();
-        foreach (var program in release.Programs) {
+        int programIndex = 0;
+        foreach (var program in releases.First().Programs) {
+
+            List<ReleasedProgram> programs = new();
+            foreach (var release in releases) {
+                programs.Add(release.Programs.ElementAt(programIndex));
+            }
 
             var partsTableContent = new List<Dictionary<string, string>>();
             var partGroups = program.Parts.GroupBy(p => p.Name);
@@ -57,12 +66,12 @@ internal class QuestPDFWriter : IReleasePDFWriter {
             // TODO: add an option to use the file name or the line number (in the pattern)
             //int index = 1;
             var imgtxts = program.Parts.Select(p => new ImageText() { Text = $"{p.Name}", Location = p.Center });
-            byte[] imageData = PatternImageFactory.CreatePatternImage(program.ImagePath, release.MachineTableOrientation, program.Material.Width, program.Material.Length, imgtxts);
+            byte[] imageData = PatternImageFactory.CreatePatternImage(program.ImagePath, releases.First().MachineTableOrientation, program.Material.Width, program.Material.Length, imgtxts);
 
             pages.Add(new() {
-                Header = $"{job.JobName}  [{release.MachineName}]",
-                Title = program.Name,
-                Title2 = program.HasFace6 ? $"6{program.Name[1..]}" : "",
+                Header = $"{job.JobName}  [{string.Join(',', releases.Select(r => r.MachineName))}]",
+                Title = string.Join(" | ", programs.Select(p => p.Name)),
+                Title2 = program.HasFace6 ? string.Join(" | ", programs.Select(p => $"6{p.Name[1..]}" )) : "",
                 Subtitle = $"{material.Name} - {material.Width:0.00}x{material.Length:0.00}x{material.Thickness:0.00} (grained:{(material.IsGrained ? "yes" : "no")})",
 
                 Footer = "footer",
@@ -73,13 +82,18 @@ internal class QuestPDFWriter : IReleasePDFWriter {
                 },
             });
 
+            programIndex++;
+
         }
 
         return pages;
     }
 
-    private static CoverModel CreateCover(ReleasedJob job, MachineRelease release) {
-        var usedmaterials = release.Programs.Select(p => p.Material).GroupBy(m => (m.Name, m.Width, m.Length, m.Thickness, m.IsGrained));
+    private static CoverModel CreateCover(ReleasedJob job, IEnumerable<MachineRelease> releases) {
+
+        if (!releases.Any()) return new();
+
+        var usedmaterials = releases.First().Programs.Select(p => p.Material).GroupBy(m => (m.Name, m.Width, m.Length, m.Thickness, m.IsGrained));
         var materialTableContent = new List<Dictionary<string, string>>();
         foreach (var mat in usedmaterials) {
             materialTableContent.Add(new() {
@@ -97,7 +111,7 @@ internal class QuestPDFWriter : IReleasePDFWriter {
             Content = materialTableContent
         };
 
-        var releasedparts = release.Programs.SelectMany(p => p.Parts).OrderBy(p => p.ProductNumber).GroupBy(p => p.Name);
+        var releasedparts = releases.First().Programs.SelectMany(p => p.Parts).OrderBy(p => p.ProductNumber).GroupBy(p => p.Name);
         var partsTableContent = new List<Dictionary<string, string>>();
         foreach (var group in releasedparts) {
             var part = group.First();
@@ -118,9 +132,9 @@ internal class QuestPDFWriter : IReleasePDFWriter {
         var toolTableContent = new List<Dictionary<string, string>>();
         var row = new Dictionary<string, string>();
         bool hasTools = false;
-        foreach (var pos in release.ToolTable.Keys.OrderBy(p => p)) {
-            row.Add(pos.ToString(), release.ToolTable[pos]);
-            if (!string.IsNullOrWhiteSpace(release.ToolTable[pos])) hasTools = true;
+        foreach (var pos in releases.First().ToolTable.Keys.OrderBy(p => p)) {
+            row.Add(pos.ToString(), releases.First().ToolTable[pos]);
+            if (!string.IsNullOrWhiteSpace(releases.First().ToolTable[pos])) hasTools = true;
         }
         toolTableContent.Add(row);
 
@@ -137,7 +151,7 @@ internal class QuestPDFWriter : IReleasePDFWriter {
         string workIdStr = job.WorkOrderId is null ? "" : GetGuidAsBase64((Guid)job.WorkOrderId);
 
         var cover = new CoverModel() {
-            Title = $"{job.JobName}  [{release.MachineName}]",
+            Title = $"{job.JobName}  [{string.Join(',', releases.Select(r => r.MachineName))}]",
             WorkOrderId = workIdStr,
             Info = new Dictionary<string, string>() {
                     {"Vendor", job.VendorName },
@@ -152,5 +166,50 @@ internal class QuestPDFWriter : IReleasePDFWriter {
     }
 
     private static string GetGuidAsBase64(Guid id) => Convert.ToBase64String(id.ToByteArray()).Replace("/", "-").Replace("+", "_").Replace("=", "");
+
+    private class ReleaseGroupComparer : IEqualityComparer<MachineRelease> {
+        
+        public bool Equals(MachineRelease? x, MachineRelease? y) {
+            
+            if (x is null && y is null) return true;
+            if (x is null || y is null) return false;
+
+            var xEnum = x.Programs.GetEnumerator();
+            var yEnum = y.Programs.GetEnumerator();
+
+            while (true) {
+
+                bool xResult = xEnum.MoveNext();
+                bool yResult = yEnum.MoveNext();
+
+                if (xResult != yResult) return false;
+                if (!xResult) break;
+
+                var xProg = xEnum.Current;
+                var yProg = yEnum.Current;
+
+                if (xProg.Material.Name != yProg.Material.Name) return false;
+                if (xProg.Material.Width != yProg.Material.Width) return false;
+                if (xProg.Material.Length != yProg.Material.Length) return false;
+                if (xProg.Material.Thickness != yProg.Material.Thickness) return false;
+
+                var xParts = xProg.Parts.GroupBy(p => p.Name).Select(g => new PartGroup(g.Key, g.Count()));
+                var yParts = yProg.Parts.GroupBy(p => p.Name).Select(g => new PartGroup(g.Key, g.Count()));
+
+                if (!xParts.All(yParts.Contains)) return false;
+
+            }
+
+            if (x.ToolTable.Keys.Count() == y.ToolTable.Keys.Count() && x.ToolTable.Keys.All(k => y.ToolTable.ContainsKey(k) && object.Equals(y.ToolTable[k], x.ToolTable[k]))) return true;
+
+            return false;
+
+        }
+
+        public int GetHashCode([DisallowNull] MachineRelease obj) => 0;
+
+        public record PartGroup(string Name, int Count);
+
+    }
 
 }
