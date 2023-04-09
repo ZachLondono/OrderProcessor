6af7038900a5f4bafea0a04384220032e102b6fe
ZachLondono <zachary.londono5@gmail.com>
2023-04-08 22:46:49 -0400

    Added service that will be used to check for current and latest version

---
+++ ApplicationCore/ApplicationCore/Features/Updates/ApplicationVersionService.cs
@@ -0,0 +1,11 @@
+﻿namespace ApplicationCore.Features.Updates;
+
+internal class ApplicationVersionService {
+
+	public Task<string> DownloadInstaller(string uri) => throw new NotImplementedException();
+
+	public Task<Version> GetCurrentVersion(string uri) => throw new NotImplementedException();
+
+	public Task<Version> GetLatestVersion(string uri) => throw new NotImplementedException();
+
+}
--- ApplicationCore/ApplicationCore/Features/Updates/UpdatesDialogViewModel.cs
+++ ApplicationCore/ApplicationCore/Features/Updates/UpdatesDialogViewModel.cs
@@ -5,6 +5,8 @@ internal class UpdatesDialogViewModel {
     private const string RELEASE_INSTALLER_URI = "http://zacharylondono.com/release";
     private const string PREVIEW_INSTALLER_URI = "http://zacharylondono.com/preview";
 
+    private string InstallerUri { get => UsePreviewChannel ? PREVIEW_INSTALLER_URI : RELEASE_INSTALLER_URI; }
+
     public bool UsePreviewChannel { get; private set; } = true; // TODO: get preview flag from config file
 
     public Version CurrentVersion { get; private set; } = new("0.0.0.0");
@@ -47,18 +49,23 @@ internal class UpdatesDialogViewModel {
 
     public Action? OnPropertyChanged { get; set; }
 
+    private readonly ApplicationVersionService _versionService;
+
+    public UpdatesDialogViewModel(ApplicationVersionService versionService) {
+        _versionService = versionService;
+    }
+
     public async Task CheckForUpdates() {
         IsCheckingForUpdates = true;
-        await Task.Delay(1000);
-        var newVersion = new Version("0.0.0.1");
+        CurrentVersion = await _versionService.GetCurrentVersion(InstallerUri);
+        var newVersion = await _versionService.GetLatestVersion(InstallerUri);
         _availableUpdate = newVersion == CurrentVersion ? null : newVersion;
         IsCheckingForUpdates = false;
     }
 
     public async Task DownloadUpdate() {
         IsUpdateDownloading = true;
-        await Task.Delay(1000);
-        _updateInstallerFilePath = @"path/to/downloaded/installer";
+        _updateInstallerFilePath = await _versionService.DownloadInstaller(InstallerUri);
         IsUpdateDownloading = false;
     }
 
