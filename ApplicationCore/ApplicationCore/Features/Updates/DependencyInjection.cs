a6d3ba1609e7b68c338142a24734ced1aebdc936
ZachLondono <zachary.londono5@gmail.com>
2023-04-08 22:46:49 -0400

    Added service that will be used to check for current and latest version

--- ApplicationCore/ApplicationCore/Features/Updates/DependencyInjection.cs
+++ ApplicationCore/ApplicationCore/Features/Updates/DependencyInjection.cs
@@ -1,15 +1,12 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ApplicationCore.Features.Updates;
+
 public static class DependencyInjection {
 
 	public static IServiceCollection AddUpdates(this IServiceCollection services) {
-		return services.AddTransient<UpdatesDialogViewModel>();
+		return services.AddTransient<UpdatesDialogViewModel>()
+						.AddTransient<ApplicationVersionService>();
 	}
 
 }
