ac2aef2200df34f63ae068ac384302284c5c3e24
ZachLondono <zachary.londono5@gmail.com>
2023-04-13 19:42:51 -0400

    Initilize work order database, if it does not already exist #55

--- ApplicationCore/ApplicationCore/Features/WorkOrders/Data/SqliteWorkOrdersDbConnectionFactory.cs
+++ ApplicationCore/ApplicationCore/Features/WorkOrders/Data/SqliteWorkOrdersDbConnectionFactory.cs
@@ -1,15 +1,26 @@
 ﻿using ApplicationCore.Features.Configuration;
 using ApplicationCore.Infrastructure.Bus;
+using ApplicationCore.Infrastructure.Data;
+using Dapper;
 using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System.Data;
 
 namespace ApplicationCore.Features.WorkOrders.Data;
 
 internal class SqliteWorkOrdersDbConnectionFactory : IWorkOrdersDbConnectionFactory {
 
+    public const int DB_VERSION = 1;
+    private static readonly SemaphoreSlim semaphore = new(1);
+
+    private readonly ILogger<SqliteWorkOrdersDbConnectionFactory> _logger;
+    private readonly IConfiguration _configuration;
     private readonly IBus _bus;
 
-    public SqliteWorkOrdersDbConnectionFactory(IBus bus) {
+    public SqliteWorkOrdersDbConnectionFactory(ILogger<SqliteWorkOrdersDbConnectionFactory> logger, IConfiguration configuration, IBus bus) {
+        _logger = logger;
+        _configuration = configuration;
         _bus = bus;
     }
 
@@ -28,8 +39,51 @@ internal class SqliteWorkOrdersDbConnectionFactory : IWorkOrdersDbConnectionFact
 
         var connection = new SqliteConnection(builder.ConnectionString);
 
+        if (File.Exists(datasource)) {
+
+            int dbVersion = await GetDatabaseVersion(connection);
+            if (dbVersion != DB_VERSION) {
+                throw new IncompatibleDatabaseVersion(dbVersion);
+            }
+
+        } else {
+
+            await InitilizeDatabase(connection);
+
+        }
+
+        semaphore.Release();
+
         return connection;
 
+
+    }
+
+    private async Task InitilizeDatabase(SqliteConnection connection) {
+
+        var schemaPath = _configuration.GetRequiredSection("Schemas").GetValue<string>("WorkOrders");
+
+        if (schemaPath is null) {
+            throw new InvalidOperationException("Ordering data base schema path is not set");
+        }
+
+        _logger.LogInformation("Initilizing ordering database, version {DB_VERSION} from schema in file {FilePath}", DB_VERSION, schemaPath);
+
+        var schema = await File.ReadAllTextAsync(schemaPath);
+
+        await connection.OpenAsync();
+        var trx = await connection.BeginTransactionAsync();
+
+        await connection.ExecuteAsync(schema, trx);
+        await connection.ExecuteAsync($"PRAGMA SCHEMA_VERSION = {DB_VERSION};", trx);
+
+        trx.Commit();
+        connection.Close();
+
+    }
+
+    private async Task<int> GetDatabaseVersion(SqliteConnection connection) {
+        return await connection.QuerySingleAsync<int>("PRAGMA schema_version;");
     }
 
 }
