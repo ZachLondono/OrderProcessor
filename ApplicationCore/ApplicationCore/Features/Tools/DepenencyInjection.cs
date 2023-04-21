ff0a669a9e020f71247404b5800434452e478646
ZachLondono <zachary.londono5@gmail.com>
2023-04-21 18:22:54 -0400

    Added contracts that can be used by other features to get available tools

--- ApplicationCore/ApplicationCore/Features/Tools/DepenencyInjection.cs
+++ ApplicationCore/ApplicationCore/Features/Tools/DepenencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using ApplicationCore.Features.Tools.Contracts;
+using ApplicationCore.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ApplicationCore.Features.Tools;
 
@@ -6,7 +8,26 @@ public static class DepenencyInjection {
 
     public static IServiceCollection AddToolEditor(this IServiceCollection services) {
 
-        return services.AddTransient<ToolFileEditorViewModel>();
+        services.AddTransient<ToolFileEditorViewModel>();
+
+        services.AddTransient<CNCToolBox.GetToolCarousels>(s => {
+
+            var bus = s.GetRequiredService<IBus>();
+            return async () => {
+                var result = await bus.Send(new GetTools.Query(ToolFileEditorViewModel.FILE_PATH));
+
+                return result.Match(
+                   toolMap => toolMap.Select(map => new ToolCarousel() {
+                       MachineName = map.MachineName,
+                       PositionCount = map.ToolPositionCount,
+                       Tools = map.Tools.Select(t => new Tool(t.Name, t.Position, t.AlternativeNames.ToArray())).ToArray()
+                   }),
+                   error => Enumerable.Empty<ToolCarousel>());
+            };
+
+        });
+
+        return services;
 
     }
 
