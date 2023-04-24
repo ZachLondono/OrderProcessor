5ad0e9517a6f44c41bd1ba910c7c9592d7ce7576
ZachLondono <zachary.londono5@gmail.com>
2023-04-24 16:43:16 -0400

    Refactored WSXMLParser to only parse WSXML files

---
+++ ApplicationCore/ApplicationCore/Features/CNC/ReleasePDF/WSXML/WSXMLParser.cs
@@ -0,0 +1,65 @@
+﻿using System.Xml.Linq;
+
+namespace ApplicationCore.Features.CNC.ReleasePDF.WSXML;
+
+internal partial class WSXMLParser {
+
+    public static WSXMLReport? ParseWSXMLReport(string reportFilePath) {
+
+        XDocument xdoc = XDocument.Load(reportFilePath);
+        if (xdoc.Root is null) {
+            Console.WriteLine("No root found");
+            return null;
+        }
+
+        var job = xdoc.Root.Element("Job");
+        if (job is null) {
+            Console.WriteLine("No job found");
+            return null;
+        }
+
+        var manufacturing = job.Element("Manufacturing");
+        if (manufacturing is null) {
+            Console.WriteLine("No manufacturing data found");
+            return null;
+        }
+
+        var allParts = job.Elements("Item")
+                           .Where(item => item.ElementValue("Note") == "Nested blocknest")
+                           .Where(nest => nest.Elements("Part").Any())
+                           .SelectMany(item => 
+                                item.Elements("Part")
+                                            .Select(Part.FromXElemnt)
+                                            .ToList())
+                           .DistinctBy(part => part.Id)
+                           .ToDictionary(part => part.Id, part => part);
+
+        var patternScheduleItems = manufacturing.Elements("PatternSchedule");
+
+        var patternSchedules = patternScheduleItems.Select(PatternSchedule.FromXElement);
+
+        var materials = job.Elements("Material")
+                            .Select(MaterialRecord.FromXElement)
+                            .ToDictionary(m => m.Id);
+
+        var labels = manufacturing.Elements("Label")
+                                    .Select(PartLabels.FromXElement)
+                                    .ToDictionary(l => l.Id);
+
+        var operationGroups = manufacturing.Elements("OperationGroups")
+                                            .Select(OperationGroups.FromXElement);
+
+        var report = new WSXMLReport() {
+            JobName = job.ElementValue("Name"),
+            Parts = allParts,
+            PatternSchedules = patternSchedules,
+            Materials = materials,
+            PartLabels = labels,
+            OperationGroups = operationGroups
+        };
+
+        return report;
+
+    }
+
+}
---
+++ ApplicationCore/ApplicationCore/Features/CNC/ReleasePDF/WSXML/WSXMLReport.cs
@@ -0,0 +1,12 @@
+﻿namespace ApplicationCore.Features.CNC.ReleasePDF.WSXML;
+
+internal class WSXMLReport {
+
+    public required string JobName { get; set; }
+    public required Dictionary<string, Part> Parts { get; set; }
+    public required IEnumerable<PatternSchedule> PatternSchedules { get; set; }
+    public required Dictionary<string, MaterialRecord> Materials { get; set; }
+    public required Dictionary<string, PartLabels> PartLabels { get; set; }
+    public required IEnumerable<OperationGroups> OperationGroups { get; set; }
+
+}
