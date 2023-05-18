5a5b7c6136defb6827661793ab41cc9763a803b9
ZachLondono <zachary.londono5@gmail.com>
2023-05-18 16:30:52 -0400

    Added seperate files for 'code-behind'

---
+++ ApplicationCore/ApplicationCore/Features/Orders/Details/OrderExport/ExportProgressDialog.razor.cs
@@ -0,0 +1,42 @@
+﻿using static ApplicationCore.Features.Orders.Details.OrderExport.ExportProgressViewModel;
+using Microsoft.AspNetCore.Components;
+using Blazored.Modal;
+using Microsoft.JSInterop;
+
+namespace ApplicationCore.Features.Orders.Details.OrderExport;
+
+public partial class ExportProgressDialog {
+
+    [CascadingParameter]
+    private BlazoredModalInstance BlazoredModal { get; set; } = default!;
+
+    [Parameter]
+    public ExportConfiguration? Configuration { get; set; } = default;
+
+    private List<ProgressLogMessage> _messages = new();
+    private ElementReference _messagebox;
+
+    protected override async Task OnInitializedAsync() {
+
+        DataContext.OnPropertyChanged += () => InvokeAsync(StateHasChanged);
+        DataContext.OnMessagePublished += AddMessageToLog;
+
+        if (Configuration is not null) {
+            await Task.Run(() => DataContext.ExportOrder(Configuration));
+        }
+
+    }
+
+    private void AddMessageToLog(ProgressLogMessage message) {
+        InvokeAsync(() => {
+            _messages.Add(message);
+            StateHasChanged();
+        });
+        JSRuntime.InvokeVoidAsync("scrollToEnd", new object[] { _messagebox });
+    }
+
+    private async Task CloseDialog() {
+        await BlazoredModal.CloseAsync();
+    }
+
+}
---
+++ ApplicationCore/ApplicationCore/Features/Orders/Details/OrderExport/ExportSetupDialog.razor.cs
@@ -0,0 +1,80 @@
+﻿
+using ApplicationCore.Features.Orders.Shared.Domain.Entities;
+using Blazored.Modal;
+using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace ApplicationCore.Features.Orders.Details.OrderExport;
+
+public partial class ExportSetupDialog {
+
+    [Parameter]
+    public Order? Order { get; set; }
+
+    [CascadingParameter]
+    private BlazoredModalInstance ModalInstance { get; set; } = default!;
+
+    [CascadingParameter]
+    public IModalService ModalService { get; set; } = default!;
+
+    public ExportConfiguration? Configuration { get; set; } = new();
+    private string? _errorMessage = null;
+
+    protected override async Task OnInitializedAsync() {
+
+        if (Order is null) {
+            _errorMessage = "No order set";
+            return;
+        }
+
+        var vendor = await GetVendorByIdAsync(Order.VendorId);
+        if (vendor is null) return;
+        Configuration = new() {
+
+            FillDovetailOrder = vendor.ExportProfile.ExportDBOrder,
+            FillMDFDoorOrder = vendor.ExportProfile.ExportMDFDoorOrder,
+            GenerateEXT = vendor.ExportProfile.ExportExtFile,
+            ExtJobName = $"{Order.Number} - {Order.Name}",
+            OutputDirectory = Order.WorkingDirectory
+
+        };
+
+        StateHasChanged();
+
+    }
+
+    private async Task Cancel() {
+
+        await ModalInstance.CancelAsync();
+
+    }
+
+    public async Task Export() {
+
+        _errorMessage = null;
+
+        if (Configuration is null) {
+            _errorMessage = "Could not load release configuration";
+            return;
+        }
+
+        var parameters = new ModalParameters() {
+            { "Configuration",  Configuration }
+        };
+
+        var options = new ModalOptions() {
+                HideHeader = true,
+                HideCloseButton = true,
+                DisableBackgroundCancel = true,
+                Size = ModalSize.Large
+            };
+
+        var dialog = ModalService.Show<ExportProgressDialog>("Order Export Progress", parameters, options);
+
+        _ = await dialog.Result;
+
+        await ModalInstance.CloseAsync();
+
+    }
+
+}
