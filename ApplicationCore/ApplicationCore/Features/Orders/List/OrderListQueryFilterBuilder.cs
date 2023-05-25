584602fb2afc188afb186f6a3786a55f2b252ba8
ZachLondono <zachary.londono5@gmail.com>
2023-05-25 00:28:07 -0400

    Fixed bug that didn't skip the correct items

--- ApplicationCore/ApplicationCore/Features/Orders/List/OrderListQueryFilterBuilder.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/List/OrderListQueryFilterBuilder.cs
@@ -20,25 +20,27 @@ public class OrderListQueryFilterBuilder {
         if (SearchTerm is not null) {
             filters.Add($"(name LIKE @ModifiedSearchTerm OR number LIKE @ModifiedSearchTerm)");
         }
-        if (Page >= 1 && PageSize > 0) {
-            // This should be somewhat more efficient then using OFFSET (which is apparently just an alias for "discard N first results")
-            // Instead, just selecting the id column should be somewhat more efficient than selecting all columns. Then just don't include those in the final result.
-            filters.Add("id NOT IN (SELECT id FROM orders ORDER BY order_date DESC LIMIT @CurrentPageStart)");
-        }
-        
+       
         string filter = "";
         if (filters.Any()) {
             filter += " WHERE " + string.Join(" AND ", filters.ToArray());
         }
-        
+
+        string offset = "";
+        if (Page >= 1 && PageSize > 0) {
+            // This should be somewhat more efficient then using OFFSET (which is apparently just an alias for "discard N first results")
+            // Instead, just selecting the id column should be somewhat more efficient than selecting all columns. Then just don't include those in the final result.
+            offset = $"{(filters.Any() ? " AND" : " WHERE")} id NOT IN (SELECT id FROM orders {filter} ORDER BY order_date DESC LIMIT @CurrentPageStart)";
+        } 
+
         string limit = "";
         if (PageSize > 0) {
             limit = $" LIMIT @PageSize";
         }
         
         return $"""
-                {filter}
-                ORDER BY order_date DESC{limit};
+                {filter}{offset}
+                ORDER BY order_date DESC{limit}
                 """;
         
 
