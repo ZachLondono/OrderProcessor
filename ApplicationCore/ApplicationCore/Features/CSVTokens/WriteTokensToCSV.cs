865f293f3c4cb89270397687dd5c755e6a95cbf9
ZachLondono <zachary.londono5@gmail.com>
2023-06-19 23:21:24 -0400

    Added a feature that will write a CSV file for a CNC batch

---
+++ ApplicationCore/ApplicationCore/Features/CSVTokens/WriteTokensToCSV.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationCore.Infrastructure.Bus;
+using CADCodeProxy.Machining;
+using CADCodeProxy.CSV;
+
+namespace ApplicationCore.Features.CSVTokens;
+
+public class WriteTokensToCSV {
+
+    public record Command(Batch Batch, string OutputDirectory) : ICommand<string>;
+
+    public class Handler : CommandHandler<Command, string> {
+
+        public override Task<Response<string>> Handle(Command command) {
+
+            var writer = new CSVTokenWriter();
+
+            var filePath = writer.WriteBatchCSV(command.Batch, command.OutputDirectory);
+
+            return Task.FromResult(Response<string>.Success(filePath));
+
+        }
+
+    }
+
+}
