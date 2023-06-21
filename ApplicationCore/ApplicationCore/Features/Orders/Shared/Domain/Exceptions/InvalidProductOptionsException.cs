999b61d647ef28e1c9e81761e65be7b582e91f62
ZachLondono <zachary.londono5@gmail.com>
2023-06-21 13:36:26 -0400

    Call base constructor

--- ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/Exceptions/InvalidProductOptionsException.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/Exceptions/InvalidProductOptionsException.cs
@@ -4,7 +4,7 @@ public class InvalidProductOptionsException : Exception {
 
     public string Reason { get; init; }
 
-    public InvalidProductOptionsException(string reason) {
+    public InvalidProductOptionsException(string reason) : base(reason) {
         Reason = reason;
     }
 
