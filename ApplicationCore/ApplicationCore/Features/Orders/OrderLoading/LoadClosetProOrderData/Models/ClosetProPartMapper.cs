9de3738c89e624bbe69d67c412519db782d9a3eb
ZachLondono <zachary.londono5@gmail.com>
2023-07-18 16:31:52 -0400

    Implementing mapping more product types from closet pro order

--- ApplicationCore/ApplicationCore/Features/Orders/OrderLoading/LoadClosetProOrderData/Models/ClosetProPartMapper.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/OrderLoading/LoadClosetProOrderData/Models/ClosetProPartMapper.cs
@@ -1,4 +1,5 @@
-﻿using ApplicationCore.Features.Orders.Shared.Domain.Entities;
+﻿using ApplicationCore.Features.Orders.Shared.Domain.Builders;
+using ApplicationCore.Features.Orders.Shared.Domain.Entities;
 using ApplicationCore.Features.Orders.Shared.Domain.Enums;
 using ApplicationCore.Features.Orders.Shared.Domain.Products;
 using ApplicationCore.Features.Orders.Shared.Domain.ValueObjects;
@@ -8,38 +9,152 @@ namespace ApplicationCore.Features.Orders.OrderLoading.LoadClosetProOrderData.Mo
 
 public class ClosetProPartMapper {
 
-    public Dictionary<string, Func<Part, IProduct>> ProductNameMappings = new() {
-        { "CPS FM Vert", CreateVerticalPanelFromPart },
-        { "CPS WM Vert", CreateVerticalPanelFromPart },
-        { "FixedShelf", CreateFixedShelfFromPart },
-        { "AdjustableShelf", CreateAdjustableShelfFromPart },
-        { "Toe Kick_3.75", CreateToeKickFromPart },
-        { "Toe Kick_2.5", CreateToeKickFromPart },
-        { "Cleat", CreateCleatPart }
-    };
-    
-    public IProduct? CreateProductFromPart(Part part) {
+    public Dictionary<string, Func<Part, IProduct>> ProductNameMappings { get; }
+    public Dictionary<string, Dimension> FrontHardwareSpreads { get; }
+    public Dimension HardwareSpread { get; set; } = Dimension.Zero;
 
-        if (ProductNameMappings.TryGetValue(part.ExportName, out var mapper)) {
-            return mapper(part);
+    private readonly ComponentBuilderFactory _factory;
+
+    public ClosetProPartMapper(ComponentBuilderFactory factory) {
+
+        _factory = factory;
+
+        ProductNameMappings = new() {
+            { "CPS FM Vert", CreateVerticalPanelFromPart },
+            { "CPS WM Vert", CreateVerticalPanelFromPart },
+            { "FixedShelf", CreateFixedShelfFromPart },
+            { "AdjustableShelf", CreateAdjustableShelfFromPart },
+            { "ShoeShelf", CreateShoeShelfFromPart },
+            { "Toe Kick_3.75", CreateToeKickFromPart },
+            { "Toe Kick_2.5", CreateToeKickFromPart },
+            { "Cleat", CreateCleatPart },
+            { "Melamine Sidemount", CreateDoweledDrawerBox },
+            { "Melamine Undermount", CreateDoweledDrawerBox },
+            { "Dovetail Sidemount", CreateDovetailDrawerBox },
+            { "Dovetail Undermount", CreateDovetailDrawerBox },
+            { "Scoop Front Box", CreateDovetailDrawerBox },
+            { "Zargen", CreateZargenDrawerBox },
+            { "Slab", CreateSlabFront }
+        };
+
+        FrontHardwareSpreads = new();
+
+    }
+    
+    public List<IProduct> MapPartsToProducts(IEnumerable<Part> parts) {
+
+        List<IProduct> products = new();
+        var enumerator = parts.GetEnumerator();
+        while (enumerator.MoveNext()) {
+
+            var part = enumerator.Current;
+
+            if (part.PartName == "Cab Door Rail") {
+
+                if (!enumerator.MoveNext()) {
+                    throw new InvalidOperationException("Unexpected end of part list");
+                }
+
+                var insertPart = enumerator.Current;
+                if (insertPart.PartName != "Cab Door Insert") {
+                    throw new InvalidOperationException("Cab door rail part found without cab door insert");
+                }
+
+                products.Add(CreateFrontFromParts(part, insertPart));
+                continue;
+
+            }
+
+            if (ProductNameMappings.TryGetValue(part.ExportName, out var mapper)) {
+                products.Add(mapper(part));
+            } else {
+                throw new InvalidOperationException($"Unexpected part {part.PartName} / {part.ExportName}");
+            }
+
         }
 
-        throw new InvalidOperationException($"Unexpected part {part.PartName} / {part.ExportName}");
+        return products;
 
     }
 
-    public AdditionalItem CreateItemFromPart(BuyOutPart part) {
+    public List<AdditionalItem> MapPickListToItems(IEnumerable<PickPart> parts, out Dimension hardwareSpread) {
 
-        if (!TryParseMoneyString(part.PartCost, out decimal unitPrice)) {
-            unitPrice = 0M;
+        hardwareSpread = Dimension.Zero;
+
+        List<Dimension> spreads = new();
+
+        List<AdditionalItem> items = new();
+        foreach (var item in parts) {
+
+            if (!TryParseMoneyString(item.Cost, out var cost)) {
+                cost = 0;
+            }
+
+            items.Add(new(Guid.NewGuid(), $"({item.Quantity}) {item.Name}", cost));
+
+            if (item.Type == "Pull/Knob") {
+                // TODO: check pick list for "Pull/Knob" part types, if there is only one then the drilling spacing for drawer fronts can be inferred from that, if there are multiple then spacing cannot be inferred 
+                if (FrontHardwareSpreads.TryGetValue(item.Name, out Dimension spread)) {
+                    spreads.Add(spread);
+                }
+            }
+
         }
 
-        if (part.ExportName == "Oval Chrome") {
-            return new AdditionalItem(Guid.NewGuid(), $"{part.PartName} - {part.Color} - {part.Width}\"L", unitPrice);
+        if (spreads.Count == 1) {
+            hardwareSpread = spreads[0];
+        } else {
+            // Hardware spread is unknown
+            hardwareSpread = Dimension.Zero;
         }
 
-        return new AdditionalItem(Guid.NewGuid(), part.PartName, unitPrice);
+        return items;
 
+    }
+
+    public List<AdditionalItem> MapAccessoriesToItems(IEnumerable<Accessory> accessories) {
+ 
+        List<AdditionalItem> items = new();
+
+        foreach (var accessory in accessories) {
+
+            if (!TryParseMoneyString(accessory.Cost, out var cost)) {
+                cost = 0;
+            }
+
+            items.Add(new(Guid.NewGuid(), $"({accessory.Quantity}) {accessory.Name}", cost));
+
+        }
+
+        return items;
+
+    }
+
+    public List<AdditionalItem> MapBuyOutPartsToItems(IEnumerable<BuyOutPart> parts) {
+
+        List<AdditionalItem> items = new();
+
+        foreach (var part in parts) { 
+
+            if (!TryParseMoneyString(part.PartCost, out decimal unitPrice)) {
+                unitPrice = 0M;
+            }
+            
+            if (part.PartName == "Hang Rod") {
+                items.Add(new AdditionalItem(Guid.NewGuid(), $"{part.PartName} - {part.Color} - {part.Width}\"L", unitPrice));
+            } else {
+                items.Add(new AdditionalItem(Guid.NewGuid(), part.PartName, unitPrice));
+            }
+            
+        }
+
+        return items;
+
+    }
+
+    public IProduct CreateFrontFromParts(Part rail, Part insert) {
+        // Doors are described in two parts a "Rail" and an "Insert"
+        throw new NotImplementedException();
     }
 
     public static IProduct CreateVerticalPanelFromPart(Part part) {
@@ -155,6 +270,28 @@ public class ClosetProPartMapper {
 
     }
 
+    public static IProduct CreateShoeShelfFromPart(Part part) {
+
+        if (!TryParseMoneyString(part.PartCost, out decimal unitPrice)) {
+            unitPrice = 0M;
+        }
+        string room = GetRoomName(part);
+        string sku = "SS12-TAG"; // TODO: create a shoe shelf that works for any depth
+        Dimension width = Dimension.FromInches(part.Depth);
+        Dimension length = Dimension.FromInches(part.Width);
+        ClosetMaterial material = new(part.Color, ClosetMaterialCore.ParticleBoard);
+        ClosetPaint? paint = null;
+        string edgeBandingColor = part.InfoRecords
+                                .Where(i => i.PartName == "Edge Banding")
+                                .Select(i => i.Color)
+                                .FirstOrDefault() ?? part.Color;
+        string comment = "";
+        Dictionary<string, string> parameters = new();
+
+        return new ClosetPart(Guid.NewGuid(), part.Quantity, unitPrice, part.PartNum, room, sku, width, length, material, paint, edgeBandingColor, comment, parameters);
+
+    }
+
     public static IProduct CreateCleatPart(Part part) {
 
         if (!TryParseMoneyString(part.PartCost, out decimal unitPrice)) {
@@ -178,11 +315,85 @@ public class ClosetProPartMapper {
 
     }
 
-    public static IProduct CreateDrawerFace(Part part) {
-        throw new NotImplementedException();
+    public IProduct CreateSlabFront(Part part) {
+
+        if (!TryParseMoneyString(part.PartCost, out decimal unitPrice)) {
+            unitPrice = 0M;
+        }
+        string room = GetRoomName(part);
+        ClosetMaterial material = new(part.Color, ClosetMaterialCore.ParticleBoard);
+        ClosetPaint? paint = null;
+        string edgeBandingColor = part.Color;
+        string comment = "";
+        Dictionary<string, string> parameters = new();
+
+        string sku;
+        Dimension length;
+        Dimension width;
+        if (part.PartName == "Cab Door Insert") {
+            sku = "DOOR";
+            length = Dimension.FromInches(part.Height);
+            width = Dimension.FromInches(part.Width);
+        } else {
+            sku = "DF-XX";
+            length = Dimension.FromInches(part.Width);
+            width = Dimension.FromInches(part.Height);
+            parameters.Add("PullCenters", HardwareSpread.AsMillimeters().ToString());
+        }
+
+        return new ClosetPart(Guid.NewGuid(), part.Quantity, unitPrice, part.PartNum, room, sku, width, length, material, paint, edgeBandingColor, comment, parameters);
+
     }
 
-    public static IProduct CreateDrawerBox(Part part) {
+    public static IProduct CreateDoweledDrawerBox(Part part) {
+
+        if (!TryParseMoneyString(part.PartCost, out decimal unitPrice)) {
+            unitPrice = 0M;
+        }
+        string room = GetRoomName(part);
+        ClosetMaterial material = new(part.Color, ClosetMaterialCore.ParticleBoard);
+        ClosetPaint? paint = null;
+        string edgeBandingColor = part.Color;
+        string comment = "";
+        Dictionary<string, string> parameters = new();
+        Dimension length = Dimension.FromInches(part.Height);
+        Dimension width = Dimension.FromInches(part.Width);
+
+        return new ClosetPart(Guid.NewGuid(), part.Quantity, unitPrice, part.PartNum, room, "Mela Side Mount", width, length, material, paint, edgeBandingColor, comment, parameters);
+
+    }
+
+    public IProduct CreateDovetailDrawerBox(Part part) {
+
+        string notch = "Standard Notch";
+        DrawerSlideType slideType = DrawerSlideType.UnderMount;
+        if (part.PartName.Contains("Sidemount")) {
+            slideType = DrawerSlideType.SideMount;
+            notch = "No Notch";
+        }
+
+        if (!TryParseMoneyString(part.PartCost, out decimal unitPrice)) {
+            unitPrice = 0M;
+        }
+        string room = GetRoomName(part);
+        string materialName = DrawerBoxOptions.FINGER_JOINT_BIRCH;
+        string bottomMaterial = "1/4\" Ply";
+        string clips = "Blum";
+        string accessory = "None";
+        bool scoopFront = part.PartName == "Scoop Front Box";
+
+        return _factory.CreateDovetailDrawerBoxBuilder()
+            .WithOptions(new(materialName, materialName, materialName, bottomMaterial, clips, notch, accessory, LogoPosition.None, scoopFront:scoopFront))
+            .WithDrawerFaceHeight(Dimension.FromInches(part.Height))
+            .WithInnerCabinetWidth(Dimension.FromInches(part.Width), 1, slideType)
+            .WithInnerCabinetDepth(Dimension.FromInches(part.Depth), slideType, false)
+            .WithQty(part.Quantity)
+            .WithProductNumber(part.PartNum)
+            .BuildProduct(unitPrice, room);
+
+    }
+
+    public static IProduct CreateZargenDrawerBox(Part part) {
         throw new NotImplementedException();
     }
 
