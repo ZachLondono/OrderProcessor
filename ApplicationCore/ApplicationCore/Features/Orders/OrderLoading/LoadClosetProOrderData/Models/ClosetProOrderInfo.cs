f5e9321344f75957693e124ebd2ee1d50fe1e59c
ZachLondono <zachary.londono5@gmail.com>
2023-07-18 16:30:07 -0400

    Added buy out parts to closet pro order info

--- ApplicationCore/ApplicationCore/Features/Orders/OrderLoading/LoadClosetProOrderData/Models/ClosetProOrderInfo.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/OrderLoading/LoadClosetProOrderData/Models/ClosetProOrderInfo.cs
@@ -1,3 +1,3 @@
 ﻿namespace ApplicationCore.Features.Orders.OrderLoading.LoadClosetProOrderData.Models;
 
-public record ClosetProOrderInfo(OrderHeader Header, List<Part> Parts, List<PickPart> PickList, List<Accessory> Accessories);
+public record ClosetProOrderInfo(OrderHeader Header, List<Part> Parts, List<PickPart> PickList, List<Accessory> Accessories, List<BuyOutPart> BuyOutParts);
