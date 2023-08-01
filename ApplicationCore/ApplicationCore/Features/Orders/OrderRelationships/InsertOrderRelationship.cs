a69abd6f42a89d1ca951cc4d423b845a658690c0
ZachLondono <zachary.londono5@gmail.com>
2023-08-01 15:17:59 -0400

    Check that relationship does not already exist before trying to insert

--- ApplicationCore/ApplicationCore/Features/Orders/OrderRelationships/InsertOrderRelationship.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/OrderRelationships/InsertOrderRelationship.cs
@@ -20,6 +20,17 @@ internal class InsertOrderRelationship {
 
             using var connection = await _factory.CreateConnection();
 
+            var result = await connection.QueryFirstOrDefaultAsync<int>(
+                """
+                SELECT EXISTS (SELECT 1 FROM order_relationships WHERE (order_1_id = @Order1Id AND order_2_id = @Order2Id) OR (order_1_id = @Order2Id AND order_2_id = @Order1Id)); 
+                """,
+                command);
+
+            if (result is int n && n == 1) {
+                // Relationship already exists
+                return Response.Success();
+            }
+
             await connection.ExecuteAsync(
                 """
                 INSERT INTO order_relationships (order_1_id, order_2_id) VALUES (@Order1Id, @Order2Id);
