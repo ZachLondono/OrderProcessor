2d0f43dad332866e3aa9030649b5729f87ccc751
ZachLondono <zachary.londono5@gmail.com>
2023-08-01 13:55:51 -0400

    Added tests for Five Piece Door persistance

---
+++ ApplicationCore/ApplicationCore.Tests.Unit/Orders/Persistence/FivePieceDoorProduct.cs
@@ -0,0 +1,20 @@
+﻿using ApplicationCore.Features.Orders.Shared.Domain.Products.Doors;
+using ApplicationCore.Shared.Domain;
+
+namespace ApplicationCore.Tests.Unit.Orders.Persistence;
+
+public partial class OrderTests {
+
+    [Fact]
+    public void InsertOrderWithFivePieceDoor() {
+        var door = new FivePieceDoorProduct(Guid.NewGuid(), 1, 0M, 1, "", Dimension.FromInches(12), Dimension.FromInches(15), new(Dimension.FromInches(3)), Dimension.FromInches(0.75), Dimension.FromInches(0.25), "MDF");
+        InsertAndQueryOrderWithProduct(door);
+    }
+
+    [Fact]
+    public void DeleteOrderWithFivePieceDoor() {
+        var door = new FivePieceDoorProduct(Guid.NewGuid(), 1, 0M, 1, "", Dimension.FromInches(12), Dimension.FromInches(15), new(Dimension.FromInches(3)), Dimension.FromInches(0.75), Dimension.FromInches(0.25), "MDF");
+        InsertAndDeleteOrderWithProduct(door);
+    }
+
+}
