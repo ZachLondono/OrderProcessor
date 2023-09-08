0fd3021d62863f9dcbb58fc7a56328d3d28ee0ca
ZachLondono <zachary.londono5@gmail.com>
2023-09-07 20:02:43 -0400

    Added tests for customer persistance

---
+++ ApplicationCore/ApplicationCore.Tests.Unit/Companies/TestCompaniesConnectionFactory.cs
@@ -0,0 +1,35 @@
+﻿using ApplicationCore.Shared.Data.Companies;
+using Dapper;
+using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace ApplicationCore.Tests.Unit.Companies;
+
+public class TestCompaniesConnectionFactory : ICompaniesDbConnectionFactory {
+
+    private TestCompaniesConnection? _connection = null;
+    private readonly string _schemaFilePath;
+
+    public TestCompaniesConnectionFactory(string schemaFilePath) {
+        _schemaFilePath = schemaFilePath;
+    }
+
+    public Task<IDbConnection> CreateConnection() {
+        
+        if (_connection is null) {
+
+            var schema = File.ReadAllText(_schemaFilePath);
+
+            var connection = new SqliteConnection("Data Source=:memory:");
+
+            _connection = new(connection);
+
+            _connection.Execute(schema);
+
+        }
+
+        return Task.FromResult((IDbConnection)_connection);
+
+    }
+
+}
