16815525c82bb5fd435ae190dc67d40b4ea40f42
ZachLondono <zachary.londono5@gmail.com>
2023-09-20 19:43:38 -0400

    Added feature to copy bricscad drawings to working directory

---
+++ ApplicationCore/ApplicationCore/Features/BricsCAD/SaveDrawing/SaveOpenDrawings.cs
@@ -0,0 +1,100 @@
+﻿using ApplicationCore.Infrastructure.Bus;
+using ApplicationCore.Shared.Services;
+using BricscadApp;
+
+namespace ApplicationCore.Features.BricsCAD.SaveDrawing;
+
+internal class SaveOpenDrawings {
+
+    public record SaveItem(string DocumentName, string FileName);
+
+    public record Command(IEnumerable<SaveItem> Items) : ICommand;
+
+    public class Handler : CommandHandler<Command> {
+
+        public override Task<Response> Handle(Command command) {
+
+            AcadApplication? app;
+
+            try {
+
+                app = BricsCADApplicationRetriever.GetAcadApplication();
+
+            } catch (Exception ex) {
+
+                return Task.FromResult(Response.Error(new Error() {
+                    Title = "Failed to Access BricsCAD Application",
+                    Details = ex.Message
+                }));
+
+            }
+
+            if (app is null) {
+
+                return Task.FromResult(Response.Error(new Error() {
+                    Title = "Failed to Access BricsCAD Application",
+                    Details = "BricsCAD may not be running"
+                }));
+
+            }
+
+            List<string> unsavedDocuments = new();
+
+            AcadDocuments? allDocuments;
+            
+            try {
+
+                allDocuments = app.Documents;
+
+            } catch (Exception ex) {
+
+                return Task.FromResult(Response.Error(new Error() {
+                    Title = "Failed to Access BricsCAD Documents",
+                    Details = ex.Message
+                }));
+
+            }
+
+            foreach (var item in command.Items) {
+
+                bool wasSaved = false;
+
+                try {
+
+                    foreach (var documentObj in allDocuments) {
+
+                        if (documentObj is not AcadDocument document) continue;
+
+                        if (document.Name != item.DocumentName) continue;
+
+                        document.SaveAs(item.FileName);
+                        wasSaved = true;
+                        
+                    }
+
+                } catch (Exception ex) {
+
+                    // TODO: log exception
+
+                }
+
+                if (!wasSaved) unsavedDocuments.Add(item.DocumentName);
+
+            }
+
+            if (unsavedDocuments.Any()) {
+
+                return Task.FromResult(Response.Error(new Error() {
+                    Title = "Some or All Documents Could Not Be Saved",
+                    Details = $"The following documents where not saved: {string.Join(", ", unsavedDocuments)}"
+                }));
+
+            }
+            
+            return Task.FromResult(Response.Success());
+
+        }
+
+    }
+
+}
---
+++ ApplicationCore/ApplicationCore/Shared/Services/BricsCADApplicationRetriever.cs
@@ -0,0 +1,59 @@
+﻿using BricscadApp;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security;
+namespace ApplicationCore.Shared.Services;
+
+public class BricsCADApplicationRetriever {
+
+    internal const string strProgId = "BricscadApp.AcadApplication.20.0";
+    internal const string OLEAUT32 = "oleaut32.dll";
+    internal const string OLE32 = "ole32.dll";
+
+    public static AcadApplication? GetAcadApplication() {
+        return GetActiveObject(strProgId) as AcadApplication;
+    }
+
+
+    [System.Security.SecurityCritical]  // auto-generated_required
+    private static object? GetActiveObject(string progID) {
+        object? obj = null;
+        Guid clsid;
+
+        // Call CLSIDFromProgIDEx first then fall back on CLSIDFromProgID if
+        // CLSIDFromProgIDEx doesn't exist.
+        try {
+            CLSIDFromProgIDEx(progID, out clsid);
+        } catch (Exception) {
+            CLSIDFromProgID(progID, out clsid);
+        }
+
+        GetActiveObject(ref clsid, IntPtr.Zero, out obj);
+        return obj;
+    }
+
+    //[DllImport(Microsoft.Win32.Win32Native.OLE32, PreserveSig = false)]
+    [DllImport(OLE32, PreserveSig = false)]
+    [ResourceExposure(ResourceScope.None)]
+    [SuppressUnmanagedCodeSecurity]
+    [System.Security.SecurityCritical]  // auto-generated
+    private static extern void CLSIDFromProgIDEx([MarshalAs(UnmanagedType.LPWStr)] String progId, out Guid clsid);
+
+    //[DllImport(Microsoft.Win32.Win32Native.OLE32, PreserveSig = false)]
+    [DllImport(OLE32, PreserveSig = false)]
+    [ResourceExposure(ResourceScope.None)]
+    [SuppressUnmanagedCodeSecurity]
+    [System.Security.SecurityCritical]  // auto-generated
+    private static extern void CLSIDFromProgID([MarshalAs(UnmanagedType.LPWStr)] String progId, out Guid clsid);
+
+    //[DllImport(Microsoft.Win32.Win32Native.OLEAUT32, PreserveSig = false)]
+    [DllImport(OLEAUT32, PreserveSig = false)]
+    [ResourceExposure(ResourceScope.None)]
+    [SuppressUnmanagedCodeSecurity]
+    [System.Security.SecurityCritical]  // auto-generated
+    private static extern void GetActiveObject(ref Guid rclsid, IntPtr reserved, [MarshalAs(UnmanagedType.Interface)] out Object ppunk);
+
+
+
+}
+
