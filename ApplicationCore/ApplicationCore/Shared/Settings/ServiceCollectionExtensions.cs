1eebcdb16a8239076f211a47234e263811688461
ZachLondono <zachary.londono5@gmail.com>
2023-09-30 14:19:56 -0400

    Added writeable options to update json configuration files

---
+++ ApplicationCore/ApplicationCore/Shared/Settings/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace ApplicationCore.Shared.Settings;
+
+public static class ServiceCollectionExtensions {
+    public static IServiceCollection ConfigureWritable<T>(this IServiceCollection services, IConfigurationSection section, string file = "appsettings.json") where T : class, new() {
+
+        return services
+            .Configure<T>(section)
+            .AddTransient<IWritableOptions<T>>(provider => {
+                var options = provider.GetRequiredService<IOptionsMonitor<T>>();
+                return new WritableOptions<T>(options, section.Key, file);
+            });
+
+    }
+}
