89650d310956760ba2191da1d367a3d29dd738cb
ZachLondono <zachary.londono5@gmail.com>
2023-10-09 12:00:03 -0400

    Added supply options class

---
+++ ApplicationCore/ApplicationCore/Features/Orders/Shared/Domain/ValueObjects/SupplyOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace ApplicationCore.Features.Orders.Shared.Domain.ValueObjects;
+
+public class SupplyOptions {
+
+    public bool IncludeHinges { get; set; } = true;
+    public bool IncludeSlides { get; set; } = true;
+    public bool IncludeShelfPins { get; set; } = true;
+    public bool IncludeCams { get; set; } = true;
+    public bool IncludeCabinetLegs { get; set; } = true;
+
+}
