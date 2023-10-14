9b7f7847fea06be14f76414d797a5f8c5d7cda82
ZachLondono <zachary.londono5@gmail.com>
2023-10-13 22:46:10 -0400

    Added collapsable component

---
+++ ApplicationCore/ApplicationCore/Shared/Components/Collapsable.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ApplicationCore.Shared.Components;
+
+public partial class Collapsable {
+
+    [Parameter]
+    public RenderFragment? Summary { get; set; }
+
+    [Parameter]
+    public RenderFragment? Content { get; set; }
+
+    [Parameter]
+    public bool IsHidden { get; set; } = true;
+
+    [Parameter]
+    public EventCallback<bool> IsHiddenChanged { get; set; }
+
+    private async Task Toggle() {
+        IsHidden = !IsHidden;
+        await IsHiddenChanged.InvokeAsync(IsHidden);
+        StateHasChanged();
+    } 
+    
+}
