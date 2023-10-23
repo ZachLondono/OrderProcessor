b55948ff9228411aa6f78cfaaabf27f67bad6821
ZachLondono <zachary.londono5@gmail.com>
2023-10-22 22:09:46 -0400

    Updated closet order feature

--- ApplicationCore/ApplicationCore/DependencyInjection.cs
+++ ApplicationCore/ApplicationCore/DependencyInjection.cs
@@ -1,6 +1,7 @@
-﻿using ApplicationCore.Features.Companies.AllmoxyId;
+﻿using ApplicationCore.Features.ClosetOrderSelector;
+using ApplicationCore.Features.Companies.AllmoxyId;
+using ApplicationCore.Features.CreateOrderRelationship;
 using ApplicationCore.Features.Orders.ProductDrawings.ViewModels;
-using ApplicationCore.Widgets.Orders.OrderRelationshipList;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ApplicationCore;
@@ -10,7 +11,9 @@ public static class DependencyInjection {
     public static IServiceCollection AddViewModels(this IServiceCollection services) {
         return services
                 .ProductDrawingsViewModels()
-                .AddTransient<CustomerAllmoxyIdViewModel>();
+                .AddTransient<CustomerAllmoxyIdViewModel>()
+                .AddTransient<CreateOrderRelationshipsViewModel>()
+                .AddTransient<ClosetOrderSelectorViewModel>();
 
     }
 
@@ -19,8 +22,7 @@ public static class DependencyInjection {
         return services
                     .AddTransient<ProductDrawingManagerViewModel>()
                     .AddTransient<ProductDrawingManagerButtonViewModel>()
-                    .AddTransient<ProductDrawingRowViewModel>()
-                    .AddTransient<OrderRelationshipListWidgetViewModel>();
+                    .AddTransient<ProductDrawingRowViewModel>();
 
     }
 
---
+++ ApplicationCore/ApplicationCore/Features/ClosetOrderSelector/ClosetOrderSelectorViewModel.cs
@@ -0,0 +1,57 @@
+﻿using ApplicationCore.Infrastructure.Bus;
+
+namespace ApplicationCore.Features.ClosetOrderSelector;
+
+public class ClosetOrderSelectorViewModel {
+
+    private readonly IBus _bus;
+
+    public Action? OnPropertyChanged { get; set; }
+
+    private List<ClosetOrder> _openClosetOrders = new();
+    public List<ClosetOrder> OpenClosetOrders {
+        get => _openClosetOrders;
+        set {
+            _openClosetOrders = value;
+            OnPropertyChanged?.Invoke();
+        }
+    }
+
+    private bool _isLoading = false; 
+    public bool IsLoading {
+        get => _isLoading;
+        set {
+            _isLoading = value;
+            OnPropertyChanged?.Invoke();
+        }
+    }
+
+    private string _error = ""; 
+    public string Error {
+        get => _error;
+        set {
+            _error = value;
+            OnPropertyChanged?.Invoke();
+        }
+    }
+
+    public ClosetOrderSelectorViewModel(IBus bus) {
+        _bus = bus;
+    }
+
+    public async Task LoadOpenOrders() {
+
+        IsLoading = true;
+
+        var response = await _bus.Send(new GetOpenClosetOrders.Query());
+        response.Match(
+            orders => _openClosetOrders = new(orders),
+            error => _error = $"{error.Title} - {error.Details}"
+        );
+        _isLoading = false;
+
+        IsLoading = false;
+
+    }
+
+}
