5d34095cc45cc3450ba7d160da520a36eb032f02
ZachLondono <zachary.londono5@gmail.com>
2023-10-24 14:34:36 -0400

    Accessories from closet pro may not have dimensions

--- ApplicationCore/ApplicationCore/Features/Orders/OrderLoading/LoadClosetProOrderData/Models/Accessory.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/OrderLoading/LoadClosetProOrderData/Models/Accessory.cs
@@ -5,9 +5,9 @@ public class Accessory {
     public string Name { get; set; } = string.Empty;
     public string ExportName { get; set; } = string.Empty;
     public string Color { get; set; } = string.Empty;
-    public double Height { get; set; }
-    public double Width { get; set; }
-    public double Depth { get; set; }
+    public string Height { get; set; } = string.Empty;
+    public string Width { get; set; } = string.Empty;
+    public string Depth { get; set; } = string.Empty;
     public int Quantity { get; set; }
     public string Cost { get; set; } = string.Empty;
 }
