01af1d08f477c9b59da90ab6b96b5d9264bd4b13
ZachLondono <zachary.londono5@gmail.com>
2023-10-26 17:01:56 -0400

    Started implementing the view models and commands for customization script manager

---
+++ ApplicationCore/ApplicationCore/Features/CustomizationScriptList/Commands/DeleteCustomizationScript.cs
@@ -0,0 +1,35 @@
+﻿using ApplicationCore.Infrastructure.Bus;
+using ApplicationCore.Shared.Data.Ordering;
+using Dapper;
+
+namespace ApplicationCore.Features.CustomizationScripts.Commands;
+
+internal class DeleteCustomizationScript {
+
+    public record Command(Guid Id) : ICommand;
+
+    public class Handler : CommandHandler<Command> {
+        
+        private readonly IOrderingDbConnectionFactory _factory;
+
+        public Handler(IOrderingDbConnectionFactory factory) {
+            _factory = factory;
+        }
+
+        public override async Task<Response> Handle(Command command) {
+
+            using var connection = await _factory.CreateConnection();
+
+            await connection.ExecuteAsync(
+                """
+                DELETE FROM order_customization_scripts
+                WHERE id = @Id;
+                """, command);
+
+            return Response.Success();
+
+        }
+
+    }
+
+}
