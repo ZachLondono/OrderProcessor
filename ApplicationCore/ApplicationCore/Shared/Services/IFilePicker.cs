5143c54a12923fbb781af25ba1228c55623f54cb
ZachLondono <zachary.londono5@gmail.com>
2023-10-26 23:10:22 -0400

    Added callback to file picker that is executed when file is not picked

--- ApplicationCore/ApplicationCore/Shared/Services/IFilePicker.cs
+++ ApplicationCore/ApplicationCore/Shared/Services/IFilePicker.cs
@@ -4,6 +4,8 @@ public interface IFilePicker {
 
     public void PickFile(FilePickerOptions options, Action<string> onFilePicked);
 
+    public void PickFile(FilePickerOptions options, Action<string> onFilePicked, Action onCancel);
+
     public void PickFiles(FilePickerOptions options, Action<string[]> onFilesPicked);
 
 }
