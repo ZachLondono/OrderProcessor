ac76a8752ee313ffa667c8245cf426baade265fd
ZachLondono <zachary.londono5@gmail.com>
2023-11-03 15:54:01 -0400

    Implemented writing doweled db cut list

---
+++ ApplicationCore/ApplicationCore/Features/Orders/OrderRelease/Handlers/DoweledDrawerBoxCutList/DoweledDrawerBoxCutList.cs
@@ -0,0 +1,14 @@
+﻿namespace ApplicationCore.Features.Orders.OrderRelease.Handlers.DoweledDrawerBoxCutList;
+
+public class DoweledDrawerBoxCutList {
+
+    public required string CustomerName { get; set; }
+    public required string VendorName { get; set; }
+    public required string OrderNumber { get; set; }
+    public required string OrderName { get; set; }
+    public required DateTime OrderDate { get; set; }
+    public required IEnumerable<DoweledDBCutListLineItem> Items { get; set; }
+    public required string Material { get; set; }
+    public required string Note { get; set; }
+
+}
---
+++ ApplicationCore/ApplicationCore/Features/Orders/OrderRelease/Handlers/DoweledDrawerBoxCutList/DoweledDrawerBoxCutListWriter.cs
@@ -0,0 +1,152 @@
+﻿using ApplicationCore.Shared.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using ExcelApp = Microsoft.Office.Interop.Excel.Application;
+
+namespace ApplicationCore.Features.Orders.OrderRelease.Handlers.DoweledDrawerBoxCutList;
+
+public class DoweledDrawerBoxCutListWriter : IDoweledDrawerBoxCutListWriter {
+
+    private readonly IFileReader _fileReader;
+    //private readonly DoweledDrawerBoxCutListSettings _settings;
+    private readonly ILogger<DoweledDrawerBoxCutListWriter> _logger;
+
+    public Action<string>? OnError { get; set; }
+
+    public DoweledDrawerBoxCutListWriter(IFileReader fileReader, ILogger<DoweledDrawerBoxCutListWriter> logger) {
+        _fileReader = fileReader;
+        _logger = logger;
+    }
+
+    public DoweledDBCutListResult? WriteCutList(DoweledDrawerBoxCutList cutList, string outputDirectory, bool generatePDF) {
+
+        string templateFilePath = @"C:\Users\Zachary Londono\Documents\Doweled DB Cut List Template.xlsx";
+
+        if (!File.Exists(templateFilePath)) {
+            OnError?.Invoke($"Doweled drawer box cut list template does not exist or cannot be accessed - '{templateFilePath}'");
+            return null;
+
+        }
+
+        ExcelApp app;
+        Workbook workbook;
+        Worksheet sheet;
+        try {
+
+            app = new() {
+                DisplayAlerts = false,
+                Visible = false
+            };
+
+            workbook = app.Workbooks.Open(templateFilePath);
+            sheet = workbook.Worksheets["Cut List"];
+
+        } catch (Exception ex) {
+
+            _logger.LogError(ex, "Exception thrown while opening doweled drawer Box cut list template");
+            OnError?.Invoke("Failed to access doweled drawer Box cut list workbook template");
+            return null;
+
+        }
+
+        var fileName = _fileReader.GetAvailableFileName(outputDirectory, $"{cutList.OrderNumber} Doweled Drawer Box CUTLIST - {cutList.Material}", "xlsx");
+        var fullFilePath = Path.Combine(outputDirectory, fileName);
+        try {
+
+            WriteHeader(sheet, cutList);
+            var lastRow = WriteLineItems(sheet, cutList.Items);
+
+            sheet.PageSetup.PrintArea = $"A1:F{lastRow}";
+
+            workbook.SaveAs(fullFilePath);
+
+        } catch (Exception ex) {
+
+            _logger.LogError(ex, "Exception thrown while writing doweled drawer Box cut list");
+            OnError?.Invoke("Failed write doweled drawer Box cut list");
+            return null;
+
+        }
+
+        string? pdfFilePath = null;
+        if (generatePDF) {
+
+            try {
+                pdfFilePath = ExportToPDF(workbook, outputDirectory, cutList.OrderNumber);
+            } catch (Exception ex) {
+                _logger.LogError(ex, "Exception thrown while exporting doweled drawer Box cut list PDF");
+                OnError?.Invoke("Failed to export doweled drawer Box cut list to pdf");
+            }
+
+        }
+
+        try {
+
+            workbook.Close();
+            app.Quit();
+
+            if (workbook is not null) _ = Marshal.ReleaseComObject(workbook);
+            if (app is not null) _ = Marshal.ReleaseComObject(app);
+
+            // Clean up COM objects, calling these twice ensures it is fully cleaned up.
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+        } catch (Exception ex) {
+
+            _logger.LogError(ex, "Exception thrown wile releasing Excel com objects, after generating doweled drawer Box cut list");
+            OnError?.Invoke("Error occurred while closing doweled drawer Box cut list");
+
+        }
+
+        return new(fullFilePath, pdfFilePath);
+
+    }
+
+    private static void WriteHeader(Worksheet sheet, DoweledDrawerBoxCutList cutList) {
+
+        sheet.Range["CustomerName"].Value2 = cutList.CustomerName;
+        sheet.Range["VendorName"].Value2 = cutList.VendorName;
+        sheet.Range["OrderNumber"].Value2 = cutList.OrderNumber;
+        sheet.Range["OrderName"].Value2 = cutList.OrderName;
+        sheet.Range["OrderDate"].Value2 = cutList.OrderDate.ToShortDateString();
+        sheet.Range["TotalBoxCount"].Value2 = cutList.Items.Count();
+        sheet.Range["Material"].Value2 = cutList.Material;
+        sheet.Range["OrderNote"].Value2 = cutList.Note;
+
+    }
+
+    private static int WriteLineItems(Worksheet sheet, IEnumerable<DoweledDBCutListLineItem> lineItems) {
+
+        int currentOffset = 1;
+        foreach (var item in lineItems) {
+
+            sheet.Range["CabNumCol"].Offset[currentOffset].Value2 = item.CabNumbers;
+            sheet.Range["PartNameCol"].Offset[currentOffset].Value2 = item.PartName;
+            sheet.Range["QtyCol"].Offset[currentOffset].Value2 = item.Qty;
+            sheet.Range["WidthCol"].Offset[currentOffset].Value2 = item.Width;
+            sheet.Range["LengthCol"].Offset[currentOffset].Value2 = item.Length;
+            sheet.Range["NoteCol"].Offset[currentOffset].Value2 = item.Note;
+
+            currentOffset++;
+
+        }
+
+        return sheet.Range["CabNumCol"].Offset[currentOffset].Row;
+
+    }
+
+    private string ExportToPDF(Workbook workbook, string outputDirectory, string orderNumber) {
+
+        var fileName = _fileReader.GetAvailableFileName(outputDirectory, $"{orderNumber} Doweled Drawer Box CUTLIST", "pdf");
+        var fullFilePath = Path.Combine(outputDirectory, fileName);
+        workbook.ExportAsFixedFormat2(XlFixedFormatType.xlTypePDF, fullFilePath);
+
+        return fullFilePath;
+
+    }
+
+}
