a379e17b4433a73948e010bec891feaa66787470
ZachLondono <zachary.londono5@gmail.com>
2023-11-07 18:15:33 -0500

    Added provider class

---
+++ ApplicationCore/ApplicationCore/Features/Orders/OrderLoading/LoadHafeleDBSpreadsheetOrderData/HafeleDBSpreadSheetOrderProvider.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Features.Orders.OrderLoading.Dialog;
+using ApplicationCore.Features.Orders.OrderLoading.Models;
+
+namespace ApplicationCore.Features.Orders.OrderLoading.LoadHafeleDBSpreadsheetOrderData;
+
+internal class HafeleDBSpreadSheetOrderProvider : IOrderProvider {
+
+    public IOrderLoadWidgetViewModel? OrderLoadingViewModel { get; set; }
+
+    public HafeleDBSpreadSheetOrderProvider() {
+
+    }
+
+    public Task<OrderData?> LoadOrderData(string source) {
+        throw new NotImplementedException();
+    }
+
+}
