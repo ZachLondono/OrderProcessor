1d460dcfca76e704d81764defe0d8af490089433
ZachLondono <zachary.londono5@gmail.com>
2023-11-08 17:54:21 -0500

    Added a settings file for loading doweled db order from hafele spreadsheet

---
+++ ApplicationCore/ApplicationCore/Shared/Settings/HafeleDBOrderProviderSettings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ApplicationCore.Shared.Settings;
+
+public class HafeleDBOrderProviderSettings {
+
+    [ConfigurationKeyName("vendor_id")]
+    public Guid VendorId { get; set; }
+
+    [ConfigurationKeyName("front_back_thickness_mm")]
+    public double FrontBackThicknessMM { get; set; }
+
+    [ConfigurationKeyName("side_thickness_mm")]
+    public double SideThicknessMM { get; set; }
+
+    [ConfigurationKeyName("bottom_thickness_mm")]
+    public double BottomThicknessMM { get; set; }
+
+    [ConfigurationKeyName("front_back_height_adj_mm")]
+    public double FrontBackHeightAdjMM { get; set; }
+
+}
