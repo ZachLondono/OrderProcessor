50bf349ccfe378641ed4e93cf4c7d563f0c838e5
ZachLondono <zachary.londono5@gmail.com>
2023-11-09 15:15:55 -0500

    Implemented button to choose hafele order sheet and load order from it

--- ApplicationCore/ApplicationCore/Features/Orders/OrderLoading/Models/OrderProviderFactory.cs
+++ ApplicationCore/ApplicationCore/Features/Orders/OrderLoading/Models/OrderProviderFactory.cs
@@ -5,6 +5,7 @@ using ApplicationCore.Features.Orders.OrderLoading.LoadClosetProOrderData.LoadCl
 using ApplicationCore.Features.Orders.OrderLoading.LoadClosetProOrderData.LoadClosetProWebOrderData;
 using ApplicationCore.Features.Orders.OrderLoading.LoadDoorSpreadsheetOrderData;
 using ApplicationCore.Features.Orders.OrderLoading.LoadDoweledDBSpreadsheetOrderData;
+using ApplicationCore.Features.Orders.OrderLoading.LoadHafeleDBSpreadsheetOrderData;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ApplicationCore.Features.Orders.OrderLoading.Models;
@@ -25,6 +26,7 @@ public class OrderProviderFactory : IOrderProviderFactory {
         OrderSourceType.DoorOrder => _serviceProvider.GetRequiredService<DoorSpreadsheetOrderProvider>(),
         OrderSourceType.DoweledDBOrderForm => _serviceProvider.GetRequiredService<DoweledDBSpreadsheetOrderProvider>(),
         OrderSourceType.ClosetOrderForm => _serviceProvider.GetRequiredService<ClosetSpreadsheetOrderProvider>(),
+        OrderSourceType.HafeleDBOrderForm => _serviceProvider.GetRequiredService<HafeleDBSpreadSheetOrderProvider>(),
         _ => throw new KeyNotFoundException("No valid order provider for given order source type"),
     };
 
