e63114b198259e805a842c7846da5e4462619ec9
ZachLondono <zachary.londono5@gmail.com>
2023-12-14 16:55:55 -0500

    Fixes #142

--- ApplicationCore/ApplicationCore/Shared/CNC/WorkOrderReleaseEmail/ReleaseEmailBodyGenerator.cs
+++ ApplicationCore/ApplicationCore/Shared/CNC/WorkOrderReleaseEmail/ReleaseEmailBodyGenerator.cs
@@ -39,14 +39,16 @@ public class ReleaseEmailBodyGenerator {
                 <br />
                 <br />
 
-                <table>
-                    <tr>
-                        <td style="border: 1px solid black; padding: 5px;">
-                            <div style="font-weight: bold;">Note:</div>
-                            <div style="white-space: pre-wrap;">{model.Note}</div>
-                        </td>
-                    </tr>
-                </table>
+                <div>
+                    <table>
+                        <tr>
+                            <td style="border: 1px solid black; padding: 5px;">
+                                <div style="font-weight: bold;">Note:</div>
+                                <div style="white-space: pre-wrap;">{model.Note}</div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
 
                 """;
 
@@ -62,20 +64,21 @@ public class ReleaseEmailBodyGenerator {
             string tableHeader =
                 $"""
 
-                <br />
-                <br />
+                    <br />
+                    <br />
 
-                <table style="border: 1px solid black;">
+                    <div>
+                        <table style="border: 1px solid black;">
 
-                    <caption><b>{job.JobName} Materials</b></caption>
+                            <caption><b>{job.JobName} Materials</b></caption>
 
-                    <tr style="border: 1px solid black; border-collapse: collapse;">
-                        <th style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">Qty</th>
-                        <th style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">Name</th>
-                        <th style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">Width</th>
-                        <th style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">Length</th>
-                        <th style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">Thickness</th>
-                    </tr>
+                            <tr style="border: 1px solid black; border-collapse: collapse;">
+                                <th style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">Qty</th>
+                                <th style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">Name</th>
+                                <th style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">Width</th>
+                                <th style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">Length</th>
+                                <th style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">Thickness</th>
+                            </tr>
 
                 """;
 
@@ -84,13 +87,13 @@ public class ReleaseEmailBodyGenerator {
                 tableBody +=
                     $"""
 
-                        <tr style="border: 1px solid black; border-collapse: collapse;">
-                            <td style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">{material.Qty}</td>
-                            <td style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">{material.Name}</td>
-                            <td style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">{material.Width}</td>
-                            <td style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">{material.Length}</td>
-                            <td style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">{material.Thickness}</td>
-                        </tr>
+                                <tr style="border: 1px solid black; border-collapse: collapse;">
+                                    <td style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">{material.Qty}</td>
+                                    <td style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">{material.Name}</td>
+                                    <td style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">{material.Width}</td>
+                                    <td style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">{material.Length}</td>
+                                    <td style="border: 1px solid black; border-collapse: collapse; padding-left: 5px; padding-right:5px;">{material.Thickness}</td>
+                                </tr>
 
                     """;
 
@@ -99,7 +102,8 @@ public class ReleaseEmailBodyGenerator {
             string tableFooter =
                 """
 
-                <table>
+                        </table>
+                    </div>
                 """;
 
             body += tableHeader + tableBody + tableFooter;
