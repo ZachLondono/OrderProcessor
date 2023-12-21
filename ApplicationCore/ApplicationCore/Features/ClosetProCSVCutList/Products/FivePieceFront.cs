93b70db90a8d784d8b4c9cde90bad063fe322342
ZachLondono <zachary.londono5@gmail.com>
2023-12-21 17:50:50 -0500

    Seperated logic for creating differnt products from closet pro parts into different classes

---
+++ ApplicationCore/ApplicationCore/Features/ClosetProCSVCutList/Products/FivePieceFront.cs
@@ -0,0 +1,39 @@
+﻿using ApplicationCore.Features.Orders.Shared.Domain.Enums;
+using ApplicationCore.Features.Orders.Shared.Domain.Products;
+using ApplicationCore.Features.Orders.Shared.Domain.Products.Doors;
+using ApplicationCore.Features.Orders.Shared.Domain.ValueObjects;
+using ApplicationCore.Shared.Domain;
+
+namespace ApplicationCore.Features.ClosetProCSVCutList.Products;
+
+public class FivePieceFront {
+
+    public required int Qty { get; init; }
+    public required string Room { get; init; }
+    public required decimal UnitPrice { get; init; }
+    public required string Color { get; init; }
+    public required int PartNumber { get; init; }
+
+    public required Dimension Height { get; init; }
+    public required Dimension Width { get; init; }
+    public required DoorFrame Frame { get; init; }
+    public required DoorType Type { get; init; }
+    public required Dimension? HardwareSpread { get; init; }
+
+    public IProduct ToProduct() {
+
+        return new FivePieceDoorProduct(Guid.NewGuid(),
+                                       Qty,
+                                       UnitPrice,
+                                       PartNumber,
+                                       Room,
+                                       Width,
+                                       Height,
+                                       Frame,
+                                       Dimension.FromInches(0.75),
+                                       Dimension.FromInches(0.25),
+                                       Color);
+
+    }
+
+}
