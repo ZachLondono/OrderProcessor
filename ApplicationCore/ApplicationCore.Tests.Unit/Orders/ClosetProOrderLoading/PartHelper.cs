310baa2305bed844f0f78f0c7adc6934cdca179d
ZachLondono <zachary.londono5@gmail.com>
2023-12-21 00:19:53 -0500

    Moved part helper into seperate class

---
+++ ApplicationCore/ApplicationCore.Tests.Unit/Orders/ClosetProOrderLoading/PartHelper.cs
@@ -0,0 +1,32 @@
+﻿using ApplicationCore.Features.ClosetProCSVCutList;
+using ApplicationCore.Features.Orders.Shared.Domain.Enums;
+using ApplicationCore.Features.Orders.Shared.Domain.Products;
+using ApplicationCore.Features.Orders.Shared.Domain.Products.Closets;
+using FluentAssertions;
+
+namespace ApplicationCore.Tests.Unit.Orders.ClosetProOrderLoading;
+
+public class PartHelper {
+
+    public Part Part { get; set; } = new();
+
+    public ClosetPart CompareToProduct(IProduct product) {
+
+        product.Should().BeOfType<ClosetPart>();
+
+        var closetPart = product as ClosetPart;
+        closetPart.Should().NotBeNull();
+        closetPart!.Material.Core.Should().Be(ClosetMaterialCore.ParticleBoard);
+        closetPart.Material.Finish.Should().Be(Part.Color);
+        closetPart.Qty.Should().Be(Part.Quantity);
+        closetPart.ProductNumber.Should().Be(Part.PartNum);
+        closetPart.Room.Should().Be(ClosetProPartMapper.GetRoomName(Part));
+        if (ClosetProPartMapper.TryParseMoneyString(Part.PartCost, out decimal price)) {
+            closetPart.UnitPrice.Should().Be(price);
+        }
+
+        return closetPart;
+
+    }
+
+}
