b0e8f4c9c95f9364b7adca490eb14a6ca79fb6ce
ZachLondono <zachary.londono5@gmail.com>
2023-12-21 00:19:53 -0500

    Moved part helper into seperate class

--- ApplicationCore/ApplicationCore.Tests.Unit/Orders/ClosetProOrderLoading/ProductMappingTests.cs
+++ ApplicationCore/ApplicationCore.Tests.Unit/Orders/ClosetProOrderLoading/ProductMappingTests.cs
@@ -1,9 +1,6 @@
 ﻿using ApplicationCore.Features.ClosetProCSVCutList;
 using ApplicationCore.Features.Orders.Shared.Domain.Builders;
 using ApplicationCore.Features.Orders.Shared.Domain.Components;
-using ApplicationCore.Features.Orders.Shared.Domain.Enums;
-using ApplicationCore.Features.Orders.Shared.Domain.Products;
-using ApplicationCore.Features.Orders.Shared.Domain.Products.Closets;
 using ApplicationCore.Shared.Domain;
 using FluentAssertions;
 
@@ -463,32 +460,6 @@ public class ProductMappingTests {
         closetPart.Parameters.Should().Contain(new KeyValuePair<string, string>("FINRIGHT", "0"));
         closetPart.Parameters.Should().Contain(new KeyValuePair<string, string>("Row3Holes", "0"));
         double.Parse(closetPart.Parameters["Row1Holes"]).Should().BeApproximately(267.8, 0.15);
-
-    }
-
-    public class PartHelper {
-
-        public Part Part { get; set; } = new();
-
-        public ClosetPart CompareToProduct(IProduct product) {
-
-            product.Should().BeOfType<ClosetPart>();
-
-            var closetPart = product as ClosetPart;
-            closetPart.Should().NotBeNull();
-            closetPart!.Material.Core.Should().Be(ClosetMaterialCore.ParticleBoard);
-            closetPart.Material.Finish.Should().Be(Part.Color);
-            closetPart.Qty.Should().Be(Part.Quantity);
-            closetPart.ProductNumber.Should().Be(Part.PartNum);
-            closetPart.Room.Should().Be(ClosetProPartMapper.GetRoomName(Part));
-            if (ClosetProPartMapper.TryParseMoneyString(Part.PartCost, out decimal price)) {
-                closetPart.UnitPrice.Should().Be(price);
-            }
-
-            return closetPart;
-
-        }
-
 
     }
 
