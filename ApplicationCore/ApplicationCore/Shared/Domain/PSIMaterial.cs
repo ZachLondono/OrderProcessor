18fce327c3c16339be4fa869715a00f77db615f2
ZachLondono <zachary.londono5@gmail.com>
2024-01-17 14:30:54 -0500

    Added support for updated CADCode material name for jobs coming from PSI

--- ApplicationCore/ApplicationCore/Shared/Domain/PSIMaterial.cs
+++ ApplicationCore/ApplicationCore/Shared/Domain/PSIMaterial.cs
@@ -6,12 +6,71 @@ public partial record PSIMaterial(string Side1Color, string Side1FinishType, str
 
     public static bool TryParse(string materialName, out PSIMaterial material) {
 
+        var regex = PSIMaterialNamePatternRegexFormat2();
+
+        if (regex.IsMatch(materialName)) {
+            return TryParseFormat2(materialName, out material);
+        }
+
+        return TryParseFormat1(materialName, out material);
+
+    }
+
+    public static bool TryParseFormat2(string materialName, out PSIMaterial material) {
+
         /*
-         *  PSI Material Format
+         *  PSI Material Format 2
+         *  Grained[2 Spaces][Side 1 Color][1 Space]-[2 Spaces][Mela / Veneer][5 Spaces][Thickness w/ 2 decimal places][1 Space][PB / Ply / Flake / MDF Dr Core][2 Spaces][Side 2 Color][1 Space]-[2 Spaces][Mela / Veneer]
+         */
+
+        material = new("", "", "", "", "", 0);
+
+        materialName = CleanMaterialName(materialName);
+
+        var split1 = materialName.Split("     ");
+
+        if (split1.Length != 2) return false;
+
+        var leftSplit = split1[0].Split("  ");
+        if (leftSplit.Length != 3) return false;
+
+        if (leftSplit[0] != "Grained") return false;
+
+        string side1Color = leftSplit[1][..^1].Trim();
+        string side1Finish = leftSplit[2].Trim();
+
+        var rightSplit = split1[1].Split("  ");
+        if (rightSplit.Length != 3) return false;
+
+        string side2Color = rightSplit[1][..^1].Trim();
+        string side2Finish = rightSplit[2].Trim();
+
+        if (side1Color == string.Empty || side1Finish == string.Empty ||
+            side2Color == string.Empty || side2Finish == string.Empty) return false;
+
+        var rightSplit2 = rightSplit[0].Split(' ');
+        if (rightSplit2.Length != 2) return false;
+        string thicknessStr = rightSplit2[0];
+        string coreType = rightSplit2[1];
+
+        if (!double.TryParse(thicknessStr, out double thickness)) {
+            thickness = 0;
+        }
+
+        material = new(side1Color, side1Finish, side2Color, side2Finish, coreType, thickness);
+
+        return true;
+
+    }
+
+    public static bool TryParseFormat1(string materialName, out PSIMaterial material) {
+
+        /*
+         *  PSI Material Format 1
          *  Grained[2 Spaces][Side 1 Color][1 Space][Mela / Veneer][5 Spaces][Thickness w/ 2 decimal places][1 Space][PB / Ply / Flake / MDF Dr Core][2 Spaces][Side 2 Color][1 Space][Mela / Veneer]
          */
 
-        var regex = PSIMaterialNamePatternRegex();
+        var regex = PSIMaterialNamePatternRegexFormat1();
 
         material = new("", "", "", "", "", 0);
 
@@ -91,6 +150,9 @@ public partial record PSIMaterial(string Side1Color, string Side1FinishType, str
     }
 
     [GeneratedRegex("Grained+\\s{2}(.+)\\s{1}[A-Za-z]+\\s{5}[0-9]*\\.[0-9]+\\s{1}[A-Za-z]+\\s{2}(.+)\\s{1}[A-Za-z]+", RegexOptions.IgnoreCase, "en-US")]
-    private static partial Regex PSIMaterialNamePatternRegex();
+    private static partial Regex PSIMaterialNamePatternRegexFormat1();
+
+    [GeneratedRegex("Grained+\\s{2}(.+)\\s{1}-\\s{2}[A-Za-z]+\\s{5}[0-9]*\\.[0-9]+\\s{1}[A-Za-z]+\\s{2}(.+)\\s{1}-\\s{2}[A-Za-z]+", RegexOptions.IgnoreCase, "en-US")]
+    private static partial Regex PSIMaterialNamePatternRegexFormat2();
 
 }
