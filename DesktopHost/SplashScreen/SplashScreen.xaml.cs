151f775dd917787049d1dbeb226642c12d9fd5a9
ZachLondono <zachary.londono5@gmail.com>
2024-01-19 19:01:29 -0500

    Updated splashscreen UI

--- DesktopHost/SplashScreen/SplashScreen.xaml.cs
+++ DesktopHost/SplashScreen/SplashScreen.xaml.cs
@@ -1,23 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Windows.ApplicationModel;
 
 namespace DesktopHost;
 /// <summary>
 /// Interaction logic for SplashScreen.xaml
 /// </summary>
 public partial class SplashScreen : Window {
+    
     public SplashScreen() {
         InitializeComponent();
+        SetCurrentVersionTag();
     }
+
+    public void SetCurrentVersionTag() {
+
+#if DEBUG
+        VersionTag.Text = "DEBUG";
+#else
+        try {
+		    var version = Package.Current.Id.Version;
+            VersionTag.Text = $"{version.Major}.{version.Minor}.{version.Build}";
+        } catch {}
+#endif
+
+	}
+
 }
