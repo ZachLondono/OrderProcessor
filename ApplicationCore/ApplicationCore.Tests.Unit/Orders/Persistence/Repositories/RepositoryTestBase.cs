6d5cdb0c88b568a59fa3a80631d9a9bec452f6c1
ZachLondono <zachary.londono5@gmail.com>
2024-04-16 17:17:07 -0400

    Added some utility classes to help testing

---
+++ ApplicationCore/ApplicationCore.Tests.Unit/Orders/Persistence/Repositories/RepositoryTestBase.cs
@@ -0,0 +1,20 @@
+﻿namespace ApplicationCore.Tests.Unit.Orders.Persistence.Repositories;
+
+public abstract class RepositoryTestBase : IDisposable {
+
+    private readonly string _filePath;
+
+    protected TestFileOrderingConnectionFactory Factory { get; init; }
+
+    public RepositoryTestBase() {
+
+        _filePath = Guid.NewGuid().ToString();
+        Factory = new(_filePath);
+
+    }
+
+    public void Dispose() {
+        File.Delete(_filePath);
+    }
+
+}
---
+++ ApplicationCore/ApplicationCore.Tests.Unit/Orders/Persistence/TestFileOrderingConnectionFactory.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using Domain.Orders.Persistance;
+using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace ApplicationCore.Tests.Unit.Orders.Persistence;
+
+public class TestFileOrderingConnectionFactory(string filePath) : IOrderingDbConnectionFactory {
+
+    private static readonly SemaphoreSlim s_semaphore = new(1);
+    private readonly string _schemaFilePath = "./Application/Schemas/ordering_schema.sql";
+    private readonly string _filePath = filePath;
+
+    public async Task<IDbConnection> CreateConnection() {
+
+        s_semaphore.Wait();
+        try {
+
+            var builder = new SqliteConnectionStringBuilder {
+                DataSource = _filePath,
+                Pooling = false,
+                Cache = SqliteCacheMode.Shared
+            };
+
+            var connection = new SqliteConnection(builder.ConnectionString);
+
+            if (!File.Exists(_filePath)) {
+                await InitializeDatabase(connection, _schemaFilePath);
+            }
+
+            return connection;
+
+        } finally {
+
+            s_semaphore.Release();
+
+        }
+
+    }
+
+    private static async Task InitializeDatabase(SqliteConnection connection, string schemaFilePath) {
+
+        var schema = await File.ReadAllTextAsync(schemaFilePath);
+        await connection.ExecuteAsync(schema);
+
+    }
+
+}
