2b1efe2ca2fda3f7440777562c527d0c0361d065
ZachLondono <zachary.londono5@gmail.com>
2024-04-16 17:34:51 -0400

    Added tests for reading/writing order hardware

---
+++ ApplicationCore/ApplicationCore.Tests.Unit/Orders/Persistence/OrderDrawerSlidesPersistanceTests.cs
@@ -0,0 +1,72 @@
+﻿using ApplicationCore.Features.DeleteOrder;
+using Domain.Orders.Persistance;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Domain.Infrastructure.Data;
+using Domain.Orders.Entities.Hardware;
+using Domain.Orders.Persistance.Repositories;
+using Domain.ValueObjects;
+
+namespace ApplicationCore.Tests.Unit.Orders.Persistence;
+
+public class OrderDrawerSlidesPersistanceTests {
+
+    protected readonly IOrderingDbConnectionFactory Factory = new TestOrderingConnectionFactory("./Application/Schemas/ordering_schema.sql");
+    private readonly ILogger<InsertOrder.Handler> _logger = Substitute.For<ILogger<InsertOrder.Handler>>();
+
+    private readonly InsertOrder.Handler _insertOrder;
+    private readonly DeleteOrder.Handler _deleteOrder;
+
+    public OrderDrawerSlidesPersistanceTests() {
+        _insertOrder = new(_logger, Factory);
+        _deleteOrder = new(Factory);
+        SqlMapping.AddSqlMaps();
+    }
+
+    [Fact]
+    public async Task InsertOrder_WithDrawerSlides_ShouldInsertRowsIntoSuppliesTable() {
+
+        // Arrange
+        var slides = new DrawerSlide(Guid.NewGuid(), 1, Dimension.FromInches(21), "Hettich");
+        var order = new OrderBuilder() {
+            Hardware = new([], [slides], [])
+        }.Build();
+
+        // Act
+        var result = await _insertOrder.Handle(new(order));
+        result.OnError(error => Assert.Fail("Handler returned error"));
+
+        // Assert
+        var connection = Factory.CreateConnection().Result;
+        var repo = new OrderDrawerSlidesRepository(connection);
+        var orderSlides = await repo.GetOrderDrawerSlides(order.Id);
+        orderSlides.Should().ContainEquivalentOf(slides);
+
+    }
+
+    [Fact]
+    public async Task DeleteOrder_WithSupplies_ShouldDeleteRowsIntoSuppliesTable() {
+
+        // Arrange
+        var slides = new DrawerSlide(Guid.NewGuid(), 1, Dimension.FromInches(21), "Hettich");
+        var order = new OrderBuilder() {
+            Hardware = new([], [slides], [])
+        }.Build();
+
+        var result = await _insertOrder.Handle(new(order));
+        result.OnError(error => Assert.Fail("Handler returned error"));
+
+        // Act
+        var result2 = await _deleteOrder.Handle(new(order.Id));
+        result2.OnError(error => Assert.Fail("Handler returned error"));
+
+        // Assert
+        var connection = Factory.CreateConnection().Result;
+        var repo = new OrderDrawerSlidesRepository(connection);
+        var orderSlides = await repo.GetOrderDrawerSlides(order.Id);
+        orderSlides.Should().BeEmpty();
+
+    }
+
+}
---
+++ ApplicationCore/ApplicationCore.Tests.Unit/Orders/Persistence/OrderSuppliesPersistanceTests.cs
@@ -0,0 +1,71 @@
+﻿using ApplicationCore.Features.DeleteOrder;
+using Domain.Orders.Persistance;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Domain.Infrastructure.Data;
+using Domain.Orders.Entities.Hardware;
+using Domain.Orders.Persistance.Repositories;
+
+namespace ApplicationCore.Tests.Unit.Orders.Persistence;
+
+public class OrderSuppliesPersistanceTests {
+
+    protected readonly IOrderingDbConnectionFactory Factory = new TestOrderingConnectionFactory("./Application/Schemas/ordering_schema.sql");
+    private readonly ILogger<InsertOrder.Handler> _logger = Substitute.For<ILogger<InsertOrder.Handler>>();
+
+    private readonly InsertOrder.Handler _insertOrder;
+    private readonly DeleteOrder.Handler _deleteOrder;
+
+    public OrderSuppliesPersistanceTests() {
+        _insertOrder = new(_logger, Factory);
+        _deleteOrder = new(Factory);
+        SqlMapping.AddSqlMaps();
+    }
+
+    [Fact]
+    public async Task InsertOrder_WithSupplies_ShouldInsertRowsIntoSuppliesTable() {
+
+        // Arrange
+        var supply = new Supply(Guid.NewGuid(), 1, "Hinge Plates");
+        var order = new OrderBuilder() {
+            Hardware = new([supply], [], [])
+        }.Build();
+
+        // Act
+        var result = await _insertOrder.Handle(new(order));
+        result.OnError(error => Assert.Fail("Handler returned error"));
+
+        // Assert
+        var connection = Factory.CreateConnection().Result;
+        var repo = new OrderSuppliesRepository(connection);
+        var supplies = await repo.GetOrderSupplies(order.Id);
+        supplies.Should().ContainEquivalentOf(supply);
+
+    }
+
+    [Fact]
+    public async Task DeleteOrder_WithSupplies_ShouldDeleteRowsIntoSuppliesTable() {
+
+        // Arrange
+        var supply = new Supply(Guid.NewGuid(), 1, "Hinge Plates");
+        var order = new OrderBuilder() {
+            Hardware = new([supply], [], [])
+        }.Build();
+
+        var result1 = await _insertOrder.Handle(new(order));
+        result1.OnError(error => Assert.Fail("Handler returned error"));
+
+        // Act
+        var result2 = await _deleteOrder.Handle(new(order.Id));
+        result2.OnError(error => Assert.Fail("Handler returned error"));
+
+        // Assert
+        var connection = Factory.CreateConnection().Result;
+        var repo = new OrderSuppliesRepository(connection);
+        var supplies = await repo.GetOrderSupplies(order.Id);
+        supplies.Should().BeEmpty();
+
+    }
+
+}
