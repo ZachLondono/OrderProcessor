4dde65ad31e403ee9a9ff8d862d04187aa418248
ZachLondono <zachary.londono5@gmail.com>
2024-04-22 16:46:15 -0400

    Added static factory method to create empty hardware container

--- Domain/Domain/Orders/ValueObjects/Hardware.cs
+++ Domain/Domain/Orders/ValueObjects/Hardware.cs
@@ -2,4 +2,8 @@
 
 namespace Domain.Orders.ValueObjects;
 
-public record Hardware(Supply[] Supplies, DrawerSlide[] DrawerSlides, HangingRail[] HangingRails);
+public record Hardware(Supply[] Supplies, DrawerSlide[] DrawerSlides, HangingRail[] HangingRails) {
+
+    public static Hardware None() => new([], [], []);
+
+};
