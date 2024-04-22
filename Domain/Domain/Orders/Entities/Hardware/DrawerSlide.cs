8ab0d91fb8566a0d61f5b321fc60a5ec60cd9512
ZachLondono <zachary.londono5@gmail.com>
2024-04-22 17:42:11 -0400

    Added static factory methods to create drawer slides instances

--- Domain/Domain/Orders/Entities/Hardware/DrawerSlide.cs
+++ Domain/Domain/Orders/Entities/Hardware/DrawerSlide.cs
@@ -2,4 +2,10 @@
 
 namespace Domain.Orders.Entities.Hardware;
 
-public record DrawerSlide(Guid Id, int Qty, Dimension Length, string Style);
+public record DrawerSlide(Guid Id, int Qty, Dimension Length, string Style) {
+
+    public static DrawerSlide UndermountSlide(int qty, Dimension length) => new(Guid.NewGuid(), qty, length, "Hettich Undermount Slide");
+
+    public static DrawerSlide SidemountSlide(int qty, Dimension length) => new(Guid.NewGuid(), qty, length, "Sidemount Slide");
+
+};
