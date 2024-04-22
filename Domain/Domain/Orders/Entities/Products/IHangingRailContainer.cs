ce7035b56e6c46cd40f6c89a7e4a591c3e57a722
ZachLondono <zachary.londono5@gmail.com>
2024-04-22 16:53:20 -0400

    Added interfaces for products which contain hardware

---
+++ Domain/Domain/Orders/Entities/Products/IHangingRailContainer.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Orders.Entities.Hardware;
+
+namespace Domain.Orders.Entities.Products;
+
+public interface IHangingRailContainer {
+
+    public IEnumerable<HangingRail> GetHangingRails();
+
+}
