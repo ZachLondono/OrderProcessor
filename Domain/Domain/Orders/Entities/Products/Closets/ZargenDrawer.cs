5a351eb55a61a4273fbb176bd53d0e861c51b420
ZachLondono <zachary.londono5@gmail.com>
2024-06-14 18:33:29 -0400

    Get zargen slide supplies from zargen drawer boxes

--- Domain/Domain/Orders/Entities/Products/Closets/ZargenDrawer.cs
+++ Domain/Domain/Orders/Entities/Products/Closets/ZargenDrawer.cs
@@ -1,11 +1,12 @@
-﻿using Domain.Orders.Enums;
+﻿using Domain.Orders.Entities.Hardware;
+using Domain.Orders.Enums;
 using Domain.Orders.ValueObjects;
 using Domain.ProductPlanner;
 using Domain.ValueObjects;
 
 namespace Domain.Orders.Entities.Products.Closets;
 
-public class ZargenDrawer : IProduct, IPPProductContainer {
+public class ZargenDrawer : IProduct, IPPProductContainer, ISupplyContainer {
 
     public Guid Id { get; }
     public int Qty { get; }
@@ -23,17 +24,17 @@ public class ZargenDrawer : IProduct, IPPProductContainer {
     public IReadOnlyDictionary<string, string> Parameters { get; }
     public List<string> ProductionNotes { get; set; } = [];
 
-    public static Dimension[] StandardHeights => new Dimension[] {
-        Dimension.FromMillimeters(93),
-        Dimension.FromMillimeters(125),
-        Dimension.FromMillimeters(157),
-        Dimension.FromMillimeters(189),
-        Dimension.FromMillimeters(221),
-        Dimension.FromMillimeters(253),
-        Dimension.FromMillimeters(285),
-        Dimension.FromMillimeters(317),
-        Dimension.FromMillimeters(349),
-        Dimension.FromMillimeters(477),
+    public static Dictionary<Dimension, string> StandardHeights => new() {
+        { Dimension.FromMillimeters(93), "6035" },
+        { Dimension.FromMillimeters(125), "6036" },
+        { Dimension.FromMillimeters(157), "6136" },
+        { Dimension.FromMillimeters(189), "6236" },
+        { Dimension.FromMillimeters(221), "6236" },
+        { Dimension.FromMillimeters(253), "6436" },
+        { Dimension.FromMillimeters(285), "6436" },
+        { Dimension.FromMillimeters(317), "6436" },
+        { Dimension.FromMillimeters(349), "6436" },
+        { Dimension.FromMillimeters(477), "6436" },
     };
 
     public ZargenDrawer(Guid id, int qty, decimal unitPrice, int productNumber, string room, string sku, Dimension openingWidth, Dimension height, Dimension depth, ClosetMaterial material, ClosetPaint? paint, string edgeBandingColor, string comment, IReadOnlyDictionary<string, string> parameters) {
@@ -88,6 +89,10 @@ public class ZargenDrawer : IProduct, IPPProductContainer {
 
         return new List<PPProduct>() { new PPProduct(Id, Qty, Room, SKU, ProductNumber, "Royal_c", materialType, "slab", "standard", Comment, finishMaterials, ebMaterials, parameters, new Dictionary<string, string>(), new Dictionary<string, string>()) };
 
+    }
+
+    public IEnumerable<Supply> GetSupplies() {
+        yield return new Supply(Guid.NewGuid(), Qty, $"Zargen Drawer Slide : {Height.AsMillimeters()}H  {StandardHeights[Height]}/{Depth.AsMillimeters()} ");
     }
 
 }
