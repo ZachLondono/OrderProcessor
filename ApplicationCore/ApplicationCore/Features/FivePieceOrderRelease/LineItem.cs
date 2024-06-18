f39a85e9571415e3de3d7a65ced1b7267a9385e2
ZachLondono <zachary.londono5@gmail.com>
2024-06-18 13:43:56 -0400

    Started implementing five piece order releasing

---
+++ ApplicationCore/ApplicationCore/Features/FivePieceOrderRelease/LineItem.cs
@@ -0,0 +1,15 @@
+﻿using Domain.ValueObjects;
+
+namespace ApplicationCore.Features.FivePieceOrderRelease;
+
+internal record LineItem(int PartNum,
+                         string Description,
+                         int Line,
+                         int Qty,
+                         Dimension Width,
+                         Dimension Height,
+                         string SpecialFeatures,
+                         Dimension LeftStile,
+                         Dimension RightStile,
+                         Dimension TopRail,
+                         Dimension BottomRail);
