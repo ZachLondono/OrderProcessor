8ce037bb4da67147b48bab175b9e730c6449cfb3
ZachLondono <zachary.londono5@gmail.com>
2024-09-11 22:51:06 -0400

    Added production note entity

---
+++ Domain/Domain/Orders/Entities/ProductionNote.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Orders.Entities;
+
+public class ProductionNote {
+
+    public Guid Id { get; init; }
+    public string Value { get; init; }
+
+    public ProductionNote(Guid id, string value) {
+        Id = id;
+        Value = value;
+    }
+
+    public static ProductionNote Create(string value)
+        => new(Guid.NewGuid(), value);
+
+}
