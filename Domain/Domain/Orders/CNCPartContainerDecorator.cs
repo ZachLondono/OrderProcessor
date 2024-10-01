4a7e41ce0cad5c488447f28eb68ff030f5c69922
ZachLondono <zachary.londono5@gmail.com>
2024-10-01 18:46:01 -0400

    Created a decorator to add additional information to cnc generated parts.

---
+++ Domain/Domain/Orders/CNCPartContainerDecorator.cs
@@ -0,0 +1,40 @@
+﻿using CADCodeProxy.Machining;
+using Domain.Orders.Entities;
+
+namespace Domain.Orders;
+
+public class CNCPartContainerDecorator : ICNCPartContainer {
+
+    private readonly ICNCPartContainer _partContainer;
+    private readonly int _sequenceNumber;
+    private readonly Order _order;
+    private readonly string _customerName;
+
+    public CNCPartContainerDecorator(ICNCPartContainer partContainer, int sequenceNumber, Order order, string customerName) {
+        _partContainer = partContainer;
+        _sequenceNumber = sequenceNumber;
+        _order = order;
+        _customerName = customerName;
+    }
+
+    public bool ContainsCNCParts() => _partContainer.ContainsCNCParts();
+
+    public IEnumerable<Part> GetCNCParts() {
+
+        foreach (var part in _partContainer.GetCNCParts()) {
+
+            part.PrimaryFace.ProgramName = $"{_sequenceNumber}{_order.Number}{part.PrimaryFace.ProgramName}";
+
+            if (part.SecondaryFace is not null) {
+                part.PrimaryFace.ProgramName = $"{_sequenceNumber}{_order.Number}{part.PrimaryFace.ProgramName}";
+            }
+
+            part.InfoFields.Add("CustomerInfo1", _customerName);
+
+            yield return part;
+
+        }
+
+    }
+
+}
