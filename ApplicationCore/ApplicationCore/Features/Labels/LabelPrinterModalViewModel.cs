31fc2a3f31b34a4186b88c3a705df00063d62fa3
ZachLondono <zachary.londono5@gmail.com>
2024-10-03 17:04:22 -0400

    Started adding label printer modal feature

---
+++ ApplicationCore/ApplicationCore/Features/Labels/LabelPrinterModalViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Orders.Entities;
+
+namespace ApplicationCore.Features.Labels;
+
+public partial class LabelPrinterModal {
+
+    public void LoadLabelsFromOrder(Order order) {
+
+    }
+
+    public void PrintAllLabels() {
+
+    }
+
+    public void ToggleLabelSelection() {
+
+    }
+
+}
