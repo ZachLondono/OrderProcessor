924f13c88b3073a7be2d54d5161d841b2f55a112
ZachLondono <zachary.londono5@gmail.com>
2024-10-05 00:35:33 -0400

    Added Fraction ToString tests

---
+++ Domain/Domain.Tests.Unit/ValueObjects/FractionTests.cs
@@ -0,0 +1,26 @@
+﻿using Domain.ValueObjects;
+using FluentAssertions;
+
+namespace Domain.Tests.Unit.ValueObjects;
+
+public class FractionTests {
+
+    [Theory]
+    [InlineData(1, 2, "1/2")]
+    [InlineData(2, 2, "1")]
+    [InlineData(3, 2, "1 1/2")]
+    [InlineData(0, 2, "0")]
+    public void ToString_ShouldFormatFractionCorrectly(int numerator, int denominator, string expected) {
+
+        // Arrange
+        var frac = new Fraction(numerator, denominator);
+
+        // Act
+        var result = frac.ToString();
+
+        // Assert
+        result.Should().Be(expected);
+
+    }
+
+}
