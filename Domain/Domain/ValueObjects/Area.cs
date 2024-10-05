b74e240a5638e379e3617db27d5547d500f7a8dc
ZachLondono <zachary.londono5@gmail.com>
2024-10-05 00:04:39 -0400

    Added a class which represents an Area as either square inches or square millimeters

---
+++ Domain/Domain/ValueObjects/Area.cs
@@ -0,0 +1,26 @@
+﻿namespace Domain.ValueObjects;
+
+public class Area {
+
+    private readonly double _sqr_mm;
+
+    private Area(double sqr_mm) {
+        if (sqr_mm < 0) {
+            throw new ArgumentOutOfRangeException(nameof(sqr_mm), "Dimension cannot be negative");
+        }
+        _sqr_mm = sqr_mm;
+    }
+
+    public double AsSquareInches() => _sqr_mm / (25.4*25.4);
+
+    public double AsSquareMillimeters() => _sqr_mm;
+
+    public static Area FromSquareInches(double sqr_in) => new(sqr_in * (25.4*25.4));
+
+    public static Area FromSquareMillimeters(double sqr_in) => new(sqr_in * (25.4*25.4));
+
+    public static Dimension Sqrt(Area area) => Dimension.FromMillimeters(Math.Sqrt(area.AsSquareMillimeters()));
+
+    public static Area operator +(Area area1, Area area2) => Area.FromSquareMillimeters(area1.AsSquareMillimeters() + area2.AsSquareMillimeters());
+
+}
