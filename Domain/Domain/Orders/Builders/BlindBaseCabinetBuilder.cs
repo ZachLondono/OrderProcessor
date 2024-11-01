34b79d38b3efdec8f9e6dc75de1b23c5b3b0b155
ZachLondono <zachary.londono5@gmail.com>
2024-11-01 18:11:47 -0400

    Updated cabinet builders to accept null drawer box options

--- Domain/Domain/Orders/Builders/BlindBaseCabinetBuilder.cs
+++ Domain/Domain/Orders/Builders/BlindBaseCabinetBuilder.cs
@@ -14,7 +14,7 @@ public class BlindBaseCabinetBuilder : CabinetBuilder<BlindBaseCabinet> {
     public BlindSide BlindSide { get; private set; }
     public Dimension BlindWidth { get; private set; }
     public ToeType ToeType { get; private set; }
-    public CabinetDrawerBoxOptions BoxOptions { get; private set; }
+    public CabinetDrawerBoxOptions? BoxOptions { get; private set; }
     public bool IsGarage { get; private set; }
 
     public BlindBaseCabinetBuilder() {
@@ -66,7 +66,7 @@ public class BlindBaseCabinetBuilder : CabinetBuilder<BlindBaseCabinet> {
         return this;
     }
 
-    public BlindBaseCabinetBuilder WithBoxOptions(CabinetDrawerBoxOptions boxOptions) {
+    public BlindBaseCabinetBuilder WithBoxOptions(CabinetDrawerBoxOptions? boxOptions) {
         BoxOptions = boxOptions;
         return this;
     }
