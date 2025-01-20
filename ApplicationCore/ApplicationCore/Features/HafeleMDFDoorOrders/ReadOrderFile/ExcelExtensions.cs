e65556fbef1f2789c6c8cfb6a938f5b17d50187e
ZachLondono <zachary.londono5@gmail.com>
2025-01-20 13:49:01 -0500

    Implemented reading data from hafele MDF Door order file

---
+++ ApplicationCore/ApplicationCore/Features/HafeleMDFDoorOrders/ReadOrderFile/ExcelExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ClosedXML.Excel;
+
+namespace ApplicationCore.Features.HafeleMDFDoorOrders.ReadOrderFile;
+
+public static class ExcelExtensions {
+
+    public static T GetValueOrDefault<T>(this IXLCell cell, T defaultVal) {
+
+        try {
+
+            return cell.GetValue<T>();
+
+        } catch {
+
+            return defaultVal;
+
+        }
+
+    }
+
+}
+
