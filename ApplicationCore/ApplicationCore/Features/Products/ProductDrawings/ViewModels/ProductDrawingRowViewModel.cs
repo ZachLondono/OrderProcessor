c416acb3f41297e364a8b45757f78107228b912e
ZachLondono <zachary.londono5@gmail.com>
2025-03-14 18:01:19 -0400

    Revert "Removed delay"
    This reverts commit 065b5490882c490d9fa2680fa56aae64652f32a1.

--- ApplicationCore/ApplicationCore/Features/Products/ProductDrawings/ViewModels/ProductDrawingRowViewModel.cs
+++ ApplicationCore/ApplicationCore/Features/Products/ProductDrawings/ViewModels/ProductDrawingRowViewModel.cs
@@ -56,6 +56,8 @@ public class ProductDrawingRowViewModel {
 
         IsLoading = true;
 
+        await Task.Delay(1000);
+
         try {
 
             var response = await _bus.Send(new ImportProductDrawingIntoActiveDocument.Command(drawing, mode));
