95853629d44976aa8ba4b9cb500f9f055725fd56
ZachLondono <zachary.londono5@gmail.com>
2025-03-19 22:14:03 -0400

    Removed unused classes

--- Orders/OrderLoading/OrderLoading/LoadHafeleMDFDoorSpreadsheetOrderData/EmailDetails.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OrderLoading.LoadHafeleMDFDoorSpreadsheetOrderData;
-
-public class EmailDetails {
-
-    public required string Company { get; set; } = string.Empty;
-    public required string OrderNumber { get; set; } = string.Empty;
-    public required EmailAttachment[] Attachments { get; init; } = [];
-
-}
