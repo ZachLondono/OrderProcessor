368585b3c5dd76d9db6d5b209605407cc4645b5d
ZachLondono <zachary.londono5@gmail.com>
2025-03-20 16:37:57 -0400

    Implemented hafele mdf door order provider

--- Orders/OrderLoading/OrderLoading/LoadHafeleMDFDoorSpreadsheetOrderData/HafeleMDFDoorOrderProvider.cs
+++ Orders/OrderLoading/OrderLoading/LoadHafeleMDFDoorSpreadsheetOrderData/HafeleMDFDoorOrderProvider.cs
@@ -1,66 +1,61 @@
-﻿using Domain.Services;
+﻿using Domain.Orders.Entities.Products;
+using Domain.Orders.Entities.Products.Doors;
+using Domain.Orders.Enums;
+using Domain.Orders.ValueObjects;
+using Domain.ValueObjects;
+using OrderLoading.LoadHafeleMDFDoorSpreadsheetOrderData.ReadOrderFile;
 
 namespace OrderLoading.LoadHafeleMDFDoorSpreadsheetOrderData;
 
 public class HafeleMDFDoorOrderProvider : IOrderProvider {
 
     private const string _workingDirectoryRoot = @"R:\Door Orders\Hafele\Orders";
-    private readonly IFileReader _fileReader;
 
     public IOrderLoadWidgetViewModel? OrderLoadingViewModel { get; set; }
 
-    public HafeleMDFDoorOrderProvider(IFileReader fileReader) {
-        _fileReader = fileReader;
-    }
-
     public Task<OrderData?> LoadOrderData(string source) {
 
-        throw new NotImplementedException();
+        var parts = source.Split(":");
 
-    }
+        if (parts.Length != 3) {
+            OrderLoadingViewModel?.AddLoadingMessage(MessageSeverity.Error, $"Improper Hafele MDF Door Order Form Source - {source}");
+            return Task.FromResult<OrderData?>(null);
+        }
 
-    /*
-    public void ImportOrderFromMailItem(EmailDetails details, MailItem mailItem) {
+        var company = parts[0];
+        var orderNumber = parts[1];
+        var filePath = parts[2];
 
-        var structure = CreateDirectoryStructure(_workingDirectoryRoot, $"{details.OrderNumber} - {details.Company}");
-
+        var structure = CreateDirectoryStructure(_workingDirectoryRoot, $"{orderNumber} - {company}");
+        
         if (structure is null) {
-            return;
+            return Task.FromResult<OrderData?>(null);
         }
 
-        foreach (var emailAttachment in details.Attachments) {
+        File.Copy(filePath, Path.Combine(structure.IncomingDirectory));
 
-            if (!emailAttachment.CopyToIncoming) {
-                continue;
-            }
+        var result = HafeleMDFDoorOrder.Load(filePath);
 
-            var attachment = mailItem.Attachments[emailAttachment.Index];
-
-            if (attachment is null) {
-                continue;
-            }
-
-            var orderFilePath = Path.Combine(structure.IncomingDirectory, attachment.FileName);
-            attachment.SaveAsFile(orderFilePath);
-
-            if (!emailAttachment.IsOrderForm) {
-                continue;
-            }
-
-            var orderData = HafeleMDFDoorOrder.Load(orderFilePath);
-
-            foreach (var error in orderData.Errors) {
-                OrderLoadingViewModel?.AddLoadingMessage(MessageSeverity.Error, error);
-            }
-
-            foreach (var warning in orderData.Warnings) {
-                OrderLoadingViewModel?.AddLoadingMessage(MessageSeverity.Warning, warning);
-            }
-
+        foreach (var error in result.Errors) {
+            OrderLoadingViewModel?.AddLoadingMessage(MessageSeverity.Error, error);
         }
+
+        foreach (var warning in result.Warnings) {
+            OrderLoadingViewModel?.AddLoadingMessage(MessageSeverity.Warning, warning);
+        }
+
+        if (result.Data is null) {
+            OrderLoadingViewModel?.AddLoadingMessage(MessageSeverity.Error, $"Failed to load order data from '{filePath}'");
+            return Task.FromResult<OrderData?>(null);
+        }
+
+        var fileData = result.Data;
+
+        var orderData = MapHafeleOrder(orderNumber, Guid.Empty, Guid.Empty, fileData, structure.WorkingDirectory);
+
+        return Task.FromResult<OrderData?>(orderData);
 
     }
-    */
 
     public DirectoryStructure? CreateDirectoryStructure(string workingDirectoryRoot, string workingDirectoryName) {
 
@@ -71,15 +66,152 @@ public class HafeleMDFDoorOrderProvider : IOrderProvider {
             return null;
         }
 
-        string cutlistDir = Path.Combine(workingDirectory, "CUTLIST");
-        _ = Directory.CreateDirectory(cutlistDir);
+        string cutListDir = Path.Combine(workingDirectory, "CUTLIST");
+        _ = Directory.CreateDirectory(cutListDir);
         string incomingDir = Path.Combine(workingDirectory, "incoming");
         _ = Directory.CreateDirectory(incomingDir);
         string ordersDir = Path.Combine(workingDirectory, "orders");
         _ = Directory.CreateDirectory(ordersDir);
 
-        return new DirectoryStructure(workingDirectory, incomingDir, ordersDir, cutlistDir );
+        return new DirectoryStructure(workingDirectory, incomingDir, ordersDir, cutListDir );
 
+    }
+
+    public OrderData MapHafeleOrder(string orderNumber, Guid customerId, Guid vendorId, HafeleMDFDoorOrder data, string workingDirectory) {
+
+        if (!data.Data.MaterialThicknessesByName.TryGetValue(data.Options.Material, out var materialThickness)) {
+            throw new InvalidOperationException($"Material '{data.Options.Material}' not found in material thicknesses look up");
+        }
+
+        var thicknessDim = Dimension.FromInches(materialThickness);
+
+        string? paintColor = GetFinish(data.Options.Finish)?.Color ?? null;
+
+        var products = data.Sizes.Select(s => CreateProduct(data.Options, s, thicknessDim, paintColor)).ToList();
+
+        var orderedDate = data.Options.Date;
+        var dueDate = orderedDate.AddDays(GetLeadTime(data.Options.ProductionTime));
+
+        var isRush = IsRush(data.Options.ProductionTime);
+
+        var info = new Dictionary<string, string> {
+            { "Customer", data.Options.Company },
+            { "Account #", data.Options.AccountNumber },
+            { "Customer Email", data.Options.Email },
+            { "Customer PO", data.Options.PurchaseOrder },
+            { "Customer Job Name", data.Options.JobName },
+            { "Hafele PO", data.Options.HafelePO },
+            { "Hafele Order #", data.Options.HafeleOrderNumber },
+            { "Tracking #", data.Options.TrackingNumber },
+        };
+
+        return new OrderData() {
+            Number = orderNumber,
+            Name = data.Options.JobName,
+            Comment = data.Options.OrderComments,
+            Tax = 0,
+            PriceAdjustment = 0,
+            Shipping = new ShippingInfo() {
+                Method = data.Options.Delivery,
+                Contact = data.Options.Contact,
+                Price = 0,
+                PhoneNumber = data.Options.Phone,
+                Address = new Address() {
+                    Line1 = data.Options.AddressLine1,
+                    Line2 = data.Options.AddressLine2,
+                    City = data.Options.City,
+                    State = data.Options.State,
+                    Zip = data.Options.Zip,
+                    Country = "USA"
+                }
+            },
+            Billing = new BillingInfo() {
+                Address = new Address(),
+                PhoneNumber = "",
+                InvoiceEmail = ""
+            },
+            OrderDate = orderedDate,
+            DueDate = dueDate,
+            CustomerId = customerId,
+            VendorId = vendorId,
+            Rush = isRush,
+            Info = [],
+            Products = products,
+            Hardware = Hardware.None(),
+            AdditionalItems = [],
+            WorkingDirectory = workingDirectory
+        };
+
+    }
+
+    public static IProduct CreateProduct(Options options, Size size, Dimension thickness, string? paintColor) {
+
+        AdditionalOpening[] additionalOpenings;
+        DoorType doorType;
+
+        switch (size.Type) {
+
+            default:
+                doorType = DoorType.Door;
+                additionalOpenings = [];
+                break;
+
+        };
+
+        var frameSize = new DoorFrame() {
+            TopRail = Dimension.FromInches(size.TopRail),
+            BottomRail = Dimension.FromInches(size.BottomRail),
+            LeftStile = Dimension.FromInches(size.LeftStile),
+            RightStile = Dimension.FromInches(size.RightStile),
+        };
+
+        return MDFDoorProduct.Create(size.UnitPrice,
+                                    "",
+                                    size.Qty,
+                                    size.LineNumber,
+                                    doorType,
+                                    Dimension.FromInches(size.Height),
+                                    Dimension.FromInches(size.Width),
+                                    size.SpecialInstructions,
+                                    frameSize,
+                                    options.Material,
+                                    thickness,
+                                    options.DoorStyle,
+                                    options.EdgeProfile,
+                                    options.PanelDetail,
+                                    Dimension.FromInches(options.PanelDrop),
+                                    DoorOrientation.Vertical,
+                                    additionalOpenings,
+                                    paintColor);
+
+    }
+
+    public int GetLeadTime(string leadTime) => leadTime switch {
+        "Standard 10 day" => 10,
+        "5 Day Rush" => 5,
+        _ => throw new InvalidOperationException($"Unexpected lead time - '{leadTime}'")
+    };
+
+    public bool IsRush(string leadTime) => leadTime switch {
+        "Standard 10 day" => false,
+        "5 Day Rush" => true,
+        _ => throw new InvalidOperationException($"Unexpected lead time - '{leadTime}'")
+    };
+
+    private static Finish? GetFinish(string finish) => finish switch {
+        "None" => null,
+        "White Prime Only" => new(FinishType.Prime, "White Primer"),
+        "Grey Prime Only" => new(FinishType.Prime, "Grey Primer"),
+        "Black Prime Only" => new(FinishType.Prime, "Black Primer"),
+        "Standard Color" => new(FinishType.Paint, "Standard Color"),
+        "Custom Color" => new(FinishType.Paint, "Custom Color"),
+        _ => throw new InvalidOperationException($"Unexpected finish option - '{finish}'")
+    };
+
+    record Finish(FinishType Type, string Color);
+    enum FinishType {
+        Prime,
+        Paint
     }
 
 }
