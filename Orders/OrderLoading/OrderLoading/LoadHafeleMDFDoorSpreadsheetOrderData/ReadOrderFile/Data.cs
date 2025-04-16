dd3593218e54fa6a9b0bc325ef19c2b74fa15257
ZachLondono <zachary.londono5@gmail.com>
2025-04-16 16:17:21 -0400

    Updated how material thicknesses and door panel counts are read from hafele order forms

--- Orders/OrderLoading/OrderLoading/LoadHafeleMDFDoorSpreadsheetOrderData/ReadOrderFile/Data.cs
+++ Orders/OrderLoading/OrderLoading/LoadHafeleMDFDoorSpreadsheetOrderData/ReadOrderFile/Data.cs
@@ -16,15 +16,13 @@ public class Data {
         var markUp = sheet.Cell("Hafele_Mark_Up").GetDouble();
         var discount = sheet.Cell("Discount").GetDouble();
 
-        var materials = sheet.Cells("Materials").Select(c => c.GetValue<string>()).Where(m => !string.IsNullOrEmpty(m)).ToArray();
-        var thicknesses = sheet.Cells("Q10:Q14").Select(c => c.GetValue<string>()).Where(t => !string.IsNullOrEmpty(t)).Select(t => double.Parse(t)).ToArray();
+        var (materials, thicknesses) = GetMaterialData(sheet);
         Dictionary<string, double> thicknessesByMaterial = [];
         for (int i = 0; i < materials.Length; i++) {
             thicknessesByMaterial[materials[i]] = thicknesses[i];
         }
 
-        var doorTypes = sheet.Cells("Door_Types").Select(c => c.GetValue<string>()).ToArray();
-        var doorPanelCounts = sheet.Cells("Door_Types_Panel_Counts").Select(c => (int) c.GetDouble()).ToArray();
+        var (doorTypes, doorPanelCounts) = GetDoorTypeData(sheet);
         Dictionary<string, int> panelCountByDoorTypes = [];
         for (int i = 0; i < doorTypes.Length; i++) {
             panelCountByDoorTypes[doorTypes[i]] = doorPanelCounts[i];
@@ -36,6 +34,66 @@ public class Data {
             MaterialThicknessesByName = thicknessesByMaterial,
             PanelCountByDoorType = panelCountByDoorTypes
         };
+
+    }
+
+    private static (string[] MaterialNames, double[] MaterialThicknesses) GetMaterialData(IXLWorksheet sheet) {
+
+        var materials = sheet.Cells("Materials")
+                             .Select(c => c.GetValue<string>())
+                             .Where(m => !string.IsNullOrEmpty(m))
+                             .ToArray();
+
+        int expectedRowStart = 10;
+        int expectedRowEnd = expectedRowStart + materials.Length - 1;
+
+        var thicknesses = sheet.Cells($"Q{expectedRowStart}:Q{expectedRowEnd}")
+                                .Select(c => c.GetValue<string>())
+                                .Where(t => !string.IsNullOrEmpty(t))
+                                .Select(t => double.Parse(t))
+                                .ToArray();
+
+        return (materials, thicknesses);
+
+    }
+
+    private static (string[] DoorTypes, int[] PanelCounts) GetDoorTypeData(IXLWorksheet sheet) {
+
+        IXLCells doorTypesRange;
+
+        try {
+
+            doorTypesRange = sheet.Cells("Door_Types");
+
+        } catch {
+
+            doorTypesRange = sheet.Cells("B10:B23");
+
+        }
+
+        var doorTypes = doorTypesRange.Select(c => c.GetValue<string>())
+                                      .ToArray();
+
+
+        IXLCells panelCountRange;
+
+        try {
+
+            panelCountRange = sheet.Cells("Door_Types_Panel_Counts");
+
+        } catch {
+
+            int expectedRowStart = 10;
+            int expectedRowEnd = expectedRowStart + doorTypes.Length - 1;
+
+            panelCountRange = sheet.Cells($"D{expectedRowStart}:D{expectedRowEnd}");
+
+        }
+
+        var doorPanelCounts = panelCountRange.Select(c => (int)c.GetDouble())
+                                   .ToArray();
+
+        return (doorTypes, doorPanelCounts);
 
     }
 
