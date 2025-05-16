0774cf0f7fa6d962e31c903f0e7f95e1d82dac41
ZachLondono <zachary.londono5@gmail.com>
2025-05-16 18:34:19 -0400

    Moved the logic necessary for creating products from a hafele mdf door order file into the class which represents the mdf door order file.

--- Orders/OrderLoading/OrderLoading/LoadHafeleMDFDoorSpreadsheetOrderData/ReadOrderFile/HafeleMDFDoorOrder.cs
+++ Orders/OrderLoading/OrderLoading/LoadHafeleMDFDoorSpreadsheetOrderData/ReadOrderFile/HafeleMDFDoorOrder.cs
@@ -1,4 +1,9 @@
 ﻿using ClosedXML.Excel;
+using Domain.Orders.Entities.Products.Doors;
+using Domain.Orders.Enums;
+using Domain.Orders.ValueObjects;
+using Domain.ValueObjects;
+using OneOf.Types;
 
 namespace OrderLoading.LoadHafeleMDFDoorSpreadsheetOrderData.ReadOrderFile;
 
@@ -38,5 +43,113 @@ public class HafeleMDFDoorOrder {
         return new(warnings, errors, order);
 
     }
+
+    public ICollection<MDFDoorProduct> GetProducts() {
+
+        if (!Data.MaterialThicknessesByName.TryGetValue(Options.Material, out var materialThickness)) {
+            throw new InvalidOperationException($"Material '{Options.Material}' not found in material thicknesses look up");
+        }
+
+        var thicknessDim = Dimension.FromInches(materialThickness);
+        var finish = GetFinish(Options.Finish);
+        decimal markUp = (decimal) Data.HafeleMarkUpToCustomers;
+
+        List<MDFDoorProduct> doors = [];
+
+        foreach (var size in Sizes) {
+            var door = CreateProduct(size, finish, thicknessDim, markUp);
+            doors.Add(door);
+        }
+
+        return doors;
+
+    }
+
+    private MDFDoorProduct CreateProduct(Size size, MDFDoorFinish finish, Dimension thickness, decimal markUp) {
+
+        AdditionalOpening[] additionalOpenings;
+        DoorType doorType;
+
+        switch (size.Type) {
+
+            case "Single Panel":
+                doorType = DoorType.Door;
+                additionalOpenings = [];
+                break;
+
+            case "Drawer Front - A":
+            case "Drawer Front - B":
+                doorType = DoorType.DrawerFront;
+                additionalOpenings = [];
+                break;
+
+            case "Double Panel, SS":
+                doorType = DoorType.Door;
+                additionalOpenings = [
+                        new(Dimension.FromInches(size.Rail3), Dimension.FromInches(size.Panel1Height))
+                    ];
+                break;
+
+            case "Triple Panel":
+                doorType = DoorType.Door;
+                additionalOpenings = [
+                        new(Dimension.FromInches(size.Rail3), Dimension.FromInches(size.Panel1Height)),
+                        new(Dimension.FromInches(size.Rail4), Dimension.FromInches(size.Panel2Height))
+                    ];
+                break;
+
+            case "Quadruple Panel":
+                doorType = DoorType.Door;
+                additionalOpenings = [
+                        new(Dimension.FromInches(size.Rail3), Dimension.FromInches(size.Panel1Height)),
+                        new(Dimension.FromInches(size.Rail4), Dimension.FromInches(size.Panel2Height)),
+                        new(Dimension.FromInches(size.Rail5), Dimension.FromInches(size.Panel3Height))
+                    ];
+                break;
+
+            default:
+                throw new NotImplementedException($"{size.Type} doors not implemented");
+
+        };
+
+        var frameSize = new DoorFrame() {
+            TopRail = Dimension.FromInches(size.TopRail),
+            BottomRail = Dimension.FromInches(size.BottomRail),
+            LeftStile = Dimension.FromInches(size.LeftStile),
+            RightStile = Dimension.FromInches(size.RightStile),
+        };
+
+        var adjustedUnitPrice = size.UnitPrice / (1 + markUp);
+
+        return MDFDoorProduct.Create(adjustedUnitPrice,
+                                    "",
+                                    size.Qty,
+                                    size.LineNumber,
+                                    doorType,
+                                    Dimension.FromInches(size.Height),
+                                    Dimension.FromInches(size.Width),
+                                    size.SpecialInstructions,
+                                    frameSize,
+                                    Options.Material,
+                                    thickness,
+                                    Options.DoorStyle,
+                                    Options.EdgeProfile,
+                                    Options.PanelDetail,
+                                    Dimension.FromInches(Options.PanelDrop),
+                                    DoorOrientation.Vertical,
+                                    additionalOpenings,
+                                    finish);
+
+    }
+
+    private static MDFDoorFinish GetFinish(string finish) => finish switch {
+        "None" => new None(),
+        "White Prime Only" => new Primer("White Primer"),
+        "Grey Prime Only" => new Primer("Grey Primer"),
+        "Black Prime Only" => new Primer("Black Primer"),
+        "Standard Color" => new Paint("Standard Color"),
+        "Custom Color" => new Paint("Custom Color"),
+        _ => throw new InvalidOperationException($"Unexpected finish option - '{finish}'")
+    };
 
 }
