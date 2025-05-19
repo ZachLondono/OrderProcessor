17cbbc96825447c9ece37a87dd33a7e5f2759c24
ZachLondono <zachary.londono5@gmail.com>
2025-05-19 13:03:16 -0400

    Fixedf migration script

--- ApplicationCore/ApplicationCore/Application/Schemas/Ordering Migrations/version9.sql
+++ ApplicationCore/ApplicationCore/Application/Schemas/Ordering Migrations/version9.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE mdf_open_panels (
 	id BLOB NOT NULL,
-	back_rabbet INTEGER NOT NULL,
+	rabbet_back INTEGER NOT NULL,
 	route_for_gasket INTEGER NOT NULL
 );
 
-ALTER TABLE mdf_door_configs ADD COLUMN mdf_open_panel_id BLOB DEFAULT NULL;
+ALTER TABLE mdf_door_products ADD COLUMN mdf_open_panel_id BLOB DEFAULT NULL;
 ALTER TABLE mdf_door_openings ADD COLUMN mdf_open_panel_id BLOB DEFAULT NULL;
 
