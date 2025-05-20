3c0bad58405be86d34c9b1553ec10ab5e5da6562
ZachLondono <zachary.londono5@gmail.com>
2025-05-20 19:16:57 -0400

    Implemented posting hafele mdf door order to google sheet.

---
+++ ApplicationCore/ApplicationCore/Features/MDFDoorOrders/ProcessHafeleMDFOrder/GoogleSheetRow.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+namespace ApplicationCore.Features.MDFDoorOrders.ProcessHafeleMDFOrder;
+
+public class GoogleSheetRow {
+
+    public string FileDate { get; set; } = string.Empty;
+    public string HafelePO { get; set; } = string.Empty;
+    public string ProjectNumber { get; set; } = string.Empty;
+    public string ConfigNumber { get; set; } = string.Empty;
+    public string CustomerName { get; set; } = string.Empty;
+    public string CustomerPO { get; set; } = string.Empty;
+    public string TotalItemCount { get; set; } = string.Empty;
+    public string ShipDate { get; set; } = string.Empty;
+    public string InvoiceAmount { get; set; } = string.Empty;
+    public string TrackingNumber { get; set; } = string.Empty;
+
+    public async Task PostData() {
+
+        ProcessStartInfo startInfo = new() {
+            CreateNoWindow = true,
+            UseShellExecute = false,
+            WindowStyle = ProcessWindowStyle.Hidden,
+            FileName = "R:\\DB ORDERS\\GoogleSheetsExe\\publish\\GoogleSheetsUpdater.exe",
+            Arguments = $"hafele \"{FileDate}\" \"{HafelePO}\" \"{ProjectNumber}\" \"{ConfigNumber}\" \"{CustomerName}\" \"{CustomerPO}\" \"{TotalItemCount}\" \"{ShipDate}\" \"{InvoiceAmount}\" \"{TrackingNumber}\""
+        };
+
+        try {
+
+            using Process? process = Process.Start(startInfo);
+
+            if (process is null) {
+                return;
+            }
+
+            await process.WaitForExitAsync();
+
+        } catch (Exception e) {
+            throw new Exception("Error while tracking order on google sheet", e);
+            throw;
+        }
+
+    }
+
+}
--- ApplicationCore/ApplicationCore/Features/MDFDoorOrders/ProcessHafeleMDFOrder/HafeleMDFDoorOrderProcessor.cs
+++ ApplicationCore/ApplicationCore/Features/MDFDoorOrders/ProcessHafeleMDFOrder/HafeleMDFDoorOrderProcessor.cs
@@ -1,4 +1,6 @@
-﻿using OrderLoading.LoadHafeleMDFDoorSpreadsheetOrderData.ReadOrderFile;
+﻿using ApplicationCore.Pages.CustomerDetails;
+using Domain.Companies.Entities;
+using OrderLoading.LoadHafeleMDFDoorSpreadsheetOrderData.ReadOrderFile;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,9 @@ namespace ApplicationCore.Features.MDFDoorOrders.ProcessHafeleMDFOrder;
 
 public class HafeleMDFDoorOrderProcessor {
 
-    public void ProcessOrder(ProcessOptions options) {
+    public async Task ProcessOrderAsync(ProcessOptions options) {
 
-        var orderData = LoadOrderData(options.OrderSheetTemplatePath);
+        var orderData = LoadOrderData(options.DataFile);
 
         if (orderData is null) {
 
@@ -32,7 +34,7 @@ public class HafeleMDFDoorOrderProcessor {
         }
 
         if (options.PostToGoogleSheets) {
-            PostOrderToGoogleSheet();
+            await PostOrderToGoogleSheet(orderData);
         }
 
     }
@@ -50,6 +52,23 @@ public class HafeleMDFDoorOrderProcessor {
 
     private string FillOrderSheet() => throw new NotImplementedException();
 
-    private string PostOrderToGoogleSheet() => throw new NotImplementedException();
+    private static async Task PostOrderToGoogleSheet(HafeleMDFDoorOrder order) {
+
+        var row = new GoogleSheetRow() {
+            FileDate = order.Options.Date.ToShortDateString(),
+            HafelePO = order.Options.HafelePO,
+            ProjectNumber = order.Options.HafeleOrderNumber,
+            ConfigNumber = "",
+            CustomerName = order.Options.Company,
+            CustomerPO = order.Options.JobName,
+            TotalItemCount = order.Sizes.Sum(s => s.Qty).ToString(),
+            ShipDate = order.Options.GetDueDate().ToShortDateString(),
+            InvoiceAmount = order.GetInvoiceAmount().ToString(),
+            TrackingNumber = ""
+        };
+
+        await row.PostData();
+
+    }
 
 }
