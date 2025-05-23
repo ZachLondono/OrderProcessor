a8fe51d992dd70ab49570905a398469ab4ced51f
ZachLondono <zachary.londono5@gmail.com>
2025-05-23 16:10:59 -0400

    Added properties to determine if a mdf door panel is solid or open

--- Domain/Domain/Orders/ValueObjects/MDFDoorPanel.cs
+++ Domain/Domain/Orders/ValueObjects/MDFDoorPanel.cs
@@ -7,6 +7,12 @@ public class MDFDoorPanel : OneOfBase<SolidPanel, OpenPanel> {
     public MDFDoorPanel(OneOf<SolidPanel, OpenPanel> _) : base(_) { }
 
 	public static implicit operator MDFDoorPanel(SolidPanel _) => new(_);
+	public static explicit operator SolidPanel(MDFDoorPanel _) => _.AsT0;
+
 	public static implicit operator MDFDoorPanel(OpenPanel _) => new(_);
+	public static explicit operator OpenPanel(MDFDoorPanel _) => _.AsT1;
+
+	public bool IsSolid => IsT0;
+	public bool IsOpen => IsT1;
 
 }
