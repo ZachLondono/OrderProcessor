d05b64a7133d56649177420618b3de527f0df1e6
ZachLondono <zachary.londono5@gmail.com>
2025-06-18 13:50:19 -0400

    Added an additional door type for applied panels

--- Domain/Domain/Orders/Enums/DoorType.cs
+++ Domain/Domain/Orders/Enums/DoorType.cs
@@ -1,7 +1,16 @@
 ﻿namespace Domain.Orders.Enums;
 
 public enum DoorType {
+
     Door,
+
+    // Drawer fronts do not get hinges and hardware is attached to the center of the drawer front or as a pair, centered vertically
     DrawerFront,
-    HamperDoor
+
+    // Hamper doors will have hinges at the bottom and hardware centered at the top
+    HamperDoor,
+
+    // Applied panels do not get hinges or hardware, they are attached to a cabinet or appliance for decoration
+    AppliedPanel
+
 }
